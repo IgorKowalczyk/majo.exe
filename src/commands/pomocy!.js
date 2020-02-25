bd3d882f250f4ed3ac22ab5363fbcbfeeb3dec69
IgorKowalczyk <majonezexe@gmail.com>
2020-02-25 17:13:57 +0100

    New commend

--- src/commands/pomocy!.js
+++
@@ -1,5 +0,0 @@
-const pomocy! = async (message) => {
-  await message.channel.send('@here, ludzie pomoc! Znowu potrzebna jest dzisiaj!')
-}
-
-module.exports = { pomocy! }
---
+++ src/commands/pomocy.js
@@ -0,0 +1,5 @@
+const pomocy = async (message) => {
+  await message.channel.send('@here, ludzie pomocy!!! Znowu potrzebna!!! Halo!!!')
+}
+
+module.exports = { pomocy }
