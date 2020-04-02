92cf0999276b6aab996e5b3daa1a827dd3bbaaef
IgorKowalczyk <majonezexe@gmail.com>
2020-04-02 12:13:02 +0200

    Status

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Serving ${client.guilds.size} servers, (!majo {command})`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+/*
 client.on('ready', () => {
     client.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
     client.user.setPresence({
@@ -12,6 +33,8 @@ client.on('ready', () => {
         }
     });
 });
+
+*/
 
 /*
 client.user.setActivity('!majonezie (command)', { type: 'PLAYING' });
