c5e8cf94e9c920ef3a79b9c15afc3b73eac87104
IgorKowalczyk <majonezexe@gmail.com>
2020-04-05 19:07:02 +0200

    Add play command

---
+++ commands/stop.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'stop',
+	description: 'Stop all songs in the queue!',
+	execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
+		serverQueue.songs = [];
+		serverQueue.connection.dispatcher.end();
+	},
+};
