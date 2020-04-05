8bf48081127b813fd47620f794b605b3d405fe36
IgorKowalczyk <majonezexe@gmail.com>
2020-04-05 13:53:02 +0200

    Change readme and add better invite

--- commands/botinvite.js
+++ commands/botinvite.js
@@ -1,7 +1,18 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  message.author.send("https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591");
+  const embed = {
+  "title": "Majo.exe",
+  "description": "Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591) \n Check website: [majobot](https://igorkowalczyk.github.io)\n Join to official server [PL]: [Join](https://discord.gg/f4KtqNB)",
+  "url": "https://igorkowalczyk.github.io/majobot",
+  "color": 3447003,
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/avatars/544164729354977282/c39c2d7b39e5d6d5d13a8c2bdb010373.png?size=2048"
+  }
+};
+
+message.author.send("(Auto generated)", { embed });
+
 message.channel.send({embed: {
             color: 3447003,
             title: "Check a priv message!"
--- readme.md
+++ readme.md
@@ -8,6 +8,26 @@
 4. Create a `.env` file and add a `BOT_TOKEN` environmental variable whose value is the token above and add a `PREFIX` - this is your bot prefix (The `.env` file was private).
 5. Run `npm run dev`
 
+## Example `.env` file
+
+```
+# Environment Config
+
+# store your secrets and config variables in here
+# only invited collaborators will be able to see your .env values
+
+# reference these in your code with process.env.SECRET
+
+
+TOKEN=YOUR-TOKEN-GOES-HERE
+PREFIX=!majo
+
+# !majo is the default prefix, you can change it later.
+
+# note: .env is a shell file so there can't be spaces around =
+
+```
+
 ## Prefix
 
 - `!majo {command - eg. help}` is default prefix of the bot.
