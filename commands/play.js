8361fb38137acafb8908b2db7df65ebedd0b30cb
IgorKowalczyk <majonezexe@gmail.com>
2020-04-07 21:14:19 +0200

    New commands

---
+++ commands/play.js
@@ -0,0 +1,44 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+if (member.voiceChannel || guilds[message.guild.id].voiceChannel != null) {
+      if (guilds[message.guild.id].queue.length > 0 || guilds[message.guild.id].isPlaying) {
+        getID(args, function (id) {
+          addToQueue(id, message);
+          youtubeInfo(id, function (err, videoinfo) {
+            if (err) {
+              throw new Error(err);
+            }
+            guilds[message.guild.id].queueNames.push(videoinfo.title);
+            addToPlayedTracks(message, videoinfo, message.author);
+            message.reply('the song: **' + videoinfo.title + '** has been added to the queue.');
+          });
+        });
+      } else {
+        guilds[message.guild.id].isPlaying = true;
+        getID(args, function (id) {
+          guilds[message.guild.id].queue.push(id);
+          playMusic(id, message);
+          youtubeInfo(id, function (err, videoinfo) {
+            if (err) {
+              throw new Error(err);
+            }
+            guilds[message.guild.id].queueNames.push(videoinfo.title);
+            addToPlayedTracks(message, videoinfo, message.author);
+            message.reply('the song: **' + videoinfo.title + '** is now playing!');
+          });
+        });
+      }
+    } else if (member.voiceChannel === false) {
+      message.reply('you have to be in a voice channel to play music!');
+    } else {
+      message.reply('You have to be in a voice channel to play music!');
+    }
+}
+
+module.exports.help = {
+    name: "play",
+    description: "Play a music from youtube",
+    usage: "play [url/text]",
+    type: "Music"   
+}
---
+++ commands/queue.js
@@ -0,0 +1,27 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+var codeblock = '```';
+    for (let i = 0; i < guilds[message.guild.id].queueNames.length; i++) {
+      let temp = (i + 1) + '. ' + guilds[message.guild.id].queueNames[i] +
+      (i === 0 ? ' **(Current Song)**' : '') + '\n';
+      if ((codeblock + temp).length <= 2000 - 3) {
+        codeblock += temp;
+      } else {
+        codeblock += '```';
+        message.channel.send(codeblock);
+        codeblock = '```';
+      }
+    }
+
+    codeblock += '```';
+    message.channel.send(codeblock);
+  }
+  
+module.exports.help = {
+    name: "queue",
+    description: "Display the queue.",
+    usage: "queue",
+    type: "Music"   
+}
