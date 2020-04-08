e7cd2644dfc6ced6ee49c1d555e6407014a91c1d
IgorKowalczyk <majonezexe@gmail.com>
2020-04-08 13:22:40 +0200

    Update eightball.js

--- commands/eightball.js
+++ commands/eightball.js
@@ -1,14 +1,22 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  if (!args.length) return message.channel.send("thee needeth to asketh f'r a f'rtune").then(msg => msg.delete(2300)); 
+  if (!args.length) return message.channel.send({embed: {
+         color: 3447003,
+         title: "thee needeth to asketh f'r a f'rtune"
+         }})
+
 
   var fortunes = ["Yes.", "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definelty.", "You may rely on it.",
   "As I see it, yes.", "Most likely.", "Outlook good.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", 
   "Better not tell you now...", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", 
   "My sources say no.", "Outlook not so good...", "Very doubtful.",];  
   
-  await message.channel.send(fortunes[Math.floor(Math.random()*fortunes.length)]);
+  await message.channel.send({embed: {
+         color: 3447003,
+         title: fortunes[Math.floor(Math.random()*fortunes.length)]
+         }})
+
 }
 
 module.exports.help = {
