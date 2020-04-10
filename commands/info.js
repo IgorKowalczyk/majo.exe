28d5145f6f92c376d1b9c270267a085b2c06fd3f
IgorKowalczyk <majonezexe@gmail.com>
2020-04-10 19:27:51 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,6 @@ const moment = require("moment");
 require("moment-duration-format");
 
 module.exports.run = async (client, message, args) => {
-// You can change this to whatever you want, i suggest editing it.
 const prefix = process.env.PREFIX;
 const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 let embed = new Discord.RichEmbed()
@@ -13,16 +12,15 @@ let embed = new Discord.RichEmbed()
 .setDescription(`My prefix is: \`${prefix}\`\n`)
 .setThumbnail(client.user.displayAvatarURL)
 .addField('• Developer', `${cnf.owner}`, true)
-.addField('• Bot Version', `${cnf.version}`, true)
-//.addField('• Node', `${process.version}`, {code: "asciidoc"}, true)
-.addField('• Discord', `v${version}`, true)
+.addField('• Bot Version', `v${version}`, true)
+.addField('• Node', `${cnf.node}`, true)
 .addField('• Uptime', `${duration}`, true)
 .addField('• Guild Count', `${client.guilds.size}`, true)
 .addField('• User Count', `${client.users.size}`, true)
 .addField('• Channel Count', `${client.channels.size}`, true)
 .addField('• Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
-.addField('• Useful Links', `[Website](${cnf.website}) | [Support Server](${cnf.supporturl}) | [Invite me!](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id}))`, true)
-.setFooter(`Bot created by ${cnf.owner}`, true)
+.addField('• Useful Links', `[Website](${cnf.website}) | [Invite me!](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id}))`, true)
+.setFooter(`Bot created by ${cnf.owner}`, "
 message.channel.send(embed);
 }
 
@@ -32,4 +30,5 @@ module.exports.help = {
     usage: "info",
     type: "General"  
 }
+
 
