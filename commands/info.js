98e731803f904197526d483be4d9e67b9798d30c
IgorKowalczyk <majonezexe@gmail.com>
2020-04-10 18:48:19 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,5 @@
 const cnf = require('../config.json');
 const Discord = require('discord.js');
-const moment = require("moment");
 require("moment-duration-format");
 
 module.exports.run = async (client, message, args) => {
