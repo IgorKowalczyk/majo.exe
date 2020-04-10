5cdc1c39de9bf0060f70737a46f63a45768c6702
IgorKowalczyk <majonezexe@gmail.com>
2020-04-10 21:06:54 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -12,7 +12,7 @@ let embed = new Discord.RichEmbed()
 .setDescription(`My prefix is: \`${prefix}\`\n`)
 .setThumbnail(client.user.displayAvatarURL)
 .addField('• Developer', `${cnf.owner}`, true)
-.addField('• Bot Version', `v${version}`, true)
+/*.addField('• Bot Version', `v${version}`, true)
 .addField('• Node', `${cnf.node}`, true)
 .addField('• Uptime', `${duration}`, true)
 .addField('• Guild Count', `${client.guilds.size}`, true)
@@ -20,7 +20,7 @@ let embed = new Discord.RichEmbed()
 .addField('• Channel Count', `${client.channels.size}`, true)
 .addField('• Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
 .addField('• Useful Links', `[Website](${cnf.website}) | [Invite me!](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id}))`, true)
-.setFooter(`Bot created by ${cnf.owner}`)
+.setFooter(`Bot created by ${cnf.owner}`)*/
 message.channel.send(embed);
 }
 
