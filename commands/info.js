cefe1fe0f23b3ae093fe7f5b7027bbc06ee40dc5
IgorKowalczyk <majonezexe@gmail.com>
2020-04-10 18:45:53 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -10,18 +10,18 @@ const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [m
 let embed = new Discord.RichEmbed()
 .setTitle(`Information`)
 .setColor('3447003')
-.setDescription(`My prefix is: \`${prefix}\`\n`)
+.setDescription(`My prefix is: \`${prefix}\`\n`,)
 .setThumbnail(client.user.displayAvatarURL)
-.addField('• Developer', `${cnf.owner}`),
-.addField('• Bot Version', `${cnf.version}`),
-.addField('• Node', `${cnf.node}`),
-.addField('• Uptime', `${duration}'),
-.addField('• Guild Count', `${client.guilds.size}`),
-.addField('• User Count', `${client.users.size}`),
-.addField('• Channel Count', `${client.channels.size}`),
-.addField('• Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`),
-.addField('• Useful Links', `[Website](${cnf.website}) | [Support Server](${cnf.supporturl}) | [Invite me!](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id}))`),
-.setFooter(`Bot created by ${cnf.owner}`, "
+.addField('• Developer', `${cnf.owner}`,)
+.addField('• Bot Version', `${cnf.version}`,)
+.addField('• Node', `${cnf.node}`,)
+.addField('• Uptime', `${duration}`,)
+.addField('• Guild Count', `${client.guilds.size}`,)
+.addField('• User Count', `${client.users.size}`,)
+.addField('• Channel Count', `${client.channels.size}`,)
+.addField('• Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`,)
+.addField('• Useful Links', `[Website](${cnf.website}) | [Support Server](${cnf.supporturl}) | [Invite me!](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id}))`,)
+.setFooter(`Bot created by ${cnf.owner}`,)
 message.channel.send(embed);
 }
 
