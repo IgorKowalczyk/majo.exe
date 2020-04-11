7197aa3c490ed4a4c10a0b4f5febbd4db9ba17ab
IgorKowalczyk <majonezexe@gmail.com>
2020-04-11 14:24:01 +0200

    Add beter invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,17 +1,12 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  const embed = {
-  "title": "Majo.exe",
-  "description": "Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591) \n Check website: [majobot](https://igorkowalczyk.github.io)\n Join to official server [PL]: [Join](https://discord.gg/f4KtqNB)",
-  "url": "https://igorkowalczyk.github.io/majobot",
-  "color": 3447003,
-  "thumbnail": {
-    "url": "https://cdn.discordapp.com/avatars/544164729354977282/c39c2d7b39e5d6d5d13a8c2bdb010373.png?size=2048"
-  }
-};
-
-message.author.send("Invite the bot:", {embed});
+message.author.send({embed: {
+                    color: 3447003,
+                    title: "Majo.exe",
+					description: "Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591) \n Check website: [majobot](https://igorkowalczyk.github.io)\n Join to official server [PL]:"
+					}})
+message.author.send("https://discord.gg/f4KtqNB");
 
 message.channel.send({embed: {
             color: 3447003,
