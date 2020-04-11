9ae9149ad6a5f0d983b41fb10601a462cb15503f
IgorKowalczyk <majonezexe@gmail.com>
2020-04-11 15:37:23 +0200

    Invite

--- commands/invite.js
+++ commands/invite.js
@@ -2,9 +2,13 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
 
-message.author.send("Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id})");
-
-message.author.send("https://discord.gg/f4KtqNB");
+const embed = new Discord.RichEmbed()
+.setTitle("Invite the bot")
+.setColor(#09a8ca)
+.setDescription("Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize/?permissions=2146958847&scope=bot&client_id=${client.user.id})\n\n Check official [website](https://igorkowalczyk.github.io/majobot)\n\n Check official [server](https://discord.gg/f4KtqNB)")
+.setFooter("Bot created by TEST")
+.setTimestamp()
+message.channel.send({embed})
 
 message.channel.send({embed: {
             color: 3447003,
