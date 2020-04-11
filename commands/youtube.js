d3377e9fb66c7bc31c56e4474801350203b55fd3
IgorKowalczyk <majonezexe@gmail.com>
2020-04-11 21:07:45 +0200

    Add search on youtube

---
+++ commands/youtube.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (client, message, args) => {
+
+let youtube = args.slice(0).join('+');
+
+  let link = `https://www.youtube.com/results?search_query=` + youtube;
+  if (!youtube) return message.reply(`Please enter a word `)
+  if (!link) return message.reply("Console error")
+  let embed = new Discord.RichEmbed()
+    .setColor(3447003)
+         
+    .setTimestamp()
+        
+    .addField('Action:', 'Searching on youtube')
+
+    .addField("Searched word:", `${args.slice(0).join(' ')}`)
+
+    .addField('Link:', `${link}`)
+
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+    name: "youtube",
+    description: "Search on youtube",
+    usage: "search (word)",
+    type: "Fun"  
+}
