0bc1d157053d2d86d72a350b9fb4cfa81cffc0c8
IgorKowalczyk <majonezexe@gmail.com>
2020-04-11 14:28:09 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,8 +3,7 @@ const Discord = module.require("discord.js");
 module.exports.run = async (client, message, args) => {
 message.author.send({embed: {
                     color: 3447003,
-                    title: "Majo.exe",
-					description: "Invite the bot here: [invite link](https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591) \n Check website: [majobot](https://igorkowalczyk.github.io)\n Join to official server [PL]:"
+                    title: "Majo.exe\nInvite the bot here: [invite link](https://discordapp.com/oauth2/authorize?client_id=681536055572430918&scope=bot&permissions=2146958591)\nCheck official website: [majobot](https://igorkowalczyk.github.io)\nJoin to official server [PL]:",
 					}})
 message.author.send("https://discord.gg/f4KtqNB");
 
@@ -16,7 +15,7 @@ message.channel.send({embed: {
 
 module.exports.help = {
     name: "invite",
-    description: "Sends you the bot invite for Majo.exe",
+    description: "Sends the bot invite",
     usage: "invite",
     type: "General"  
 }
