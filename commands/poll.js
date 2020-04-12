6335f8dff433729e51ec39baabddef78919ae81f
IgorKowalczyk <majonezexe@gmail.com>
2020-04-12 21:26:07 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -5,21 +5,18 @@ module.exports.run = async (client, message, args, level) => { // eslint-disable
 
 
 
-const pollmessage = await args.join("");
-
-if (pollmessage.length >= 1) return message.reply({embed: {
+const pollmessage = await args.join(" ");
+if (pollmessage.length >= 0) return message.channel.send({embed: {
             color: 3447003,
             title: "You must provide a text to ask a question!"
         }})
-message.channel.send({embed: {
+
+const pollTopic =  essage.channel.send({embed: {
             color: 3447003,
-            title: ":ballot_box: " +`${message.author.username}` + " A vote has begun! React with the emojis to vote! :ballot_box:"
+            title: ":ballot_box: " +`${message.author.username}` + " A vote has begun! React with the emojis to vote! :ballot_box:",
+			description: "Poll: " + `message.content.slice(2)`
         }})
 
-const pollTopic = await message.channel.send({embed: {
-            color: 3447003,
-            title: "Poll: " + `message.content.slice(2)` + ""
-        }})
 await pollTopic.react(`✅`);
 await pollTopic.react(`⛔`);
 // Create a reaction collector
@@ -27,7 +24,7 @@ const filter = (reaction) => reaction.emoji.name === '✅';
 const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
 collector.on('end', collected => message.channel.send({embed: {
             color: 3447003,
-            title: "Collected " + `${collected.size}` + " positive votes! :tada:`"
+            title: ":tada: Collected " + `${collected.size}` + " positive votes! :tada:"
         }}));
 }
 
