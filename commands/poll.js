0633cd1c9deb2ab7aeeab15d8511d943ef1b17ca
IgorKowalczyk <majonezexe@gmail.com>
2020-04-12 19:36:33 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -10,7 +10,10 @@ message.channel.send({embed: {
             title: ":ballot_box: " +`${message.author.username}` + "A vote has begun! React with the emojis to vote! :ballot_box:"
         }})
 
-const pollTopic = await message.channel.send(message.content.slice(2));
+const pollTopic = await message.channel.send({embed: {
+            color: 3447003,
+            title: `message.content.slice(2)`
+        }})
 await pollTopic.react(`✅`);
 await pollTopic.react(`⛔`);
 // Create a reaction collector
@@ -19,8 +22,8 @@ const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
 
 const filter2 = (reaction) => reaction.emoji.name === '✅';
 const collector2 = pollTopic.createReactionCollector(filter2, { time: 15000 });
-collector.on('end', collected => message.channel.send(`${collected.size}/${collected2.size}`));
 collector2.on('end', collected => message.channel.send(`${collected.size}/${collected2.size}`));
+collector.on('end', collected => message.channel.send(`${collected.size}/${collected2.size}`));
 }
 
 
