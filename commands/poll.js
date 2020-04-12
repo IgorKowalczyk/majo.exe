2a44c1b5faf71721c6e0c714b84a139d485d23ed
IgorKowalczyk <majonezexe@gmail.com>
2020-04-12 18:56:30 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,17 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-if (!args) return message.reply("You have to have something to vote on! LOL")
-if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
-message.channel.send(`:ballot_box:  ${message.author.username} A vote has begun! React with the emojis to vote! :ballot_box: `);
+if (!args) return message.reply({embed: {
+            color: 3447003,
+            title: "You must provide a text to ask a question!"
+        }})
+message.channel.send({embed: {
+            color: 3447003,
+            title: ":ballot_box: " +`${message.author.username}` + "A vote has begun! React with the emojis to vote! :ballot_box:"
+        }})
+
 const pollTopic = await message.channel.send(message.content.slice(2));
 await pollTopic.react(`✅`);
 await pollTopic.react(`⛔`);
 // Create a reaction collector
 const filter = (reaction) => reaction.emoji.name === '✅';
 const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
-collector.on('collect', r => message.channel.send(`Collected ${r.emoji.name}`));
-collector.on('end', collected => message.channel.send(`Collected ${collected.size} items`));
+
+const filter2 = (reaction) => reaction.emoji.name === '✅';
+const collector2 = pollTopic.createReactionCollector(filter2, { time: 15000 });
+collector.on('end', collected => message.channel.send(`${collected.size}/${collected2.size}`));
 }
 
 
