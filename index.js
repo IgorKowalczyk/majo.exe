efcfe2ed686392e3274b72d4f2eb0ff34112bda1
IgorKowalczyk <majonezexe@gmail.com>
2020-04-13 14:24:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -102,11 +102,11 @@ client.on("guildMemberRemove", (member) => {
 
 client.on('message', message=> {
     if (message.isMentioned(client.user.id)) {
-	return;
     message.channel.send({embed: {
             color: 3447003,
             title: "I can only respond to message with my prefix (" + `${prefix}` + ")"
         }})
+	return;
 }
 });
 
