354d2c1b456601aea62e2286bb8d3fc9415fad79
IgorKowalczyk <majonezexe@gmail.com>
2020-04-13 14:27:37 +0200

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -6,8 +6,14 @@ module.exports.run = async (client, message, args) => {
 let youtube = args.slice(0).join('+');
 
   let link = `https://www.youtube.com/results?search_query=` + youtube;
-  if (!youtube) return message.reply(`Please enter a word `)
-  if (!link) return message.reply("Console error")
+  if (!youtube) return message.channel.send({embed: {
+            color: 3447003,
+            title: "Please enter a word to search!"
+        }})
+  if (!link) return message.channel.send({embed: {
+            color: 3447003,
+            title: "Error!"
+        }})
   let embed = new Discord.RichEmbed()
     .setTitle("Searching in youtube")
     .setColor(3447003)
