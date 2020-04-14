31aa9ce75d47b9c7500ab3935d69c9215f47139f
IgorKowalczyk <majonezexe@gmail.com>
2020-04-14 11:37:10 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -8,19 +8,20 @@ if (pollmessage.length <= 0) return message.channel.send({embed: {
             color: 3447003,
             title: "You must provide a text to ask a question!"
         }})
-const embed = new Discord.RichEmbed()
+const embed = new Discord.RichEmbed() {
 .setTitle(":ballot_box: " +`${message.author.username}` + " A vote has begun! React with the emojis to vote! :ballot_box:")
 .setColor(3447003)
-.addField("Pool", `pollmessage`)
-.setFooter("The vote will be ended in 15 seconds! • Bot created by" + `${cnf.owner}`)
+.addField("Pool", "Poll: " + `pollmessage`)
+.setFooter("The vote will be ended in 30 seconds! • Bot created by" + `${cnf.owner}`)
 .setTimestamp()
+};
 const pollTopic = await message.channel.send({embed})
 
 await pollTopic.react(`✅`);
 await pollTopic.react(`⛔`);
 // Create a reaction collector
 const filter = (reaction) => reaction.emoji.name === '✅';
-const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
+const collector = pollTopic.createReactionCollector(filter, { time: 30000 });
 collector.on('end', collected => message.channel.send({embed: {
             color: 3447003,
             title: ":tada: Collected " + `${collected.size}` + " positive votes! :tada:"
