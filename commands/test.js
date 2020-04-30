1f5caf4fb062f14a52be569f6c90ec8f82aa4330
IgorKowalczyk <majonezexe@gmail.com>
2020-04-30 13:25:49 +0200

    .

--- commands/test.js
+++ commands/test.js
@@ -1,30 +1,23 @@
 const {MessageEmbed} = require('discord.js')
-const ms = require('ms');
+const api = require("imageapi.js")
 module.exports={
-    name: 'giveaway',
-    description: 'Create a simple giveaway',
-    usage: '<time> <channel> <prize>',
-    category: 'fun',
+    name: "meme",
+    description: "Get a meme!",
+    category: "fun",
     run: async(bot,message,args)=>{
-        if(!args[0]) return message.channel.send(`You did not specify your time!`)
-        if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return message.channel.send(`You did not use the correct formatting for the time!`)
-        if(isNaN(args[0][0])) return message.channel.send(`That is not a number!`)
-        let channel = message.mentions.channels.first()
-        if(!channel) return message.channel.send(`I could not find that channel in the guild!`)
-        let prize = args.slice(2).join(" ")
-        if(!prize) return message.channel.send(`No prize specified!`)
-        message.channel.send(`*Giveaway created in ${channel}*`)
-        let Embed = new MessageEmbed()
-        .setTitle(`New giveaway!`)
-        .setDescription(`The user ${message.author} is hosting a giveaway for the prize of **${prize}**`)
-        .setTimestamp(Date.now()+ms(args[0]))
-        .setColor(`BLUE`)
-        let m = await channel.send(Embed)
-        m.react("🎉")
-        setTimeout(() => {
-            if(m.reactions.cache.size<=1) return message.channel.send(`Not enough people reacted for me to start draw a winner!`)
-            let winner = m.reactions.cache.get("🎉").users.cache.filter(u=>!u.bot).random()
-            channel.send(`The winner of the giveaway for **${prize}** is... ${winner}`)
-        }, ms(args[0]));
+        let subreddits = [
+            "comedyheaven",
+            "dank",
+            "meme",
+            "memes"
+        ]
+        let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length)-1)]
+        let img = await api(subreddit)
+        const Embed = new MessageEmbed()
+        .setTitle(`A meme from r/${subreddit}`)
+        .setURL(`https://reddit.com/r/${subreddit}`)
+        .setColor('RANDOM')
+        .setImage(img)
+        message.channel.send(Embed)
     }
 }
