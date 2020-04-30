c264c8a2dda56ff28fbbf314f8f5f18b2bd66b5e
IgorKowalczyk <majonezexe@gmail.com>
2020-04-30 19:06:16 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,12 @@ module.exports.run = async (client, message, args) => {
 	
         await message.channel.send(`🏓 Pinging....`).then(msg=>{
         const _ = new Discord.MessageEmbed()
-        color: 3447003,
-        title: "Test",
-		description: "Bot ping: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nAPI ping: ${Math.round(client.ws.ping)}ms"
+        .setColor('RANDOM')
+        .setTitle: ":ping_pong: Pong!",
+		.setDescription: "Bot ping: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nAPI ping: ${Math.round(client.ws.ping)}ms"
         msg.edit(_);
         msg.edit("\u200B")
+
     })
 }
 
