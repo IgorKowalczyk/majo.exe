8d1a2cfc7e3da0687f8759c3b7baf8a65a3bf582
IgorKowalczyk <majonezexe@gmail.com>
2020-04-30 13:30:41 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,23 +1,23 @@
 const {MessageEmbed} = require('discord.js')
 const api = require("imageapi.js")
 module.exports={
-    name: "meme",
-    description: "Get a meme!",
+    name: "reddit",
+    description: "Get a meme from a subreddit of your choice!",
     category: "fun",
+    usage: "<subreddit>",
     run: async(bot,message,args)=>{
-        let subreddits = [
-            "comedyheaven",
-            "dank",
-            "meme",
-            "memes"
-        ]
-        let subreddit = subreddits[Math.floor(Math.random()*(subreddits.length)-1)]
-        let img = await api(subreddit)
-        const Embed = new MessageEmbed()
-        .setTitle(`A meme from r/${subreddit}`)
-        .setURL(`https://reddit.com/r/${subreddit}`)
-        .setColor('RANDOM')
-        .setImage(img)
-        message.channel.send(Embed)
+        let Subreddit = message.content.slice(8)
+        if(!Subreddit)return message.channel.send(`You did not specify your subreddit!`)
+        try{
+            let img = await api(Subreddit)
+            const Embed = new MessageEmbed()
+            .setTitle(`A random image from r/${Subreddit}`)
+            .setColor('RANDOM')
+            .setImage(img)
+            .setURL(`https://reddit.com/r/${Subreddit}`)
+            message.channel.send(Embed)
+        }catch(err){
+             message.channel.send(err)
+        }
     }
 }
