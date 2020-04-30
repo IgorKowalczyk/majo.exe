6296146183aabe52b32f4a9953225bd070898720
IgorKowalczyk <majonezexe@gmail.com>
2020-04-30 12:38:14 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,15 @@ const {MessageEmbed} = require('discord.js')
 const ms = require('ms');
 
 module.exports.run = async(client,message,args)=>{
-	    let u = message.guild.members;
+  var members = [];
+  
+  message.guild.members.forEach((member) => {
+    if (!member.user.bot)
+      members.push(member);  
+  });
+  
+  var winner = members[Math.floor(Math.random() * members.length)];
+  
         if(!args[0]) return message.channel.send(`You did not specify your time!`)
         if(!args[0].endsWith("d")&&!args[0].endsWith("h")&&!args[0].endsWith("m")) return message.channel.send(`You did not use the correct formatting for the time!`)
         if(isNaN(args[0][0])) return message.channel.send(`That is not a number!`)
