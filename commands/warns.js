7a0aa0e05adfa74d3811e4ef4a5dffd28dc0264c
IgorKowalczyk <majonezexe@gmail.com>
2020-05-01 14:16:28 +0200

    Update warns.js

--- commands/warns.js
+++ commands/warns.js
@@ -5,12 +5,12 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (client, message, args) => {
 
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
   };
   
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them yo");
   let warnlevel = warns[wUser.id].warns;
 
