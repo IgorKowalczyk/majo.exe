99e31f9c93a4016d2633acc04efc302bf2063cec
IgorKowalczyk <majonezexe@gmail.com>
2020-05-01 14:04:53 +0200

    Add better file wrtie

--- commands/warn.js
+++ commands/warn.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("../warnings.json", "utf8"));
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (client, message, args) => {
 
@@ -18,7 +18,7 @@ module.exports.run = async (client, message, args) => {
 
   warns[wUser.id].warns++;
 
-  fs.writeFile("../warnings.json", JSON.stringify(warns), (err) => {
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
     if (err) console.log(err)
   });
 
--- commands/warns.js
+++ commands/warns.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("../warnings.json", "utf8"));
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (client, message, args) => {
 
