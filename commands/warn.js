fbadb73fe45e312e95e4fa72c7fae60caade452f
IgorKowalczyk <majonezexe@gmail.com>
2020-05-01 14:25:59 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,10 +8,20 @@ module.exports.run = async (client, message, args) => {
   //!warn @daeshan <reason>
   //if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
+  if(!wUser) return message.channel.send({embed: {
+            color: 3447003,
+            title: "I can't find the user!"
+        }})
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send({embed: {
+            color: 3447003,
+            title: "I can't warn this user!"
+        }})
   let reason = args.join(" ").slice(22);
 
+  if (!reason) return message.channel.send({embed: {
+            color: 3447003,
+            title: "Please enter a reason!"
+        }})
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
   };
@@ -23,37 +33,14 @@ module.exports.run = async (client, message, args) => {
   });
 
   let warnEmbed = new Discord.RichEmbed()
-  .setDescription("Warns")
-  .setAuthor(message.author.username)
-  .setColor("#fc6400")
-  .addField("Warned User", `<@${wUser.id}>`)
-  .addField("Warned In", message.channel)
-  .addField("Number of Warnings", warns[wUser.id].warns)
-  .addField("Reason", reason);
-
-  let warnchannel = message.guild.channels.find(`name`, "incidents");
-  if(!warnchannel) return message.reply("Couldn't find channel");
-
-  warnchannel.send(warnEmbed);
-
-  if(warns[wUser.id].warns == 2){
-    let muterole = message.guild.roles.find(`name`, "muted");
-    if(!muterole) return message.reply("You should create that role dude.");
-
-    let mutetime = "10s";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`<@${wUser.id}> has been temporarily muted`);
-
-    setTimeout(function(){
-      wUser.removeRole(muterole.id)
-      message.reply(`<@${wUser.id}> has been unmuted.`)
-    }, ms(mutetime))
-  }
-  if(warns[wUser.id].warns == 3){
-    message.guild.member(wUser).ban(reason);
-    message.reply(`<@${wUser.id}> has been banned.`)
-  }
-
+  .setTitle("Warns")
+  .setColor("RANDOM")
+  .addField("Warned User:", `<@${wUser.id}>`)
+  .addField("Warned In:", message.channel)
+  .addField("Number of Warnings:", warns[wUser.id].warns)
+  .addField("Reason:", reason);
+  .setTimestamp()
+  message.channel.send(warnEmbed);
 }
 
 module.exports.help = {
