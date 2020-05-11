e28440f7496ec82eb5541273c67bcce61a2c9fb5
IgorKowalczyk <majonezexe@gmail.com>
2020-05-11 14:05:04 +0200

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -6,7 +6,7 @@ const superagent = require("snekfetch");
             const user = message.mentions.users.first();
             if(!user) return message.channel.send({embed: {
                 color: 16734039,
-                title: "You must mention someone to slap!"
+                title: "You must mention someone to slap!!!"
             }});
 
             superagent.get('https://nekos.life/api/v2/img/slap')
