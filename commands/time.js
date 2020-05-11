b4195b389fc621ff79e973cda49d76ea4cf4ec09
IgorKowalczyk <majonezexe@gmail.com>
2020-05-11 09:34:50 +0200

    Add time commands

---
+++ commands/time.js
@@ -0,0 +1,23 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, level) => {
+
+var today = new Date()
+let Day = today.toString().split(" ")[0].concat("day");
+let Month = today.toString().split(" ")[1]
+let Year = today.toString().split(" ")[3]
+const embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.addField("Today is:", `${Day}` + ", " + `${Month}` + ", " + `${Year}`)
+.addField("Time of day:", `${today.toString().split(" ")[4]}`)
+message.channel.send({ embed })
+
+}
+
+module.exports.help = {
+    name: "time",
+    description: "Display a actual time",
+    usage: "time",
+    type: "Utility"   
+}
