22ce24613349e8c44468b1a34b0efdeeb320dc56
IgorKowalczyk <majonezexe@gmail.com>
2020-05-12 12:22:30 +0200

    Create erofeet.js

---
+++ commands/erofeet.js
@@ -0,0 +1,33 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args, level) => {
+     if (!message.channel.nsfw) {
+		message.react('💢');
+		return message.channel.send({embed: {
+                color: 16734039,
+                title: "You can use this command in an NSFW Channel!"
+            }})
+	}
+    superagent.get('https://nekos.life/api/v2/img/erofeet')
+        .end((err, response) => {
+      const lewdembed = new Discord.RichEmbed()
+      .setTitle("Hentai")
+      .setImage(response.body.url)
+      .setColor(`RANDOM`)
+      .setFooter(`Tags: erofeet`)
+      .setURL(response.body.url);
+  message.channel.send(lewdembed);
+    }).catch((err) => message.channel.send({embed: {
+                color: 16734039,
+                title: "Something went wrong... :cry:"
+            }}));
+	
+}
+
+module.exports.help = {
+    name: "erofeet",
+    description: "Display a random erofeet image/gif",
+    usage: "erofeet",
+    type: "NSFW" 
+}
