25bc913f70c330a40c4e8e729d8c9348e593a1ac
IgorKowalczyk <majonezexe@gmail.com>
2020-05-12 12:18:59 +0200

    Create ero.js

---
+++ commands/ero.js
@@ -0,0 +1,33 @@
+const superagent = require("snekfetch");
+const Discord = require('discord.js')
+
+exports.run = async (client, message, args, level) => {
+     if (!message.channel.nsfw) {
+		message.react('💢');
+		return message.channel.send({embed: {
+                color: 16734039,
+                title: "You can use this command in an NSFW Channel!"
+            }})
+	}
+    superagent.get('https://nekos.life/api/v2/img/ero')
+        .end((err, response) => {
+      const lewdembed = new Discord.RichEmbed()
+      .setTitle("Hentai")
+      .setImage(response.body.url)
+      .setColor(`RANDOM`)
+      .setFooter(`Tags: ero`)
+      .setURL(response.body.url);
+  message.channel.send(lewdembed);
+    }).catch((err) => message.channel.send({embed: {
+                color: 16734039,
+                title: "Something went wrong... :cry:"
+            }}));
+	
+}
+
+module.exports.help = {
+    name: "ero",
+    description: "Display a random ero image/gif",
+    usage: "ero",
+    type: "NSFW" 
+}
