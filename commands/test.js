e6b4c27175c59927d1f3bbf02b6fc1b389832b63
IgorKowalczyk <majonezexe@gmail.com>
2020-05-15 11:03:33 +0200

    test

--- commands/test.js
+++ commands/test.js
@@ -1,64 +1,25 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (client, message, args) => {
-
-console.log(`${message.member} ran the "GiveAway" command`);
-
-var item = "";
-var time;
-var winnerCount
-let messageArray = message.content.split(" ");
-//-- giveaway 1(winnerCount) 60(seconds) Item Giveaway
-
-for (var i = 3; i < messageArray.length; i++){
-  item += (messageArray[i] + " ");
-}
-winnerCount = Number(messageArray[1]);
-time = Number(messageArray[2]);
-
-
-var giveEmbed = new Discord.RichEmbed();
-giveEmbed.setDescription(item);
-var embedSent = await message.channel.send(giveEmbed);
-embedSent.react("🎉");
-setTimeout(function() {
-  var peopleReacted = embedSent.reactions.get("🎉");
-  var winners = [];
-
-  // Checks if fewer people reacted than the winnerCount allows users to win
-  if (peopleReacted.length >= winnerCount) {
-    winners = peopleReacted;
-  } else {
-    // Gets as many random users from the peopleReacted as winnerCount allows users to win
-    for (var i = 0; i < winnerCount; i++){
-      var index = Math.floor(Math.random() * peopleReacted.length);
-      winners.push(peopleReacted[index]);
-      // After adding a user to winners, remove that item from the array to prevent him from winning multiple times
-      peopleReacted.splice(index, 1);
+ 
+ const Discord = require("discord.js"),
+ const { GiveawaysManager } = require("discord-giveaways");
+// Starts updating currents giveaways
+const manager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
+        embedColor: "#FF0000",
+        reaction: "🎉"
     }
-  }
+});
+// We now have a giveawaysManager property to access the manager everywhere!
+client.giveawaysManager = manager;
 
-  var winnerMsg = "User(s) ";
-  for (var i = 0; i < winners.length; i++){
-    // Add each winner to the winnerMsg
-    winnerMsg += (winners[i].toString() + ", ");
-  }
-
-  var haveHas;
-  if (winners.length === 1){
-    haveHas = "has";
-  }
-  else {
-    haveHas = "have";
-  }
-  message.channel.send(`${winnerMsg} ${haveHas} won ${item}`);
-})
-}
-
-module.exports.help = {
-    name: "test",
-    description: "Create a giveaway",
-    usage: "giveaway <winnerCount> <seconds> <item>",
-    type: "Fun" 
-}
-
+ const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+ client.giveawaysManager.start(message.channel, {
+            time: ms(args[0]),
+            prize: args.slice(2).join(" "),
+            winnerCount: parseInt(args[1])
+        }).then((gData) => {
+            console.log(gData); // {...} (messageid, end date and more)
+        });
