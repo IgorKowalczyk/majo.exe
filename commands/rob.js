46baa96ff5a820c6a3a47ed2191461314c761ec4
IgorKowalczyk <majonezexe@gmail.com>
2020-05-15 17:09:18 +0200

    Add better rob

--- commands/rob.js
+++ commands/rob.js
@@ -1,66 +1,39 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
+const db = require('quick.db')
+const Discord = require('discord.js')
 const ms = require("parse-ms");
 
 module.exports.run = async (client, message, args) => {
-let user = message.mentions.members.first()
-if (!user) return;
+    let user = message.author;
+    let author = await db.fetch(`rob_${message.guild.id}_${user.id}`)
 
-let targetuser = await db.fetch(`money_${message.guild.id}_${user.id}`);
-let author = await db.fetch(`rob_${message.guild.id}_${user.id}`);
-let author2 = await db.fetch(`money_${message.guild.id}_${user.id}`);
-let random = random = Math.floor(Math.random() * 100) + 1;
+    let timeout = 600000;
+    
+    if (author !== null && timeout - (Date.now() - author) > 0) {
+        let time = ms(timeout - (Date.now() - author));
+    
+        let timeEmbed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(`:x:  You have already robbed someone\n\nTry again in ${time.minutes}m ${time.seconds}s `);
+        message.channel.send(timeEmbed)
+      } else {
 
-if(author2 === 0) return;
-if(author === 0) return;
-
-let timeout = 600000;
-
-if (author !== null && timeout - (Date.now() - author) > 0) {
-    let time = ms(timeout - (Date.now() - author));
-
-    let timeEmbed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setDescription(`:cross: You have already robbed someone\n\nTry again in ${time.minutes}m ${time.seconds}s `);
-    message.channel.send(timeEmbed)
-  } else {
-
-let moneyEmbed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setDescription(`:cross: You need atleast 200 coins in your wallet to rob someone`);
-
-if (author2 < 200) {
-    return message.channel.send(moneyEmbed)
-
-}
-let moneyEmbed2 = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setDescription(`:cross: ${user.user.username} does not have anything you can rob`);
-if (targetuser < 0) {
-    return message.channel.send(moneyEmbed2)
+        let amount = Math.floor(Math.random() * 80) + 1;
+        let embed1 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription(`:check: You robbed a someone a and earned ${amount} coins`);
+        message.channel.send(embed1)
+        
+        db.add(`money_${message.guild.id}_${user.id}`, amount)
+        db.set(`rob_${message.guild.id}_${user.id}`, Date.now())
+    };
 }
 
 
-
-let vip = await db.fetch(`bronze_${user.id}`)
-if(vip === true) random = Math.floor(Math.random() * 200) + 1;
-if (vip === null) random = Math.floor(Math.random() * 100) + 1;
-
-let embed = new Discord.RichEmbed()
-.setDescription(`<:Check:618736570337591296> You robbed ${user} and got away with ${random} coins`)
-.setColor("RANDOM")
-message.channel.send(embed)
-
-db.subtract(`money_${message.guild.id}_${user.id}`, random)
-db.add(`money_${message.guild.id}_${user.id}`, random)
-db.set(`rob_${message.guild.id}_${user.id}`, Date.now())
-  
-};
-}
 
 module.exports.help = {
-    name: "rob",
-    description: "Rob mentioned user",
-    usage: "rob <mention>",
-    type: "Economy" 
+    name: "work",
+    description: "Work as a Programmer, Builder, Waiter, Busboy, Chied, Mechanic and get money",
+    usage: "work",
+    type: "Economy"  
 }
+
