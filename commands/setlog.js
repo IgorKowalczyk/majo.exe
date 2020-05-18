664a6e44880f139e28930df67a08606bf887914f
IgorKowalczyk <majonezexe@gmail.com>
2020-05-18 19:30:33 +0200

    Create setlog.js

---
+++ commands/setlog.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = (client, message, args) => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("Sorry, but you don't have acces to set the logs.");
+    let log = message.guild.channels.find("name", "log")  
+    if(log) return message.reply("**The channel #log is already exist**")   
+    if(!log) {   
+    message.guild.createChannel("log", "text").then(c=> {  
+        c.overwritePermissions(message.guild.id, {  
+            SEND_MESSAGES: false
+    })
+})
+message.channel.send("**? The channel has been created and the channel logs is #logs**")
+    }
