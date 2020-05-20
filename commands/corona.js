0f67db95ee7de39a26e06f40ad9951385a1490cd
IgorKowalczyk <majonezexe@gmail.com>
2020-05-20 10:35:51 +0200

    Create corona.js

---
+++ commands/corona.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js")
+const { NovelCovid } = require("novelcovid");
+const track = new NovelCovid();
+
+module.exports.run = async (client, message, args) => {
+    
+    if(!args.length) {
+      return message.channel.send("Please give the name of country")
+    }
+    
+    if(args.join(" ") === "all") {
+      let corona = await track.all() //it will give global cases
+      
+      let embed = new Discord.RichEmbed()
+      .setTitle("Global Cases")
+      .setColor("#ff2050")
+      .setDescription("Sometimes cases number may differ from small amount.")
+      .addField("Total Cases", corona.cases, true)
+      .addField("Total Deaths", corona.deaths, true)
+      .addField("Total Recovered", corona.recovered, true)
+      .addField("Today's Cases", corona.todayCases, true)
+      .addField("Today's Deaths", corona.todayDeaths, true)
+      .addField("Active Cases", corona.active, true);
+      
+      return message.channel.send(embed)
+      
+      
+      
+    } else {
+      let corona = await track.countries(args.join(" ")) //change it to countries
+      
+      let embed = new discord.RichEmbed()
+      .setTitle(`${corona.country}`)
+      .setColor("#ff2050")
+      .setDescription("Sometimes cases number may differ from small amount.")
+      .addField("Total Cases", corona.cases, true)
+      .addField("Total Deaths", corona.deaths, true)
+      .addField("Total Recovered", corona.recovered, true)
+      .addField("Today's Cases", corona.todayCases, true)
+      .addField("Today's Deaths", corona.todayDeaths, true)
+      .addField("Active Cases", corona.active, true);
+      
+      return message.channel.send(embed)
+      
+      
+    }
+    
+    
+  }
+  
+module.exports.help = {
+    name: "corona",
+    description: "Get the stats of corona VIRUS",
+    usage: "corona <all>/<country>",
+    type: "Utility"  
+}
