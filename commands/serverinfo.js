95821bd08235605d5d115496330eadb5947c8986
IgorKowalczyk <majonezexe@gmail.com>
2020-05-29 11:13:40 +0200

    Better serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,16 +7,44 @@ module.exports.run = async (client, message, args) => {
   
   var e = message.guild.emojis.map(e => e.toString())
 
-  var embed = new Discord.RichEmbed()
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
+    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+    let region = {
+        "brazil": ":flag_br: Brazil",
+        "eu-central": ":flag_eu: Central Europe",
+        "singapore": ":flag_sg: Singapore",
+        "us-central": ":flag_us: U.S. Central",
+        "sydney": ":flag_au: Sydney",
+        "us-east": ":flag_us: U.S. East",
+        "us-south": ":flag_us: U.S. South",
+        "us-west": ":flag_us: U.S. West",
+        "eu-west": ":flag_eu: Western Europe",
+        "vip-us-east": ":flag_us: VIP U.S. East",
+        "london": ":flag_gb: London",
+        "amsterdam": ":flag_nl: Amsterdam",
+        "hongkong": ":flag_hk: Hong Kong",
+        "russia": ":flag_ru: Russia",
+        "southafrica": ":flag_za:  South Africa"
+    };
+    const embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL)
     .setColor("RANDOM")
     .setThumbnail(message.guild.iconURL)
     .addField("Server Owner", serverowner.user.username + "#" + serverowner.user.discriminator, true)
-    .addField("Members", message.guild.memberCount, true)
-    .addField("Server Region", message.guild.region, true)
+	.addField("ID", message.guild.id, true)
+    .addField("Region", region[message.guild.region], true)
+    .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.client).size} | ${message.guild.members.filter(member => member.user.client).size}`, true)
+    .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
+    .addField("Channels", message.guild.channels.size, true)
     .addField("Roles [" + `${message.guild.roles.size}` + "]", message.guild.roles.filter(r => r.id !== message.guild.id).map(roles => roles.name).join(", ") || "No Roles", true)
 	.addField("Emojis [" + `${message.guild.emojis.size}` + "]", e.join(", ") || "No Emojis", true)
-    .setTimestamp()
+    .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+	.setTimestamp()
   
   await message.channel.send(embed=embed);
 }
