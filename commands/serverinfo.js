e7675cb11b39e8a7c3365c7070b29e8a7778791a
IgorKowalczyk <majonezexe@gmail.com>
2020-05-29 10:50:27 +0200

    Add list of all emojus

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,6 +5,13 @@ module.exports.run = async (client, message, args) => {
   let serverowner = message.guild.member(message.guild.ownerID);
   var roles = [];
   
+  var e = message.guild.emojis.map(e => e.toString())
+  if (e < 0) {
+	  e = "0"
+  }
+  
+  if (e > 10) {
+	 e = "More than you know"
   var embed = new Discord.RichEmbed()
     .setAuthor(message.guild.name, message.guild.iconURL)
     .setColor("RANDOM")
@@ -12,7 +19,8 @@ module.exports.run = async (client, message, args) => {
     .addField("Server Owner", serverowner.user.username + "#" + serverowner.user.discriminator, true)
     .addField("Members", message.guild.memberCount, true)
     .addField("Server Region", message.guild.region, true)
-    .addField("Roles", message.guild.roles.filter(r => r.id !== message.guild.id).map(roles => roles.name).join(", ") || "No Roles", true)    
+    .addField("Roles [" + `${message.guild.roles.size}` + "]", message.guild.roles.filter(r => r.id !== message.guild.id).map(roles => roles.name).join(", ") || "No Roles", true)
+	.addField("Emojis [" + `${message.guild.emojis.size}` + "]", e, true)
     .setTimestamp()
   
   await message.channel.send(embed=embed);
