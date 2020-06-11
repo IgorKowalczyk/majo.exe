097dbfa7f8bd03e35973127601b8f954e3bb8f8d
IgorKowalczyk <majonezexe@gmail.com>
2020-06-11 16:52:10 +0200

    Some small changes

--- commands/github.js
+++ commands/github.js
@@ -6,28 +6,25 @@ module.exports.run = async (client, message, args) => {
   const user = args.join(" ");
   
   if (!user || user.length < 0) {
-	return;
     let errorembeduser = new Discord.RichEmbed()
       .setTitle("Error :cry:")
       .addField("Usage", `${prefix}` + " github <username>")
       .setTimestamp()
-    message.channel.send(errorembeduser);
+    return message.channel.send(errorembeduser);
   }
   const data = await fetch(`https://api.github.com/users/${user}`).then((res) =>
     res.json()
   )
   .catch(error => {
-	return;
     let errorembed = new Discord.RichEmbed()
     .setTitle("Error :cry:")
     .setDescription("Could not fetch data. Please try again or make sure the name is correctly spelled!")
     .setTimestamp();
-  message.channel.send(errorembed);
+  return message.channel.send(errorembed);
   })
 
   
   const repos = `https://github.com/${data.login}/repositories`;
-  const blog = data.blog === null ? "Not specified." : data.blog
   const embed = new Discord.RichEmbed()
     .setTitle(`${data.login === null ? "Not specified." : data.login}`)
     .setURL(`https://github.com/${data.login}`)
@@ -36,7 +33,6 @@ module.exports.run = async (client, message, args) => {
     .addField("Bio", `${data.bio === null ? "Not specified." : data.bio}`)
     .addField(`Repositories (${data.public_repos})`, `[\`${repos}\`](${repos})`)
     .setFooter("Powered by Github API")
-    .addField("Website", `${blog}`)
     .addField("Followers", data.followers, true)
     .addField("Following", data.following, true)
     .setTimestamp()
