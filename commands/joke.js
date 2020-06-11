f623b58b770a337cd3301cfe8c67b36f8ead2632
IgorKowalczyk <majonezexe@gmail.com>
2020-06-11 21:11:57 +0200

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -2,14 +2,20 @@ const superagent = require('superagent');
 const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
+
   const { body } = await superagent
         .get('http://icanhazdadjoke.com/')
         .set('Accept', 'application/json');
+		.end((err, response) => {
         let jEmbed = new Discord.RichEmbed()
         .setTitle("Joke")
         .setDescription(body.joke)
-        .setColor("#cdf785");
+        .setColor("RANDOM");
         message.channel.send(jEmbed);
+		}).catch((err) => message.channel.send({embed: {
+                color: 16734039,
+                title: "Something went wrong... :cry:"
+            }}));
     }
 
 module.exports.help = {
