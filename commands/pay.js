6ed73e8d15d26521897c829c0afe8d561129afa7
IgorKowalczyk <majonezexe@gmail.com>
2020-06-13 21:38:41 +0200

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -9,28 +9,34 @@ module.exports.run = async (client, message, args) => {
 
   let embed1 = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setDescription(`:cross: Mention someone to pay`);
+  .setDescription(`:x: Mention someone to pay`);
 
   if (!user) {
       return message.channel.send(embed1)
   }
   let embed2 = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setDescription(`:cross: Specify an amount to pay`);
+  .setDescription(`:x: Specify an amount to pay`);
   
   if (!args[1]) {
       return message.channel.send(embed2)
   }
+
+    if (isNaN(args[1])) return message.channel.send({embed: {
+                    color: 16734039,
+                    description: "You must enter the amount of money to pay!"
+                }})
+
   let embed3 = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setDescription(`:cross: You can't pay someone negative money`);
+  .setDescription(`:x: You can't pay someone negative money`);
 
   if (message.content.includes('-')) { 
       return message.channel.send(embed3)
   }
   let embed4 = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setDescription(`:cross: You don't have that much money`);
+  .setDescription(`:x: You don't have that much money`);
 
   if (member < args[1]) {
       return message.channel.send(embed4)
