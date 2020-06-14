3b0947abf3bf3d555acb1535160ae21d9d6df4de
IgorKowalczyk <majonezexe@gmail.com>
2020-06-14 21:12:48 +0200

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -2,35 +2,23 @@ const Discord = require('discord.js');
 const yts = require( 'yt-search' )
 
 module.exports.run = async (client, message, args) => {
-yts(args.join(' '), function ( err, r ) {
-  if ( err ) {
-	return message.channel.send({embed: {
-            color: 16734039,
-            description: "Something went wrong... :cry:"
-        }})
-  }
-     if (!args[0]) return message.channel.send({embed: {
-            color: 16734039,
-            description: "Please enter a word to search!"
-        }})
-    let msg = message.channel.send({embed: {
-            color: 16734039,
-            description: "🔎 Searching on Youtube..."
-        }})
+
+yts( 'superman theme', function ( err, r ) {
   const videos = r.videos
-  videos.forEach( function ( v ) {
-    const views = String( v.views ).padStart( 10, ' ' )
-	let embed = new Discord.RichEmbed()
-      .setTitle("🔎 Youtube Search results:")
-      .setDescription(`${ views } | ${ v.title } (${ v.timestamp }) | ${ v.author.name }`)
-      .setColor('RANDOM');
-      msg.edit(embed);
+  const playlists = r.playlists || r.lists
+  const channels = r.accounts || r.channels
+
+  videos.forEach( function ( video ) {
+    const views = String( video.views ).padStart( 10, ' ' )
+    const title = video.title
+    const timestamp = video.timestamp
+    const seconds = video.seconds
+    console.log( `${ views } | ${ title } (${ timestamp }) | ${ video.author.name }` )
   } )
 } )
+
+
 }
-
-
-
 
 module.exports.help = {
     name: "youtube",
