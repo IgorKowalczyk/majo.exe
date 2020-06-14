78b1a86cd7e88c70bc69734ac29066337a97cee1
IgorKowalczyk <majonezexe@gmail.com>
2020-06-14 21:18:31 +0200

    Add better view

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,19 +1,28 @@
 const Discord = require('discord.js');
-const yts = require( 'yt-search' )
+const yts = require( 'yt-search')
 
 module.exports.run = async (client, message, args) => {
 
-yts( 'superman theme', function ( err, r ) {
+yts(args[0], function ( err, r ) {
+
+     if (!args[0]) return message.channel.send({embed: {
+            color: 16734039,
+            description: "Please enter a word to search!"
+        }})
+
   const videos = r.videos
   const playlists = r.playlists || r.lists
   const channels = r.accounts || r.channels
-
   videos.forEach( function ( video ) {
-    const views = String( video.views ).padStart( 10, ' ' )
+    const views = String( video.views ).padStart( 5, ' ' )
     const title = video.title
     const timestamp = video.timestamp
     const seconds = video.seconds
-    console.log( `${ views } | ${ title } (${ timestamp }) | ${ video.author.name }` )
+   let embed = new Discord.RichEmbed()
+      .setTitle("🔎 Youtube Search results:")
+      .setDescription(`${ views } | ${ v.title } (${ v.timestamp }) | ${ v.author.name }`)
+      .setColor('RANDOM');
+      message.channel.send(embed);
   } )
 } )
 
