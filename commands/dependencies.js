72f8fd83c5346788e21b4a51d47ccd8b2578cb6d
IgorKowalczyk <majonezexe@gmail.com>
2020-06-15 09:57:59 +0200

    Add new command

---
+++ commands/dependencies.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+  try {
+    message.channel.send('Cytrus runs on ' + Object.keys(require('../package').dependencies).length + ' dependencies');
+	
+	const embed = new Discord.RichEmbed()
+	  .setTitle("Majo dependencies")
+      .setDescription("Majo runs on" + Object.keys(require('../package').dependencies).length + "dependencies")
+	  .setTimestamp()
+	  .setColor("RANDOM")
+  message.channel.send(embed);
+  } catch (err) {
+    message.channel.send({embed: {
+                color: 16734039,
+                description: "Something went wrong... :cry:"
+            }})
+  }
+}
+
+exports.help = {
+  name: 'dependencies',
+  category: 'System',
+  description: 'Returns the amount of dependencies Cytrus uses',
+  usage: 'dependencies'
+};
