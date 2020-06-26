8fc2f7126c5bc27abf27687bd7d6e1bc9377cc61
IgorKowalczyk <majonezexe@gmail.com>
2020-06-26 21:54:30 +0200

    Revert "Add music commands"
    This reverts commit 804dda256631e40ddb98e88f066de87884edeb8c.

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = module.require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     if (!message.mentions.users.size) {     
