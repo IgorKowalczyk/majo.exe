d4306c85c598d88a30c26565499d018f560f85da
IgorKowalczyk <majonezexe@gmail.com>
2020-06-27 15:42:59 +0200

    ...

--- commands/play.js
+++ commands/play.js
@@ -32,7 +32,7 @@ module.exports.run = async (client, message, args) => {
   const song = {
     id: firstVideo.id,
     url: `https://youtube.com/watch?v=${firstVideo.id}`,
-    title: firstVideo.title),
+    title: firstVideo.title,
     author: firstVideo.channel.title,
     duration: firstVideo.duration,
     durationSeconds: firstVideo.durationSeconds,
