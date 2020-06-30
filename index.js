62d6e372d97eb033eb85401fd0f125cb3c20f76a
IgorKowalczyk <majonezexe@gmail.com>
2020-06-30 09:13:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ client.on('message', message=> {
 /* STATUS */
 
 var date = new Date().toJSON().slice(0,10).replace(/-/g,'/');
-const enddate = (new Date().getFullYear()) + "/05/13";
+const enddate = (new Date().getFullYear()) + "/06/30";
 const enddateEEP = (new Date().getFullYear()) + "/06/3";
 if (date == enddate) {
 setInterval(async () => {
