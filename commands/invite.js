22ca11a2cbeee1255db9a208dab345e073236ed1
IgorKowalczyk <majonezexe@gmail.com>
2020-06-30 21:35:28 +0200

    Update (add err)

--- commands/invite.js
+++ commands/invite.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 const cnf = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
+try {
 const embed = new Discord.RichEmbed()
   .setTitle("Invite the bot")
   .setColor('RANDOM')
-  .addField("Invite", "[Invite the bot here (Recomended!)](" + `${cnf.website}` + "/authorize) \n[Invite the bot here (Normal)](https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=" + `${client.user.id})`)
+  .addField("Invite to Discord server", "[Invite the bot here (Recomended!)](" + `${cnf.website}` + "/authorize) \n[Invite the bot here (Normal)](https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=" + `${client.user.id})`)
   .addField("Website", `[Visit webiste](${cnf.website})`)
   .addField("Server", `[Join to official server](${cnf.server})`)
   .setFooter("Bot created by " + `${cnf.owner}`)
@@ -16,6 +17,12 @@ message.channel.send({embed: {
             color: 3447003,
             description: "Check a priv message!"
         }})
+} catch (err) {
+    message.channel.send({embed: {
+                color: 16734039,
+                description: "Something went wrong... :cry:"
+            }})
+}
 }
 
 module.exports.help = {
