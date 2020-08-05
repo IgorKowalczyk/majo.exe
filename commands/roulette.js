ae8106497ea966d992ebcae864ea3876227e9643
IgorKowalczyk <majonezexe@gmail.com>
2020-08-05 08:17:44 +0200

    Update commands

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,56 +1,76 @@
-const Discord = module.require("discord.js");
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
 
 module.exports.run = async (client, message, args) => {
-    var v = ~~(Math.random() * 3);  // 0 to 2
-    var deathText = ":dizzy_face: :boom: :gun: *UNLUCKY!*";
-    var aliveText = ":sweat_smile: :gun: *LUCKY!*";
-    var defaultText = ":smile: :gun:";
+  let user = message.author;
 
-  message.channel.send({embed: {
-    color: 3447003,
-    description: message.author + ", You play russian roulette",
-    title: defaultText + "   3"
-    }})
-        .then(msg => {
-            setTimeout(function() {
-             msg.edit({embed: {
-              color: 3447003,
-              title: defaultText + "   2",
-              description: message.author + ", You play russian roulette"
-               }})
-                    .then(msg => {
-                        setTimeout(function() {
-                         msg.edit({embed: {
-                          color: 3447003,
-                          title: defaultText + "   1",
-                          description: message.author + ", You play russian roulette",
-                          }})
-                                .then(msg => {
-                                    setTimeout(function() {
-                                        if(v == 0){
-                                         msg.edit({embed: {
-                                          color: 3447003,
-                                          title: deathText,
-                                          description: message.author + ", You dead in russian roulette!"
-                                         }})
-                                        }else{
-                                         msg.edit({embed: {
-                                          color: 3447003,
-                                          title: aliveText,
-                                          description: message.author + ", You win in russian roulette!"
-                                         }})
-                                        }
-                                    }, 1000);
-                                });
-                        }, 1000);
-                    });
-            }, 1000);
-        })
-        .catch((err) => message.channel.send({embed: {
-                color: 16734039,
-                description: "Something went wrong... :cry:"
-            }}));
+  function isOdd(num) { 
+	if ((num % 2) == 0) return false;
+	else if ((num % 2) == 1) return true;
 }
+    
+let colour = args[0];
+let money = parseInt(args[1]);
+let moneydb = await db.fetch(`money_${message.guild.id}_${user.id}`)
+
+let random = Math.floor(Math.random() * 37);
+
+let moneyhelp = new Discord.RichEmbed()
+.setColor("#FFFFFF")
+.setDescription(`:x: Specify an amount to gamble | !majo roulette <color> <amount>`);
+
+let moneymore = new Discord.RichEmbed()
+.setColor("#FFFFFF")
+.setDescription(`:x: You are betting more than you have`);
+
+let colorbad = new Discord.RichEmbed()
+.setColor("#FFFFFF")
+.setDescription(`:x: Specify a color | Red [1.5x] Black [2x] Green [15x]`);
+
+
+    if (!colour)  return message.channel.send(colorbad);
+    colour = colour.toLowerCase()
+    if (!money) return message.channel.send(moneyhelp); 
+    if (money > moneydb) return message.channel.send(moneymore);
+    
+    if (colour == "b" || colour.includes("black")) colour = 0;
+    else if (colour == "r" || colour.includes("red")) colour = 1;
+    else if (colour == "g" || colour.includes("green")) colour = 2;
+    else return message.channel.send(colorbad);
+    
+    
+    
+    if (random == 0 && colour == 2) { // Green
+        money *= 15
+        db.add(`money_${message.guild.id}_${user.id}`, money)
+        let moneyEmbed1 = new Discord.RichEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`:green_circle: You won ${money} coins\n\nMultiplier: 15x`);
+        message.channel.send(moneyEmbed1)
+    } else if (isOdd(random) && colour == 1) { // Red
+        money = parseInt(money * 1.5)
+        db.add(`money_${message.guild.id}_${user.id}`, money)
+        let moneyEmbed2 = new Discord.RichEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`:red_circle: You won ${money} coins\n\nMultiplier: 1.5x`);
+        message.channel.send(moneyEmbed2)
+    } else if (!isOdd(random) && colour == 0) { // Black
+        money = parseInt(money * 2)
+        db.add(`money_${message.guild.id}_${user.id}`, money)
+        let moneyEmbed3 = new Discord.RichEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`:black_circle: You won ${money} coins\n\nMultiplier: 2x`);
+        message.channel.send(moneyEmbed3)
+    } else { // Wrong
+        db.subtract(`money_${message.guild.id}_${user.id}`, money)
+        let moneyEmbed4 = new Discord.RichEmbed()
+        .setColor("#FFFFFF")
+        .setDescription(`:x: You lost ${money} coins\n\nMultiplier: 0x`);
+        message.channel.send(moneyEmbed4)
+    }
+}
+
 
 module.exports.help = {
     name: "roulette",
