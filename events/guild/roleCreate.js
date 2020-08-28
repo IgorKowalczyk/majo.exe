22a3f7a61d5be6c255fd7708f5cdeafd6c0b9380
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-28 09:15:01 +0200

    Create roleCreate.js

---
+++ events/guild/roleCreate.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, member) => {
+try {
+ if (!role.guild.member(client.user).hasPermission("EMBED_LINKS")) return;
+ if (!role.guild.member(client.user).hasPermission("VIEW_AUDIT_LOG")) return;
+ var logChannel = role.guild.channels.cache.find(channel => channel.name.includes('log'));
+ if (!logChannel) return;
+ role.guild.fetchAuditLogs().then(logs => {
+  var userID = logs.entries.first().executor.id;
+  var userAvatar = logs.entries.first().executor.avatarURL();
+  let roleCreate = new Discord.MessageEmbed()
+   .setTitle("**ROLE CREATE**")
+   .setThumbnail(userAvatar)
+   .setDescription(`**\n**:white_check_mark: Successfully \`\`CREATE\`\` Role.\n\n**Role Name:** \`\`${role.name}\`\` (ID: ${role.id})\n**By:** <@${userID}> (ID: ${userID})`)
+   .setColor("RANDOM")
+   .setTimestamp()
+   .setFooter(role.guild.name, role.guild.iconURL());
+  logChannel.send(roleCreate);
+ });
+} catch (err) {
+ let embed = new Discord.MessageEmbed()
+  .setColor("#FF0000")
+  .setTitle("Error!")
+  .setDescription("**Error Code:** *" + err + "*")
+  .setTimestamp();
+ return logChannel.send(embed);
+}
+}
