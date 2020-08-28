3af7dbc046eb7836c86b19692e00d10f6ca416c8
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-28 09:13:33 +0200

    Create guildMemberRemove.js

---
+++ events/guild/guildMemberRemove.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, member) => {
+try {
+const channel = member.guild.channels.cache.find(channel => channel.name.includes('hello-or-bye'));
+if (!channel) return;
+ let embed = new Discord.MessageEmbed()
+  .setDescription(`**${member.displayName}#${member.user.discriminator}** left the server.`)
+  .setThumbnail(member.user.displayAvatarURL())
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter(`Total members: ${member.guild.memberCount}`);
+ channel.send((embed = embed));
+} catch(err) {
+ console.log(err);
+}
+}
