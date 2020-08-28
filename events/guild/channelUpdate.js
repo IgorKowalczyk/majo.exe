cf3d4ded887df4d8cdb37f2913e0241eba779d74
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-28 10:53:06 +0200

    Create channelUpdate.js

---
+++ events/guild/channelUpdate.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, channel) => {
+try {
+ if (!guild.member(client.user).hasPermission("EMBED_LINKS")) return;
+ if (!guild.member(client.user).hasPermission("VIEW_AUDIT_LOG")) return;
+ if (!oldChannel.guild) return;
+ var logChannel = oldChannel.guild.channels.cache.find(c => c.name === "log");
+ if (!logChannel) return;
+ if (oldChannel.type === "text") {
+   var channelType = "Text";
+ } else if (oldChannel.type === "voice") {
+  var channelType = "Voice";
+ } else if (oldChannel.type === "category") {
+  var channelType = "Category";
+ }
+ oldChannel.guild.fetchAuditLogs().then(logs => {
+  var userID = logs.entries.first().executor.id;
+  var userAvatar = logs.entries.first().executor.avatarURL();
+  if (oldChannel.name !== newChannel.name) {
+   let newName = new Discord.MessageEmbed()
+    .setTitle("**CHANNEL EDIT**")
+    .setThumbnail(userAvatar)
+    .setColor("RANDOM")
+   .setDescription(`**\n**:wrench: Successfully Edited **${channelType}** Channel Name\n\n**Old Name:** \`\`${oldChannel.name}\`\`\n**New Name:** \`\`${newChannel.name}\`\ `\n**Channel ID:** ${oldChannel.id}\n**By:** <@${userID}> (ID: ${userID})`)
+    .setTimestamp()
+    .setFooter(oldChannel.guild.name, oldChannel.guild.iconURL());
+   logChannel.send(newName);
+  }
+  if (oldChannel.topic !== newChannel.topic) {
+   let newTopic = new Discord.MessageEmbed()
+    .setTitle("**CHANNEL EDIT**")
+    .setThumbnail(userAvatar)
+    .setColor("RANDOM")
+    .setDescription(`**\n**:wrench: Successfully Edited **${channelType}** Channel Topic\n\n**Old Topic:**\n\`\`\`${oldChannel.topic ||
+     "(Not set)"}\`\`\`\n**New Topic:**\n\`\`\`${newChannel.topic ||
+     "(Not set)"}\`\`\`\n**Channel:** ${oldChannel} (ID: ${oldChannel.id})\n**By:** <@${userID}> (ID: ${userID})`)
+    .setTimestamp()
+    .setFooter(oldChannel.guild.name, oldChannel.guild.iconURL());
+   logChannel.send(newTopic);
+  }
+ });
+} catch (err) {
+ let embed = new Discord.MessageEmbed()
+  .setColor("#FF0000")
+  .setTitle("Error!")
+  .setDescription("**Error Code:** *" + err + "*")
+  .setTimestamp();
+ return logChannel.send(embed);
+}
+}
