ef91f262a24ebbc1a81d9e625804cf4d9178ed9e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-28 20:46:49 +0200

    Update voiceStateUpdate.js

--- events/guild/voiceStateUpdate.js
+++ events/guild/voiceStateUpdate.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = async (voiceOld, voiceNew) => {
 try {
- const channel = voiceOld.guild.channel.cache.find(channel => channel.name.includes('log'));
+ var logChannel = voiceNew.guild.channels.cache.find(c => c.name === 'log');  
  if (!logChannel) return;
  voiceOld.guild.fetchAuditLogs().then(logs => {
   var userID = logs.entries.first().executor.id;
@@ -11,7 +11,7 @@ try {
   if (voiceOld.serverMute === false && voiceNew.serverMute === true) {
    let serverMutev = new Discord.MessageEmbed()
     .setTitle("**VOICE MUTE**")
-    .setThumbnail("https://images-ext-1.discordapp.net/external/pWQaw076OHwVIFZyeFoLXvweo0T_fDz6U5C9RBlw_fQ/https/cdn.pg.sa/UosmjqDNgS.png")
+    .setThumbnail(voiceOld.user.avatarURL())
     .setColor("RANDOM")
     .setDescription(`**User:** <@${voiceOld.user.id}> (ID: ${voiceOld.user.id})\n**By:** <@${userID}> (ID: ${userID})\n**Channel:** \`\`${voiceOld.voiceChannel.name}\`\` (ID: ${voiceOld.voiceChannel.id})`)
     .setTimestamp()
@@ -21,7 +21,7 @@ try {
   if (voiceOld.serverMute === true && voiceNew.serverMute === false) {
    let serverUnmutev = new Discord.MessageEmbed()
     .setTitle("**VOICE UNMUTE**")
-    .setThumbnail("https://images-ext-1.discordapp.net/external/u2JNOTOc1IVJGEb1uCKRdQHXIj5-r8aHa3tSap6SjqM/https/cdn.pg.sa/Iy4t8H4T7n.png")
+    .setThumbnail(voiceOld.user.avatarURL())
     .setColor("RANDOM")
     .setDescription(`**User:** <@${voiceOld.user.id}> (ID: ${voiceOld.user.id})\n**By:** <@${userID}> (ID: ${userID})\n**Channel:** \`\`${voiceOld.voiceChannel.name}\`\` (ID: ${voiceOld.voiceChannel.id})`)
     .setTimestamp()
@@ -31,7 +31,7 @@ try {
   if (voiceOld.serverDeaf === false && voiceNew.serverDeaf === true) {
    let serverDeafv = new Discord.MessageEmbed()
     .setTitle("**VOICE DEAFEN**")
-    .setThumbnail("https://images-ext-1.discordapp.net/external/7ENt2ldbD-3L3wRoDBhKHb9FfImkjFxYR6DbLYRjhjA/https/cdn.pg.sa/auWd5b95AV.png")
+    .setThumbnail(voiceOld.user.avatarURL())
     .setColor("RANDOM")
     .setDescription(`**User:** <@${voiceOld.user.id}> (ID: ${voiceOld.user.id})\n**By:** <@${userID}> (ID: ${userID})\n**Channel:** \`\`${voiceOld.voiceChannel.name}\`\` (ID:  ${voiceOld.voiceChannel.id})`)
     .setTimestamp()
@@ -41,7 +41,7 @@ try {
   if (voiceOld.serverDeaf === true && voiceNew.serverDeaf === false) {
    let serverUndeafv = new Discord.MessageEmbed()
     .setTitle("**VOICE UNDEAFEN**")
-    .setThumbnail("https://images-ext-2.discordapp.net/external/s_abcfAlNdxl3uYVXnA2evSKBTpU6Ou3oimkejx3fiQ/https/cdn.pg.sa/i7fC8qnbRF.png")
+    .setThumbnail(voiceOld.user.avatarURL())
     .setColor("RANDOM")
     .setDescription(`**User:** <@${voiceOld.user.id}> (ID: ${voiceOld.user.id})\n**By:** <@${userID}> (ID: ${userID})\n**Channel:** \`\`${voiceOld.voiceChannel.name}\`\` (ID: ${voiceOld.voiceChannel.id})`)
     .setTimestamp()
@@ -54,7 +54,7 @@ try {
   let voiceJoin = new Discord.MessageEmbed()
    .setTitle("**JOIN VOICE ROOM**")
    .setColor("RANDOM")
-   .setThumbnail(voiceOld.user.avatarURL)
+   .setThumbnail(voiceOld.user.avatarURL())
    .setDescription(`**\n**:arrow_lower_right: Successfully \`\`JOIN\`\` To Voice Channel.\n\n**Channel:** \`\`${voiceNew.voiceChannel.name}\`\` (ID: ${voiceNew.voiceChannelID})\n**User:** ${voiceOld} (ID: ${voiceOld.id})`)
    .setTimestamp()
    .setFooter(voiceOld.user.tag, voiceOld.user.avatarURL());
@@ -75,7 +75,7 @@ try {
   let voiceLeave = new Discord.MessageEmbed()
    .setTitle("**CHANGED VOICE ROOM**")
    .setColor("RANDOM")
-   .setThumbnail(voiceOld.user.avatarURL)
+   .setThumbnail(voiceOld.user.avatarURL())
    .setDescription(`**\n**:repeat: Successfully \`\`CHANGED\`\` The Voice Channel.\n\n**From:** \`\`${voiceOld.voiceChannel.name}\`\` (ID: ${voiceOld.voiceChannelID})\n**To:** \`\`${voiceNew.voiceChannel.name}\`\` (ID: ${voiceNew.voiceChannelID})\n**User:** ${voiceOld} (ID: ${voiceOld.id})`)
    .setTimestamp()
    .setFooter(voiceOld.user.tag, voiceOld.user.avatarURL());
