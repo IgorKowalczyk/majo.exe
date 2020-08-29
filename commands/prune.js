0518ff2210360251bd058e92213f585ca29bd4cb
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-29 13:35:34 +0200

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -10,52 +10,52 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    if (!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("ADMINISTRATOR")) {
-    message.channel.send({embed: {
-     color: 16734039,
-     description: "You don't have premission to prune messages!",
-     footer: "This message will be deleted after 3 seconds"
-    }}).then(m => m.delete({timeout: 3000}))
+    let error = new Discord.MessageEmbed()
+     .setColor("16734039")
+     .setDescription("You don't have premission to prune messages!")
+     .setFooter("This message will be deleted after 3 seconds")
+    message.channel.send(error).then(m => m.delete({timeout: 3000}))
     return message.delete({timeout: 3000})
    }
    if (isNaN(args[0])) {
-    message.channel.send({embed: {
-     color: 16734039,
-     description: "Please input a vaild number!",
-     footer: "This message will be deleted after 3 seconds"
-    }}).then(m => m.delete({timeout: 3000}))
+    let error = new Discord.MessageEmbed()
+     .setColor("16734039")
+     .setDescription("Please input a vaild number!")
+     .setFooter("This message will be deleted after 3 seconds")
+    message.channel.send(error).then(m => m.delete({timeout: 3000}))
     return message.delete({timeout: 3000})
    }
    if (args[0] > 100) {
-    message.channel.send({embed: {
-     color: 16734039,
-     description: "Insert the number less than 100!",
-     footer: "This message will be deleted after 3 seconds"
-    }}).then(m => m.delete({timeout: 3000}))
+    let error = new Discord.MessageEmbed()
+     .setColor("16734039")
+     .setDescription("Insert the number less than 100!")
+     .setFooter("This message will be deleted after 3 seconds")
+    message.channel.send(error).then(m => m.delete({timeout: 3000}))
     return message.delete({timeout: 3000})
    }
    if (args[0] < 2) {
-    message.channel.send({embed: {
-     color: 16734039,
-     description: "Insert the number more than 1!",
-     footer: "This message will be deleted after 3 seconds"
-    }}).then(m => m.delete({timeout: 3000}))
+    let error = new Discord.MessageEmbed()
+     .setColor("16734039")
+     .setDescription("Insert the number more than 1!")
+     .setFooter("This message will be deleted after 3 seconds")
+    message.channel.send(error).then(m => m.delete({timeout: 3000}))
     return message.delete({timeout: 3000})
    }
    await message.delete()
    await message.channel.bulkDelete(args[0])
    .then(messages => {
-	 message.channel.send({embed: {
-      color: 16734039,
-      description: `Deleted ${messages.size}/${args[0]} messages.`,
-     footer: "This message will be deleted after 3 seconds"
-	 }}).then(m => m.delete({timeout: 3000}))
-	})
+    let error = new Discord.MessageEmbed()
+     .setColor("RANDOM")
+     .setDescription("Deleted ${messages.size}/${args[0]} messages.")
+     .setFooter("This message will be deleted after 3 seconds")
+    return message.channel.send(error).then(m => m.delete({timeout: 3000}))
+   })
   } catch(err) {
    message.channel.send({embed: {
     color: 16734039,
     description: "Something went wrong... :cry:"
   }})
-	  console.log(err)
+  console.log(err)
   }
  }
 }
