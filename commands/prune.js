d3fcbe1b94523959804b6797565d28da88389f7e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-29 09:44:46 +0200

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -13,25 +13,25 @@ module.exports = {
     return message.channel.send({embed: {
      color: 16734039,
      description: "You don't have premission to prune messages!"
-    }})
+    }}).then(msg => msg.delete(5000));
    }
    if (isNaN(args[0])) {
     return message.channel.send({embed: {
      color: 16734039,
      description: "Please input a vaild number!"
-    }})
+    }}).then(msg => msg.delete(5000));
    }
    if (args[0] > 100) {
     return message.channel.send({embed: {
      color: 16734039,
      description: "Insert the number less than 100!"
-    }})
+    }}).then(msg => msg.delete(5000));
    }
    if (args[0] < 2) {
     return message.channel.send({embed: {
      color: 16734039,
      description: "Insert the number more than 1!"
-    }})
+    }}).then(msg => msg.delete(5000));
    }
    await message.delete()
    await message.channel.bulkDelete(args[0])
@@ -39,7 +39,7 @@ module.exports = {
 	 message.channel.send({embed: {
       color: 16734039,
       description: `Deleted ${messages.size}/${args[0]} messages.`
-	 }})
+	 }}).then(msg => msg.delete(5000));
 	})
   } catch(err) {
    message.channel.send({embed: {
