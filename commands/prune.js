5e113e53884bb8b42a9d77a229f83985e7bc76f9
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-29 10:09:11 +0200

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -10,25 +10,25 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    if (!message.member.hasPermission("MANAGE_MESSAGES") || !message.member.hasPermission("ADMINISTRATOR")) {
-    message.channel.send({embed: {
+    return message.channel.send({embed: {
      color: 16734039,
      description: "You don't have premission to prune messages!"
     }}).then(m => m.delete({timeout: 5000}))
    }
    if (isNaN(args[0])) {
-    message.channel.send({embed: {
+    return message.channel.send({embed: {
      color: 16734039,
      description: "Please input a vaild number!"
     }}).then(m => m.delete({timeout: 5000}))
    }
    if (args[0] > 100) {
-    message.channel.send({embed: {
+    return message.channel.send({embed: {
      color: 16734039,
      description: "Insert the number less than 100!"
     }}).then(m => m.delete({timeout: 5000}))
    }
    if (args[0] < 2) {
-    message.channel.send({embed: {
+    return message.channel.send({embed: {
      color: 16734039,
      description: "Insert the number more than 1!"
     }}).then(m => m.delete({timeout: 5000}))
