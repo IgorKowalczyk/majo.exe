6a95dd2e4acfb1c30e882d9f053425c0bbee35d6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-29 11:58:00 +0200

    Update anal.js

--- commands/anal.js
+++ commands/anal.js
@@ -1,4 +1,4 @@
-const superagent = require("snekfetch");
+const api = require("snekfetch");
 const Discord = require('discord.js')
 
 module.exports = {
@@ -7,7 +7,7 @@ module.exports = {
  description: "Display a random anal image/gif",
  category: 'NSFW',
  usage: "anal",
- run: async (client, message, args) => {
+ run: async (client, message) => {
   if (!message.channel.nsfw) {
    message.react('💢');
    return message.channel.send({embed: {
@@ -15,13 +15,13 @@ module.exports = {
     description: "You can use this command in an NSFW Channel!"
   }})
   }
-  superagent.get('https://nekos.life/api/v2/img/anal')
+  api.get('https://nekos.life/api/v2/img/anal')
   .end((err, response) => {
   const embed = new Discord.MessageEmbed()
    .setTitle(":smirk: Anal")
    .setImage(response.body.url)
    .setColor(`RANDOM`)
-   .setFooter(`Tags: anal`)
+   .setFooter(`Tags: anal Requested by ${message.author}`)
    .setURL(response.body.url);
   message.channel.send(embed);
   }).catch((err) => message.channel.send({embed: {
