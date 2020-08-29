6a52a777f6c5ba1ab5e64cedb51e31cf84c41a32
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-08-29 08:51:27 +0200

    Update roleUpdate.js

--- events/guild/roleUpdate.js
+++ events/guild/roleUpdate.js
@@ -2,37 +2,27 @@ const Discord = require('discord.js');
 
 module.exports = async (client, oldRole, newRole) => {
 try {
-    if (oldRole.permissions !== newRole.permissions) {
-
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(`Role changed permissions`)
-            .setColor("#ffc500")
-            .setFooter(`ID: ${newRole.id}`)
-            .setTimestamp()
-
-        const oldPerms = oldRole.serialize();
-        const newPerms = newRole.serialize();
-
-        const permUpdated = [];
-
-        for (const [key, element] of Object.entries(oldPerms)) {
-            if (newPerms[key] !== element) permUpdated.push(key);
-        }
-
-        if (oldRole.permissions > newRole.permissions) {
-            //Permission lost
-
-            embed.setDescription(`**${newRole.toString()} has lost the ${permUpdated.join(", ")} permission**`)
-            logchannel.send(embed).catch()
-
-        } else if (oldRole.permissions < newRole.permissions) {
-            //Permission given
-
-            embed.setDescription(`**${newRole.toString()} has been given the ${permUpdated.join(", ")} permission**`)
-            logchannel.send(embed).catch()
-
-        }
-    }
+ var logChannel = oldRole.guild.channels.cache.find(c => c.name === 'log');  
+ if (oldRole.permissions !== newRole.permissions) {
+  const embed = new Discord.MessageEmbed()
+   .setAuthor(`Role changed permissions`)
+   .setColor("RANDOM")
+   .setFooter(`ID: ${newRole.id}`)
+   .setTimestamp()
+  const oldPerms = oldRole.serialize();
+  const newPerms = newRole.serialize();
+  const permUpdated = [];
+  for (const [key, element] of Object.entries(oldPerms)) {
+   if (newPerms[key] !== element) permUpdated.push(key);
+  }
+  if (oldRole.permissions > newRole.permissions) {
+   embed.setDescription(`**${newRole.toString()} has lost the ${permUpdated.join(", ")} permission**`)
+   logchannel.send(embed)
+  } else if (oldRole.permissions < newRole.permissions) {
+   embed.setDescription(`**${newRole.toString()} has been given the ${permUpdated.join(", ")} permission**`)
+   logchannel.send(embed).catch()
+  }
+ }
 } catch (err) {
  let embed = new Discord.MessageEmbed()
   .setColor("#FF0000")
