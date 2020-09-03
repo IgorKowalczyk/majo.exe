10db3539288a9a914aff13dfafe0f77a8b6abfea
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-03 20:41:33 +0200

    Update baka.js

--- commands/baka.js
+++ commands/baka.js
@@ -8,21 +8,23 @@ module.exports = {
  category: "Fun",
  usage: "baka",
  run: async (client, message, args) => {
-fetch("https://nekos.life/api/v2/img/baka")
- .then((res) => {
-  const embed = new Discord.MessageEmbed()
-   .setTitle("BAKA!!!")
-   .setImage(res.url)
-   .setColor("RANDOM")
-   .setFooter("idiot!" + res.url)
-   .setURL(res.url);
-  message.channel.send(embed);
-  }).catch((err) => {
-   message.channel.send({embed: {
-    color: 16734039,
-    description: "Something went wrong... :cry:"
-   }})
-   console.log(err);
-  });
+ try {
+ fetch("https://nekos.life/api/v2/img/baka")
+  .then((res) => res.text()) {
+   const embed = new Discord.MessageEmbed()
+    .setTitle("BAKA!!!")
+    .setImage(res.url)
+    .setColor("RANDOM")
+    .setFooter("idiot!" + res.url)
+    .setURL(res.url);
+   message.channel.send(embed);
+  }
+ } catch(err) {
+  message.channel.send({embed: {
+   color: 16734039,
+   description: "Something went wrong... :cry:"
+  }})
+  console.log(err);
+ }
  }
 }
