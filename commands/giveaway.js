8d8bebb32c399ecd762cecd5626819dbdd718d2a
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-05 11:21:53 +0200

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -11,33 +11,36 @@ module.exports = {
   if (!args[0]) {
    return message.channel.send({embed: {
     color: 16734039,
-    description: "You did not specify your time!\nCorrect formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
+    title: ":x: You did not specify your time!",
+    description: "Correct formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
    }})
   }
   if (!args[0].endsWith("d") && !args[0].endsWith("h") && !args[0].endsWith("m")) {
    return message.channel.send({embed: {
     color: 16734039,
-    description: "You did not use the correct formatting for the time!\nCorrect formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
+    title: ":x: You did not use the correct formatting for the time!",
+    description: "Correct formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
    }})
   }
   if (isNaN(args[0][0])) {
    return message.channel.send({embed: {
     color: 16734039,
-    description: "You did not specify your time!\nCorrect formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
+    title: ":x: You did not specify your time!".
+    description: "Correct formatting: \`number<d/h/m>\`.\nLegend: \`d\` - Day, \`h\` - Hour/s, \`m\` - Minute/s"
    }})
   }
   let channel = message.mentions.channels.first();
   if (!channel) {
    return message.channel.send({embed: {
     color: 16734039,
-    description: "You must provide a channel in the guild to create giveaway!"
+    description: ":x: You must provide a channel in the guild to create giveaway!"
    }})
   }
   let prize = args.slice(2).join(" ");
   if (!prize) {
    return message.channel.send({embed: {
     color: 16734039,
-    description: "No prize specified!"
+    description: ":x: No prize specified!"
    }})
   }
   let success = new Discord.MessageEmbed()
@@ -46,10 +49,10 @@ module.exports = {
    .setFooter("This message will be deleted after 10 seconds")
   message.channel.send(success).then(m => m.delete({timeout: 10000}))
   let embed = new Discord.MessageEmbed()
-   .setTitle(":tada: New giveaway! :tada:")
+   .setTitle(":tada: New giveaway! :tada:", message.guild.iconURL({ dynamic: true, format: 'png'}))
    .setDescription("The user " + `${message.author}` + " is hosting a giveaway for the prize of **" + `${prize}` + "**\n*React to this message with :tada: emoji to enter the giveaway!*")
    .setTimestamp(Date.now() + ms(args[0]))
-   .setFooter("Requested by " + `${message.author.username}` + " The giveaway will end in " + `${args[0]}` + "!")
+   .setFooter("Requested by " + `${message.author.username}` + " The giveaway will end in " + `${args[0]}` + "!", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
    .setColor("RANDOM");
   let m = await channel.send(embed);
   m.react("🎉");
