a54a3a8dc0e602d515fc0210abe626dbe5055583
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-06 13:45:24 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -11,8 +11,10 @@ module.exports = {
  run: async (client, message, args) => {
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
   let embed = new Discord.MessageEmbed()
-   .setTitle(":hourglass_flowing_sand: Uptime")
+   .setTitle(":hourglass_flowing_sand: Uptime", message.guild.iconURL({ dynamic: true, format: 'png'}))
    .setDescription(`${duration}`)
+   .setImage(client.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+   .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
    .setColor("RANDOM")
   message.channel.send(embed);
  }
