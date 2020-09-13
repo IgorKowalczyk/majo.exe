d16ad0743c56cb28e7200e3929fca6ffdf7e7475
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-13 13:58:49 +0200

    Create members.js

---
+++ commands/members.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+
+module.exports = {
+ name: "members",
+ aliases: ["users"],
+ description: "How many members are in the current server",
+ category: "Utility",
+ usage: "members",
+ run: async (client, message, args) => {
+  let onlineMembers = message.guild.members.cache.filter(m => m.presence.status === 'online');
+  let offlineMembers = message.guild.members.cache.filter(m => m.presence.status === 'offline');
+  let idleMembers = message.guild.members.cache.filter(m => m.presence.status === 'idle');
+  let dndMembers = message.guild.members.cache.filter(m => m.presence.status === 'dnd');
+  var online = 0;
+  var offline = 0;
+  var idle = 0;
+  var donotdisturb = 0;
+  
+  onlineMembers.forEach(member => {
+    online += 1;
+  }); 
+  
+  offlineMembers.forEach(member => {
+    offline += 1;
+  }); 
+  
+  onlineMembers.forEach(member => {
+    online += 1;
+  });   
+  
+  idleMembers.forEach(member => {
+    idle += 1;
+  });  
+  
+  dndMembers.forEach(member => {
+    donotdisturb += 1;
+  }); 
+  
+  const embed = new Discord.MessageEmbed()
+    .setAuthor("Members", message.guild.iconURL)
+    .setColor("RANDOM")
+    .addField("Overall Members:", message.guild.memberCount || message.guild.members.size)
+    .addField("Online Members:", online)
+    .addField("Offline/Invisible Members:", offline)
+    .addField("Idle Members:", idle)
+    .addField("Do Not Disturb Members:", donotdisturb)
+  
+  message.channel.send(embed);
+}
+}
