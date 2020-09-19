8f5cdf0245bf882c5a70404ec48f7bf2bdbf61c9
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-19 12:51:58 +0200

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Resumes player.',
 	cooldown: 5,
 	category: "Music",
-	execute(message) {
+run: async (client, message, args) => {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (serverQueue && !serverQueue.playing) {
 			serverQueue.playing = true;
