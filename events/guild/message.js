c6ca2d9074e40622db311b6872e83b1b012da59c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-19 11:58:29 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,7 +3,6 @@ const prefix = process.env.PREFIX;
 
 module.exports = async (client, message) => {
  try {
-  const cooldowns = new Discord.Collection();
   if (message.author.bot) return;
   if (!message.guild) return;
   if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
@@ -22,27 +21,6 @@ module.exports = async (client, message) => {
   if (cmd.length === 0) return;
   let command = client.commands.get(cmd);
   if (!command) command = client.commands.get(client.aliases.get(cmd));
-  
-  if (!cooldowns.has(command.name)) {
-   cooldowns.set(command.name, new Discord.Collection());
-  }
-  console.log(command.name);
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.cooldown);
-  const cooldownAmount = (command.cooldown || 500) * 1000;
-  if (timestamps.has(message.author.id)) {
-   const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-   if (now < expirationTime) {
-    const timeLeft = (expirationTime - now) / 1000;
-    message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
-    .then(msg => {
-     msg.delete({ timeout: timeLeft.toFixed(1) })
-    })
-   }
-  }
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
   if (!command) {
    message.channel.send({embed: {
     color: 16734039,
