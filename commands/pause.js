82bc03a13010136daaa7c6e731db3267a55d19a6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-19 13:24:11 +0200

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,15 +1,15 @@
 module.exports = {
-	name: 'pause',
-	description: 'Pauses player.',
-	cooldown: 5,
-	category: "Music",
-run: async (client, message, args) => {
-		const serverQueue = message.client.queue.get(message.guild.id);
-		if (serverQueue && serverQueue.playing) {
-			serverQueue.playing = false;
-			serverQueue.connection.dispatcher.pause();
-			return message.channel.send('⏸ Paused the music for you!');
-		}
-		return message.channel.send('There is nothing playing.');
-	}
-};
+ name: 'pause',
+ description: 'Pauses player.',
+ cooldown: 5,
+ category: "Music",
+ run: async (client, message, args) => {
+  const serverQueue = message.client.queue.get(message.guild.id);
+  if (serverQueue && serverQueue.playing) {
+   serverQueue.playing = false;
+   serverQueue.connection.dispatcher.pause();
+   return message.channel.send('⏸ Paused the music for you!');
+  }
+  return message.channel.send('There is nothing playing.');
+ }
+}
