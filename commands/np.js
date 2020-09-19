10443ea391e1724df588ee0b7192e399a251d540
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-19 12:49:55 +0200

    Update np.js

--- commands/np.js
+++ commands/np.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Shows current song.',
 	cooldown: 5,
 	category: "Music",
-	execute(message) {
+run: async (client, message, args) => {
 		const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue) return message.channel.send('There is nothing playing.');
         return message.channel.send(`🎶 Now playing: **${serverQueue.songs[0].title}** by **${serverQueue.songs[0].channel}**`);
