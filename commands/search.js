75b9e941d0812b741ccbb80558c513fd0e1257e2
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-09 21:33:03 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -37,7 +37,9 @@ module.exports = {
    const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
    const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
    message.channel.activeCollector = false;
-   client.commands.get("play").execute(message, [choice]);
+   // client.commands.get("play").execute(message, [choice]);
+   let commandFile = require(`./commands/play.js`);
+   commandFile.run(message, [choice]);
    resultsMessage.delete().catch(console.error);
    } catch (error) {
     console.error(error);
