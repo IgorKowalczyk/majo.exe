512b20be984df010f066e0526935dc36410f763a
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-09 20:06:31 +0200

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,8 +1,5 @@
-const cheerio = require('cheerio')
-const Genius = require('genius-api')
-const fetch = require('node-fetch')
-const Discord = require('discord.js')
-const genius = new Genius(process.env.GENIUS_KEY)
+const genius = require("genius-lyrics")
+const G = new genius.Client(process.env.GENIUS_KEY)
 
 module.exports = {
  name: "lyrics",
@@ -12,47 +9,11 @@ module.exports = {
  usage: "Video Title & Artist Name" ,
  category: "Music",
  run: async (client, message, args) => {
-  //Function to search the Genius API and grab the first song
-   function genius_search_song(song_string) {
-    genius.search(song_string)
-    .then(function (response) {
-     song = response.hits[0].result
-     genius.getSongLyrics(song.url)
-    })
-   }
-   //Parses the HTML for the lyrics under the lyrics class
-   function parseSongHTML(htmlText) {
-    try {
-     } catch (err) {
-      console.error(err)
-     }
-     const $ = cheerio.load(htmlText)
-     const lyrics = $(`div[class="lyrics"]`).text().trim()
-     console.log(lyrics)
-     console.log(typeof lyrics)
-     if (!lyrics) return message.channel.send("Could not obtain lyrics. Ensure you have entered the Artist")
-     if (lyrics.length > 3000) return message.channel.send("Could not provide lyrics as they were over character allowance")
-     return message.channel.send(lyrics, {split: true, code: true })
-    }
-    //Adds method to Genius class for getting the song lyrics
-    Genius.prototype.getSongLyrics = function getSongLyrics(geniusUrl) {
-     return fetch(geniusUrl, { method: 'GET' })
-     .then(response => {
-      if (response.ok) return response.text()
-      throw new Error('Could not get song URL...')
-     })
-     .then(parseSongHTML)
-    }
-    if (args.length > 0) {
-     searchArgs = args.join(' ')
-     console.log(`Genius searching for... ${searchArgs}`)
-     try {
-      genius_search_song(searchArgs)
-      } catch (err) {
-       console.error(err)
-      }
-     } else {
-      return message.channel.send("Please supply some arguments")
-     }
+  G.tracks.search(message.content.split(' ').slice(1).join(' '), {limit: 1})
+   .then(results => {
+    const song = results[0]
+    message.channel.send(`**${song.artist.name} - ${song.title}**\n<${song.url}>`) //song.lyrics
+   })
+   .catch(err => message.reply(err))
  }
 }
