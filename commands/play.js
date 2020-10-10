ccd3ea06c5d899abf107dd9555e4c046f5df05a2
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-10 08:32:48 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -17,10 +17,16 @@ module.exports = {
  run: async (client, message, args) => {
   //Permissions and checks
   const { channel } = message.member.voice;
-  if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+  if (!channel) {
+   return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+  }
   const permissions = channel.permissionsFor(message.client.user);
-  if (!permissions.has('CONNECT')) return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
-  if (!permissions.has('SPEAK')) return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+  if (!permissions.has('CONNECT')) {
+   return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+  }
+  if (!permissions.has('SPEAK')) {
+   return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+  }
   //Defines the requested song based on the args
   req_song = args.join(" ");
   //Regular Expressions to check URL
