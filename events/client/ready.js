f46b80fb1b6ac7b1ceb90f58b0f719e5cce26cce
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-10 13:43:09 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,8 @@
-const Discord = require("discord.js");
 const prefix = process.env.PREFIX;
-const chalk = require("chalk");
+const { ErelaClient, Utils } = require("erela.js");
+const { Collection, MessageEmbed, Discord } = require("discord.js");
+const chalk = require('chalk');
+const { nodes } = require("../config.json");
 
 module.exports = (client) => {
 try {
@@ -36,5 +38,43 @@ try {
 } catch(err) {
  console.log(err);
 }
+
+client.queue = new Collection();
+client.music = new ErelaClient(client, nodes)
+ .on("nodeError", console.log)
+ .on("nodeConnect", () => console.log(chalk.blue("[Lavalink] Successfully Connected..")))
+ .on("queueEnd", player => {
+  const embed = new MessageEmbed()
+   .setDescription(`\`📛\` | **Song has been:** \`Ended\``)
+   .setColor('#000001');
+  player.textChannel.send(embed)
+  client.music.players.destroy(player.guild.id)
+ })
+
+.on("trackStart", ({ textChannel }, { title, duration, uri, identifier, requester, author }) => {
+ const player = client.music.players.get(textChannel.guild.id);
+ const qduration = Utils.formatTime(player.queue.reduce((acc, cur) => ({ duration: acc.duration + cur.duration })).duration, true);
+ const embed = new MessageEmbed()
+  .setAuthor(`Starting playing...`, 'https://cdn.discordapp.com/emojis/741605543046807626.gif')
+  .setDescription(`**[${title}](${uri})**`)
+  .setColor('#000001')
+  .setThumbnail(`https://img.youtube.com/vi/${identifier}/hqdefault.jpg`)
+  .addField('Author:', author, true)
+  .addField('Requester:', requester, true)
+  .addField('Current Volume:', player.volume, true)
+  .addField('Queue Length:', player.queue.length, true)
+  .addField('Duration:', `${Utils.formatTime(duration, true)}`, true)
+  .addField('Total Duration:', qduration, true)
+  .addField(`Current Duration: \`[0:00 / ${Utils.formatTime(duration, true)}]\``, `\`\`\`🔴 | 🎶──────────────────────────────\`\`\``)
+  .setTimestamp();          
+ return textChannel.send(embed)
+})
+
+client.levels = new Map()
+ .set("none", 0.0)
+ .set("low", 0.10)
+ .set("medium", 0.15)
+ .set("high", 0.25);
+
 console.log(chalk.blue("Connected! Logged in as ") + chalk.blue.underline(`${client.user.tag}`) + chalk.blue("!"));
 }
