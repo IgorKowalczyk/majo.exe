de6691127a9a6f66c8ba3c66ef46295a4f248d79
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-10 09:46:24 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -17,14 +17,9 @@ module.exports = {
   if (!permissions.has("CONNECT")) return msg.edit("I cannot connect to your voice channel, make sure I have permission to!");
   if (!permissions.has("SPEAK")) return msg.edit("I cannot connect to your voice channel, make sure I have permission to!");
   if (!args[0]) return msg.edit("Please provide a song name or link to search.");
-  const player = client.player.spawn({
-   guild: message.guild,
-   selfDeaf: true,
-   textChannel: message.channel,
-   voiceChannel: channel,
-  });
+  const player = new Player(client);
 
-  client.music.search(args.join(" "), message.author).then(async res => {
+  client.player.search(args.join(" "), message.author).then(async res => {
    switch (res.loadType) {
     case "TRACK_LOADED":
      player.queue.add(res.tracks[0]);
