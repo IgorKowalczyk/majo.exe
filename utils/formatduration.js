dc0f7489b82080bf41bf503902df7781de316c26
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-16 20:40:10 +0200

    Delete formatduration.js

--- utils/formatduration.js
+++
@@ -1,7 +0,0 @@
-const { Utils } = require('erela.js');
-
-module.exports = (duration) => {
- if(isNaN(duration) || typeof duration === 'undefined') return '00:00';
- if(duration > 3600000000) return 'Live';
- return Utils.formatTime(duration, true);
-};
