943d62902f7e6e5b155fe2c0fab1e7756e358042
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-16 19:37:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+ "verification": "-wuCsk4qLolXEPSUTGX7YBxywcyNNf5HS2ClzgEWxNY",
+ "description": "Majo.exe - Discord's multifunctional bot, contains moderation, fun, music, economics and NSFW commands.",
+ "twitter": "@majonezexe",
+ "mobile": "false",
+ "image": "https://igorkowalczyk.github.io/lib/favicon/favicon.jpg",
+ "server": "https://discord.gg/bVNNHuQ",
+ "authorwebsite": "https://igorkowalczyk.github.io",
+ "github": "igorkowalczyk",
+ "domain": process.env.DOMAIN,
+ "google_analitics": process.env.ANALYTICS,
+ "token": process.env.TOKEN,
+ "mongodbUrl": process.env.MONGODB,
+ "clientSecret": process.env.SECRET,
+ "port": process.env.PORT,
+ "prefix": process.env.PREFIX,
+}
