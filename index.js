526e74b696b2bd548b9b9a68d2f1c031680a78fd
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-18 18:09:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const fs = require("fs");
 const http = require("http");
 const db = require("quick.db");
 const chalk = require("chalk");
-const discord_token = process.env.TOKEN;
-const prefix = process.env.PREFIX;
 const newUsers = new Discord.Collection();
 var botMembers = 0;
 
-const mongoose = require("mongoose");
 const config = require("./config");
 const GuildSettings = require("./dashboard/models/settings");
 const Dashboard = require("./dashboard/dashboard");
 
+const token = config.token;
+const prefix = config.prefix;
+
 /* Login and Commands */
-if (discord_token) {
+if (token) {
  client.commands = new Discord.Collection();
  client.aliases = new Discord.Collection();
  client.queue = new Map(); 
  ['command', 'event'].forEach(handler => {
    require(`./handlers/${handler}`)(client);
  });
- client.login(discord_token);
+ client.login(token);
 } else {
  console.log(chalk.red.bold("Majo.exe Error:") + chalk.red(" Bot token is not provided! To give your bot life, you need to enter token value in the ") + chalk.grey.italic.bold(".env") +  chalk.red(" file - ") + chalk.grey.italic.bold("TOKEN=Your_Token ") + chalk.red.underline.bold("REMEMBER: Token is super-secret - do not share it with anyone!"));
 }
