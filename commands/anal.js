5078c421e04528b2d1c984fe09361fd4c33d887a
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-18 18:27:12 +0200

    Update anal.js

--- commands/anal.js
+++ commands/anal.js
@@ -10,31 +10,31 @@ module.exports = {
  category: "NSFW",
  usage: "anal",
  run: async (client, message, args) => {
- (async () => {
-  try {
-   if (!message.channel.nsfw) {
-    message.react('💢');
-    return message.channel.send({embed: {
+  (async () => {
+   try {
+    if (!message.channel.nsfw) {
+     message.react('💢');
+     return message.channel.send({embed: {
+      color: 16734039,
+      description: ":x: You can use this command in an NSFW Channel!"
+     }})
+    }
+    const response = await fetch("https://nekos.life/api/v2/img/anal")
+    const body = await response.json();
+    const embed = new Discord.MessageEmbed()
+     .setTitle(":smirk: Anal", message.guild.iconURL({ dynamic: true, format: 'png'}))
+     .setImage(body.url)
+     .setColor("RANDOM")
+     .setFooter("Requested by " + `${message.author.username}` + " • Tags: anal", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+     .setTimestamp()
+     .setURL(body.url);
+    message.channel.send(embed);
+   } catch(err) {
+    message.channel.send({embed: {
      color: 16734039,
-     description: ":x: You can use this command in an NSFW Channel!"
+     description: "Something went wrong... :cry:"
     }})
    }
-   const response = await fetch("https://nekos.life/api/v2/img/anal")
-   const body = await response.json();
-   const embed = new Discord.MessageEmbed()
-    .setTitle(":smirk: Anal", message.guild.iconURL({ dynamic: true, format: 'png'}))
-    .setImage(body.url)
-    .setColor("RANDOM")
-    .setFooter("Requested by " + `${message.author.username}` + " • Tags: anal", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
-    .setTimestamp()
-    .setURL(body.url);
-   message.channel.send(embed);
-  } catch(err) {
-   message.channel.send({embed: {
-    color: 16734039,
-    description: "Something went wrong... :cry:"
-   }})
-  }
- })();
+  })();
  }
 }
