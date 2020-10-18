75e12fba5033991920dd148ae4a2e80cd13a0632
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-18 18:26:19 +0200

    Update addmoney.js

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -10,37 +10,37 @@ module.exports = {
  category: "Economy",
  usage: "addmoney <user> <money>",
  run: async (client, message, args) => {
- try {
-  if (message.member.hasPermission("MANAGE_MESSAGES")) {
-   let user = message.mentions.members.first();
-   if (!user) return message.channel.send({embed: {
-    color: 16734039,
-    description: ":x: You must mention someone to add money!"
-   }})
-   if (isNaN(args[1])) return message.channel.send({embed: {
-    color: 16734039,
-    description: ":x: You must enter the amount of money to add!"
-   }})
-   db.add(`money_${message.guild.id}_${user.id}`, args[1])
-   let bal = await db.fetch(`money_${message.guild.id}_${user.id}`)
-   let moneyEmbed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle(":white_check_mark: Success!", message.guild.iconURL({ dynamic: true, format: 'png'}))
-    .setDescription(`:money_with_wings: Added \`${args[1]}\` coins\n:moneybag: New Balance: \`${bal}\``)
-    .setTimestamp()
-    .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
-    message.channel.send(moneyEmbed)
-  } else {
+  try {
+   if (message.member.hasPermission("MANAGE_MESSAGES")) {
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send({embed: {
+     color: 16734039,
+     description: ":x: You must mention someone to add money!"
+    }})
+    if (isNaN(args[1])) return message.channel.send({embed: {
+     color: 16734039,
+     description: ":x: You must enter the amount of money to add!"
+    }})
+    db.add(`money_${message.guild.id}_${user.id}`, args[1])
+    let bal = await db.fetch(`money_${message.guild.id}_${user.id}`)
+    let moneyEmbed = new Discord.MessageEmbed()
+     .setColor("RANDOM")
+     .setTitle(":white_check_mark: Success!", message.guild.iconURL({ dynamic: true, format: 'png'}))
+     .setDescription(`:money_with_wings: Added \`${args[1]}\` coins\n:moneybag: New Balance: \`${bal}\``)
+     .setTimestamp()
+     .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+     message.channel.send(moneyEmbed)
+   } else {
+    message.channel.send({embed: {
+     color: 16734039,
+     description: ":x: You don't have premission add money!"
+    }})
+   }
+  } catch(err) {
    message.channel.send({embed: {
     color: 16734039,
-    description: ":x: You don't have premission add money!"
+    description: "Something went wrong... :cry:"
    }})
   }
-} catch(err) {
- message.channel.send({embed: {
-  color: 16734039,
-  description: "Something went wrong... :cry:"
- }})
-}
-}
+ }
 }
