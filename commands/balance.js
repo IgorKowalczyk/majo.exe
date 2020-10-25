9ddd1de45e160aa0b0bedec61ed97bff0dad2d5c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-25 19:27:43 +0100

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -10,12 +10,12 @@ module.exports = {
  category: "Economy",
  usage: "balance <mention>",
  run: async (client, message, args) => {
-  let user = message.mentions.members.first() || message.author;
-  let bal = db.fetch(`money_${message.guild.id}_${user.id}`)
+  const user = message.mentions.members.first() || message.author;
+  const bal = db.fetch(`money_${message.guild.id}_${user.id}`)
   if (bal === null) bal = 0;
-  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+  const bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
   if (bank === null) bank = 0;
-  let moneyEmbed = new Discord.MessageEmbed()
+  const moneyEmbed = new Discord.MessageEmbed()
    .setColor("RANDOM")
    .setTitle(":white_check_mark: Success!", message.guild.iconURL({ dynamic: true, format: 'png'}))
    .setDescription(`${user}'s Balance:\n\n:money_with_wings: Pocket: \`${bal}\`\n:moneybag: Bank: \`${bank}\``)
