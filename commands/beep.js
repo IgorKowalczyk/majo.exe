a0c904f24798d5456c33807f3bc986466c5852ab
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-26 18:28:30 +0100

    Create beep.js

---
+++ commands/beep.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const prefix = config.prefix;
+
+module.exports = {
+ name: "beep",
+ //aliases: [],
+ description: "Beep-Boop!",
+ category: "Fun",
+ usage: "beep",
+ run: async (client, message, args) => {
+  const embed = new Discord.MessageEmbed()
+   .setColor: 'RANDOM',
+   .setDescription: "Boop!"
+  message.channel.send(embed);
+ }
+}
