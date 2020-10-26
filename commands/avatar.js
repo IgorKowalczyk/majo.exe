6b640593c8bebab36863c18eccdf5bc7b6d47712
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-10-26 09:17:39 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,13 +4,13 @@ const prefix = config.prefix;
 
 module.exports = {
  name: "avatar",
- aliases: [],
+ //aliases: [],
  description: "Gets user's avatar from a mention",
  category: "Utility",
  usage: "avatar <mention>",
  run: async (client, message, args) => {
   if (!message.mentions.users.size) {     
-   const embed = new Discord.MessageEmbed()
+   let embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setAuthor(message.author.username + "'s Avatar", message.author.displayAvatarURL())
     .setImage(message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 })) 
@@ -21,9 +21,9 @@ module.exports = {
    return `${user.username},${user.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 })}`;
   });
   for (var i = 0; i < avatarList.length; i++) {
-   const Username = avatarList[i].split(',')[0];
-   const AvatarURL = avatarList[i].split(",").pop();
-   const embed = new Discord.MessageEmbed()
+   let Username = avatarList[i].split(',')[0];
+   let AvatarURL = avatarList[i].split(",").pop();
+   let embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setAuthor(Username + "'s Avatar", AvatarURL)
     .setImage(AvatarURL)
