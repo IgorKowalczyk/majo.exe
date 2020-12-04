62773b691ba7eedcd33582bb143886d197cded38
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-12-04 18:17:57 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -44,7 +44,7 @@ if (process.env.DASHBOARD = "true" || config.sessionSecret, false || config.secr
  const Dashboard = require("../../dashboard/dashboard");
  Dashboard(client);
 } else {
- return console.log(chalk.blue('Dashboard is now disabled. To enable it change the "DASHBOARD" value in .env file to "true" (Now is set to "') + chalk.blue.underline(`${config.dashboard}`) + chalk.blue('")'));
+ return console.log(chalk.blue('Dashboard is now disabled. To enable it change the "DASHBOARD" value in .env file to "true" (Now is set to "') + chalk.blue.underline(`${config.dashboard}`) + chalk.blue('")')+ chalk.blue.underline("\nBot Stats: " + `${client.guilds.cache.size}` + "Servers, " + `${client.users.cache.size}` + "Members"));
 }
 
 console.log(chalk.blue("Connected! Logged in as ") + chalk.blue.underline(`${client.user.tag}`) + chalk.blue("!") + chalk.blue.underline("\nBot Stats: " + `${client.guilds.cache.size}` + "Servers, " + `${client.users.cache.size}` + "Members"));
