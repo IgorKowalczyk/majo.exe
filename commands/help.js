657ac6ea3853df07cafe32bedc246943e2e20b3f
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-01 18:14:32 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -81,7 +81,7 @@ function getCMD(client, message, input) {
   return string.charAt(0).toUpperCase() + string.slice(1);
  }
  if(!cmd.aliases.lenght) {
-  alliaseslist = cmd.aliases.lenght ? cmd.aliases.map((a) => a.join(', ')) : '`None`';
+  alliaseslist = cmd.aliases.lenght ? cmd.aliases.map(a => a.join(', ')) : '`None`';
  } else {
   alliaseslist = "None";
  }
