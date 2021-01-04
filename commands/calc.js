868d263cc64c2f89d7a4f1b3945077a4a6be39a2
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-04 18:55:36 +0100

    Add math command

---
+++ commands/calc.js
@@ -0,0 +1,48 @@
+const math = require('math-expression-evaluator');
+const stripIndents = require('common-tags').stripIndents;
+const Discord = require("discord.js");
+const config = require("../config");
+const prefix = config.prefix;
+
+module.exports = {
+ name: "calc",
+ aliases: ["math"],
+ description: "Discord calculator",
+ category: "Fun",
+ usage: "calc <task>",
+ run: async (client, message, args) => {
+ try {
+  if(args.length < 1) {
+   return message.channel.send({embed: {
+    color: 16734039,
+    description: "You must provide a equation to be solved on the calculator! (eg. 9 + 10)"
+   }}) 
+  }
+  const question = args.join(' ');
+  let answer;
+  if(question.indexOf('9 + 10') > -1) {
+   answer = '21 (🤣 XD, You found easter-egg)';
+  } else {
+   try {
+    answer = math.eval(question);
+    const calc = new Discord.MessageEmbed()
+    .setTitle("Calculator")
+    .setColor("RANDOM")
+    .addField("Question: ", `${question}`)
+    .addField("Answer: ", `${answer}`)
+   return message.channel.send(calc);
+   } catch (err) {
+    message.channel.send({embed: {
+     color: 16734039,
+     description: "Invalid math equation: " + `${err}`
+   }});
+   }
+  }
+ } catch (err) {
+  message.channel.send({embed: {
+   color: 16734039,
+   description: "Invalid math equation: " + `${err}`
+  }});
+ }
+}
+}
