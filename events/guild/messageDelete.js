87414bd71846915ab098d2b4ef9bc0367bc55095
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-09 21:29:37 +0100

    Update messageDelete.js

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -7,8 +7,9 @@ module.exports = async (client, message) => {
   if(message.channel.type === 'dm') return;  
   if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return;  
   if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return;  
-  var log = message.guild.channels.cache.find(c => c.name === 'log');  
-  if(!log) return;  
+  const log = message.guild.channels.cache.find(c => c.name === 'log');  
+  if(!log) return;
+  const final = message.substr(0, 500); // Limit characters
   const event = new Discord.MessageEmbed()  
    .setTitle(`Message Deleted in ${message.channel}`)
    .setColor('RANDOM')
@@ -16,15 +17,15 @@ module.exports = async (client, message) => {
    .addField("Channel", `${message.channel.name} (ID: ${message.channel.id})`)
    .addField("Message ID", `(${message.id})`)
    .addField("Send By", `<@${message.author.id}> (ID: ${message.author.id})`)
-   .addField("Message", "\`\`\`" + `${message.substr(0, 500)}` + "`\`\`\`")
+   .addField("Message", "\`\`\`" + `${final}` + "`\`\`\`")
    .setTimestamp()
    .setFooter(message.guild.name, message.guild.iconURL())
    log.send(event);
  } catch (err) {
   const embed = new Discord.MessageEmbed()
    .setColor("#FF0000")
-   .setTitle("Error!")
-   .setDescription("**Error Code:** *" + err + "*")
+   .setTitle("I meet error!")
+   .setDescription("Error Code: \`\`\`" + err + "\`\`\`")
    .setTimestamp()
   return log.send(embed);
  }
