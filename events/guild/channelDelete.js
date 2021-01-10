507f4cd6845a56fba1910ccfdeec324fc2389ef5
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-10 14:24:59 +0100

    Update channelDelete

--- events/guild/channelDelete.js
+++ events/guild/channelDelete.js
@@ -1,37 +1,41 @@
 const Discord = require('discord.js');
 
 module.exports = async (client, channel) => {
-try {
- if (!channel.guild) return;
- if (!channel.guild.member(client.user).hasPermission("EMBED_LINKS")) return;
- if (!channel.guild.member(client.user).hasPermission("VIEW_AUDIT_LOG")) return;
- var logChannel = channel.guild.channels.cache.find(c => c.name === "log");
- if (!logChannel) return;
- if (channel.type === "text") {
-  var roomType = "Text";
- } else if (channel.type === "voice") {
-  var roomType = "Voice";
- } else if (channel.type === "category") {
-  var roomType = "Category";
+ try {
+  if (!channel.guild) return;
+  if (!channel.guild.member(client.user).hasPermission("EMBED_LINKS")) return;
+  if (!channel.guild.member(client.user).hasPermission("VIEW_AUDIT_LOG")) return;
+  const log = channel.guild.channels.cache.find(log => log.name === "log")
+  if(!log) return;
+  if (channel.type === "text") {
+   var type = "Text";
+  } else if (channel.type === "voice") {
+   var type = "Voice";
+  } else if (channel.type === "category") {
+   var type = "Category";
+  } else if (channel.type === "news") {
+   var type = "News Feed";
+  } else if (channel.type === "store") {
+   var type = "Store channel";
+  } else if (!channel.type) {
+   var type = "?";
+  }
+  channel.guild.fetchAuditLogs().then(logs => {
+   var userid = logs.entries.first().executor.id;
+   var uavatar = logs.entries.first().executor.avatarURL();
+   const event = new Discord.MessageEmbed()
+    .setTitle("Channel Deleted")
+    .setThumbnail(uavatar)
+    .addField("Channel name", `<#${channel.id}> (ID: ${channel.id})`)
+    .addField("Channel type", `${type}`)
+    .addField("Created at", `${channel.createdAt}`)
+    .addField("Created by", `<@${userid}> (ID: ${userid})`)
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setFooter(channel.guild.name, channel.guild.iconURL());
+   log.send(event);
+  });
+ } catch (err) {
+  console.log(err)
  }
- channel.guild.fetchAuditLogs().then(logs => {
-  var userID = logs.entries.first().executor.id;
-  var userAvatar = logs.entries.first().executor.avatarURL();
-  let channelDelete = new Discord.MessageEmbed()
-   .setTitle("**CHANNEL DELETE**")
-   .setThumbnail(userAvatar)
-   .setDescription(`**\n**:white_check_mark: Successfully \`\`DELETE\`\` **${roomType}** channel.\n\n**Channel Name:** \`\`${channel.name}\`\` (ID: ${channel.id})\n**By:** <@${userID}> (ID: ${userID})`)
-   .setColor("RANDOM")
-   .setTimestamp()
-   .setFooter(channel.guild.name, channel.guild.iconURL());
-  logChannel.send(channelDelete);
- });
-} catch (err) {
- let embed = new Discord.MessageEmbed()
-  .setColor("#FF0000")
-  .setTitle("Error!")
-  .setDescription("**Error Code:** *" + err + "*")
-  .setTimestamp();
- return logChannel.send(embed);
-}
 }
