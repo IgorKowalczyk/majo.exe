9eb3c67a8f7467fe183f8fbb077a962cb33cd3ba
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-10 12:19:04 +0100

    Try to fix logs

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -7,7 +7,7 @@ module.exports = async (client, message) => {
   if(message.channel.type === 'dm') return;  
   if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return;  
   if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return;  
-  var log = message.guild.channels.cache.find(c => c.name === 'log');  
+  var log = message.guild.channels.cache.find(channel => channel.name === 'log')
   if(!log) return;
   const final = message.substr(0, 500); // Limit characters
   const event = new Discord.MessageEmbed()  
