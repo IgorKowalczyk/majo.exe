7e3992a9809c3d0bc54624784069b025f4bb696b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-12 19:48:32 +0100

    Try to config web-dashboard

--- commands/info.js
+++ commands/info.js
@@ -12,22 +12,27 @@ module.exports = {
  usage: "info",
  run: async (client, message, args) => {
   try {
+   if(config.dashboard = "true") {
+    webpanel = `[Web-Panel](${config.domain}) |`;
+   } else {
+    webpanel = " ";
+   }
    const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
    const embed = new Discord.MessageEmbed()
     .setTitle(`Information for developers`, message.guild.iconURL({ dynamic: true, format: 'png'}))
     .setColor("RANDOM")
     .setDescription(`My prefix is: \`${config.prefix}\`\n`)
-    .addField('Head developer', `${config.author} [Portfolio](${config.authorwebsite})`)
+    .addField('Head developer', `${config.author} [[Portfolio](${config.authorwebsite})]`, true)
     .setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
-    .addField('Node', `${process.version}`)
-    .addField('OS', `Ubuntu 20.04 (Linux)`)
-    .addField('Stack', `20 (Support date: 01.04.2025)`)
-    .addField('Uptime', `${duration}`)
-    .addField('Guild Count', `${client.guilds.cache.size}`)
-    .addField('User Count', `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`)
-    .addField('Channel Count', `${client.channels.cache.size}`)
-    .addField('Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB RAM`)
-    .addField('Useful Links', `[Official server](${config.server}) | [Web-Panel](${config.website}) | [Invite me](https://discord.com/oauth2/authorize/?permissions=8&scope=bot&client_id=${client.user.id})`)
+    .addField('Node', `${process.version}`, true)
+    .addField('OS', `Ubuntu 20.04 (Linux)`, true)
+    .addField('Stack', `20 (Support date: 01.04.2025)`, true)
+    .addField('Uptime', `${duration}`, true)
+    .addField('Guild Count', `${client.guilds.cache.size}`, true)
+    .addField('User Count', `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`, true)
+    .addField('Channel Count', `${client.channels.cache.size}`, true)
+    .addField('Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB RAM`, true)
+    .addField('Useful Links', `[Official server](${config.server}) | ${webpanel} [Invite me](https://discord.com/oauth2/authorize/?permissions=8&scope=bot&client_id=${client.user.id})`)
     .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
    message.channel.send(embed);
   } catch(err) {
