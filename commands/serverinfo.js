93318d44aa1665b2d88b2e1c3fff4486a00e1416
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-15 19:59:56 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -74,6 +74,7 @@ module.exports = {
    .setTimestamp()
   message.channel.send(embed);
   } catch(err) {
+   console.log(err);
    message.channel.send({embed: {
     color: 16734039,
     description: "Something went wrong... :cry:"
