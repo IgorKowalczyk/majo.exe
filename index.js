daccac60602ea4ba88773246f879577469b1d774
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-17 19:35:05 +0100

    Try to shard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.client({disableEveryone: true,});
+//const client = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
 const http = require("http");
 const db = require("quick.db");
@@ -10,17 +10,17 @@ const token = config.token;
 const prefix = config.prefix;
 
 /* Login and Commands */
-if (token) {
+//if (token) {
  client.commands = new Discord.Collection();
  client.aliases = new Discord.Collection();
  client.queue = new Map(); 
  ['command', 'event'].forEach(handler => {
   require(`./handlers/${handler}`)(client);
  });
- client.login(token);
-} else {
+// client.login(token);
+//} else {
  console.log(chalk.red.bold("Error:") + chalk.red(" Bot token is not provided! To give your bot life, you need to enter token value in the ") + chalk.grey.italic.bold(".env") +  chalk.red(" file - ") + chalk.grey.italic.bold("TOKEN=Your_Token ") + chalk.red.underline.bold("REMEMBER: Token is super-secret - do not share it with anyone!"));
-}
+//}
 /* /Login and Commands*/
 
 process.on("unhandledRejection", (err) => {
--- shard.js
+++ shard.js
@@ -3,7 +3,7 @@ const { Discord } = require('discord.js');
 const config = require("./config");
 const token = config.token;
 
-const client = new Discord('./index.js', {
+const client = new Discord.Client('./index.js', {
  totalShards: 'auto',
  token: token
 });
