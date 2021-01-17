99dd84db91cce5c03f8e16d56fc54313c84afd68
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-17 20:08:23 +0100

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const config = require("./config");
 const token = config.token;
 
-const client = new Discord.Client('./index.js', {
+const client = new Discord('./index.js', {
  totalShards: 'auto',
  token: token
 });
