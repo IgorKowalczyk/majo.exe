4a6685438c66d860a0321703e0566eecaa77e15e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-01-17 19:41:37 +0100

    Add shards

--- events/client/shardCreate.js
+++ events/client/shardCreate.js
@@ -1,4 +1,3 @@
-/* This is not used file, only for testing */
 const Discord = require("discord.js");
 const moment = require("moment");
 const timestamp = `[${moment().format("DD-MM-YY H:m:s")}]:`;
--- index.js
+++ index.js
@@ -4,23 +4,25 @@ const fs = require("fs");
 const http = require("http");
 const db = require("quick.db");
 const chalk = require("chalk");
-
 const config = require("./config");
 const token = config.token;
-const prefix = config.prefix;
+const client = new Discord.Client({
+ totalShards: 'auto',
+ token: token
+});
 
 /* Login and Commands */
-//if (token) {
+if (token) {
  client.commands = new Discord.Collection();
  client.aliases = new Discord.Collection();
  client.queue = new Map(); 
  ['command', 'event'].forEach(handler => {
   require(`./handlers/${handler}`)(client);
  });
-// client.login(token);
-//} else {
+ client.spawn();
+} else {
  console.log(chalk.red.bold("Error:") + chalk.red(" Bot token is not provided! To give your bot life, you need to enter token value in the ") + chalk.grey.italic.bold(".env") +  chalk.red(" file - ") + chalk.grey.italic.bold("TOKEN=Your_Token ") + chalk.red.underline.bold("REMEMBER: Token is super-secret - do not share it with anyone!"));
-//}
+}
 /* /Login and Commands*/
 
 process.on("unhandledRejection", (err) => {
