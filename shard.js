32e6d383592dbf0008edbe1a96a22b7407466024
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-06 21:04:32 +0200

    F*** this file

--- shard.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const config = require("./config");
-const token = config.token;
-
-const client = new Discord.ShardingManager('./index.js', {
- token: token
-});
-const shards = 20;
-client.spawn(shards, 15000, false);
