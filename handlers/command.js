8c8668ccba9b471225248a229479393cfb9ac5ce
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-07 09:24:42 +0200

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,16 +1,30 @@
-const {readdirSync} = require('fs');
-const chalk = require("chalk");
+const { readdirSync } = require('fs');
+const ascii = require('ascii-table');
+const table = new ascii("Commands");
+table.setHeading('Command', 'Load status');
+
+/* Code by João Victor (https://github.com/Joao-Victor-Liporini). Thanks ❤️ */
 
 module.exports = (client) => {
- const commands = readdirSync('./commands/').forEach(file => {
-  const pull = require(`../commands/${file}`);
-  client.commands.set(pull.name, pull);
-  if(pull.aliases) {
-   pull.aliases.forEach(alias => {
-    client.aliases.set(alias, pull.name);
-   });
+ readdirSync('./commands/').forEach(dir => {
+  const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+  for (let file of commands) {
+   let pull = require(`../commands/${file}`);
+   try {
+    if (typeof pull.name != 'string' || typeof pull != 'object') throw new Error('Missing a name or name is not a string');
+    if (pull.category && typeof pull.category !== 'string') throw new Error('Category is not a string');
+    if (pull.description && typeof pull.description !== 'string') throw new Error('Description is not a string');
+    if (pull.usage && typeof pull.usage !== 'string') throw new Error('Usage is not a string');
+    if (pull.name) {
+     client.commands.set(pull.name, pull);
+     table.addRow(file, '√')
+    }
+    if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+   } catch (error) {
+    table.addRow(file, `X -> ${error}`)
+    continue;
+   }
   }
  });
- console.log(chalk.blue("Loading commands..."));
- console.log(chalk.blue("Successfully loaded ") + chalk.blue.underline(`${client.commands.size}`) + chalk.blue(" commands!"));
+ console.log(table.toString());
 }
