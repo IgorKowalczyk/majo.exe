b92c9a978732004f8ccd93ee180c2829a17273cc
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-07 12:05:17 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -30,6 +30,7 @@ module.exports = (client) => {
      `${emoji} | ${client.guilds.cache.size} servers`,
      `${emoji} | ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members`,
      `${emoji} | ${config.prefix} help`,
+     `${emoji} | ${client.guilds.cache.size} #Verification`,
     );	
    }	
    const random = Math.floor(Math.random() * (statuslist.length - 1) + 1);
