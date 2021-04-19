6c9bdd50ebd232387b0376ade4ced0182ba806d0
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-19 10:25:15 +0200

    Test

--- commands/sad.js
+++ commands/sad.js
@@ -11,9 +11,20 @@ module.exports = {
  category: "Fun",
  usage: "sad <image>",
  run: async (client, message, args) => {
-  if (args.length >= 1) {
-   Jimp.read(args[0]).then(function (image) {
-     image.greyscale(function(err, image) {
+  function attachIsImage(msgAttach) {
+   var url = msgAttach.url;
+   return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
+  }
+  if (message.attachments.size > 0) {
+   if (message.attachments.every(attachIsImage)){
+    return message.channel.send({embed: {
+      color: 16734039,
+      description: "Please enter a .png image!"
+    }})
+   }
+  } else {
+   Jimp.read(message.attachments).then(function (image) {
+    image.greyscale(function(err, image) {
      image.quality(2)    
      let outputfile = "./output/" + Math.random().toString(36).substr(2, 5) + "sad." + image.getExtension(); // create a random name for the output file
      image.write(outputfile, function () {
@@ -37,11 +48,6 @@ module.exports = {
      description: "Invaild image!"
     }})
    });    
-  } else {
-   return message.channel.send({embed: {
-    color: 16734039,
-    description: "Please enter a image!"
-   }})
   }
  }
 }
