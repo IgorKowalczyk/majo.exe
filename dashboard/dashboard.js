05da5df67ffa777d0853c7ee5882863ccf5b3879
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-19 16:34:31 +0200

    Small bug fixes

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -9,7 +9,9 @@ const config = require("../config");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const GuildSettings = require("./models/settings");
+const { readdirSync } = require('fs');
 const app = express();
+app.use(express.static('dashboard/static'));
 const MemoryStore = require("memorystore")(session);
 
 const port = process.env.PORT || 6565;
@@ -32,7 +34,9 @@ module.exports = async (client) => {
   process.nextTick(() => done(null, profile));
  }));
  app.use(session({
-  store: new MemoryStore({ checkPeriod: 86400000 }),
+  store: new MemoryStore({
+   checkPeriod: 86400000
+  }),
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: false,
@@ -47,6 +51,7 @@ module.exports = async (client) => {
  app.use(bodyParser.urlencoded({
   extended: true
  }));
+
  const renderTemplate = (res, req, template, data = {}) => {
   var hostname = req.headers.host;
   var pathname = url.parse(req.url).pathname;
@@ -76,24 +81,30 @@ module.exports = async (client) => {
  console.log("Setting up dashboard endpoints...");
  const checkAuth = (req, res, next) => {
   if (req.isAuthenticated()) return next();
-   req.session.backURL = req.url;
-   res.redirect("/login");
-  }
+  req.session.backURL = req.url;
+  res.redirect("/login");
+ }
+
+ // Login endpoint.
  app.get("/login", (req, res, next) => {
-  if (req.session.backURL) {
-   req.session.backURL = req.session.backURL;
-  } else if (req.headers.referer) {
-   const parsed = url.parse(req.headers.referer);
-   if (parsed.hostname === app.locals.domain) {
-    req.session.backURL = parsed.path;
+   if (req.session.backURL) {
+    req.session.backURL = req.session.backURL;
+   } else if (req.headers.referer) {
+    const parsed = url.parse(req.headers.referer);
+    if (parsed.hostname === app.locals.domain) {
+     req.session.backURL = parsed.path;
+    }
+   } else {
+    req.session.backURL = "/";
    }
-  } else {
-   req.session.backURL = "/";
-  }
-  next();
- },
- passport.authenticate("discord"));
- app.get("/callback", passport.authenticate("discord", { failureRedirect: "/" }), (req, res) => {
+   next();
+  },
+  passport.authenticate("discord"));
+
+ // Callback endpoint.
+ app.get("/callback", passport.authenticate("discord", {
+  failureRedirect: "/"
+ }), (req, res) => {
   if (req.session.backURL) {
    const url = req.session.backURL;
    req.session.backURL = null;
@@ -103,8 +114,31 @@ module.exports = async (client) => {
   }
  });
 
+ // Features list redirect endpoint.
+ app.get("/features", (req, res) => {
+  renderTemplate(res,  req, "features.ejs", {
+   readdirSync: readdirSync,
+   perms: Discord.Permissions
+  });
+ });
+
+ // Server redirect endpoint.
+ if (config.server) {
+  app.get("/server", (req, res) => {
+   res.redirect(config.server);
+  });
+ }
+
+ // Github redirect endpoint.
+ if (config.github) {
+ app.get("/github", (req, res) => {
+  res.redirect(`https://github.com/` + config.github);
+  });
+ }
+  
  // Logout endpoint.
- app.get("/logout", function (req, res) {
+ app.get("/logout", function(req, res) {
+  // We destroy the session || logout user.
   req.session.destroy(() => {
    req.logout();
    res.redirect("/");
@@ -118,50 +152,23 @@ module.exports = async (client) => {
 
  // Dashboard endpoint.
  app.get("/dashboard", checkAuth, (req, res) => {
-  renderTemplate(res, req, "dashboard.ejs", { perms: Discord.Permissions });
+  renderTemplate(res, req, "dashboard.ejs", {
+   perms: Discord.Permissions
+  });
  });
 
  // Settings endpoint.
- /*
  app.get("/dashboard/:guildID", checkAuth, async (req, res) => {
+  // Vlidate the request, check if guild exists, member is in guild and if member has minimum permissions
   const guild = client.guilds.cache.get(req.params.guildID);
   if (!guild) return res.redirect("/dashboard");
   const member = guild.members.cache.get(req.user.id);
   if (!member) return res.redirect("/dashboard");
   if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/dashboard");
-  var storedSettings = await GuildSettings.findOne({ gid: guild.id });
-  if (!storedSettings) {
-   const newSettings = new GuildSettings({
-    gid: guild.id
-   });
-   await newSettings.save().catch(()=>{});
-   storedSettings = await GuildSettings.findOne({ gid: guild.id });
-  }
-  renderTemplate(res, req, "settings.ejs", { guild, settings: storedSettings, alert: null });
+  renderTemplate(res, req, "server.ejs", {
+   guild: guild,
+  });
  });
- */
-
- // Settings endpoint.
- /*
- app.post("/dashboard/:guildID", checkAuth, async (req, res) => {
-  const guild = client.guilds.cache.get(req.params.guildID);
-  if (!guild) return res.redirect("/dashboard");
-  const member = guild.members.cache.get(req.user.id);
-  if (!member) return res.redirect("/dashboard");
-  if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/dashboard");
-  var storedSettings = await GuildSettings.findOne({ gid: guild.id });
-  if (!storedSettings) {
-   const newSettings = new GuildSettings({
-    gid: guild.id
-   });
-   await newSettings.save().catch(()=>{});
-   storedSettings = await GuildSettings.findOne({ gid: guild.id });
-  }
-  storedSettings.prefix = req.body.prefix;
-  await storedSettings.save().catch(() => {});
-  renderTemplate(res, req, "settings.ejs", { guild, settings: storedSettings, alert: "Your settings have been saved." });
- });
- */
  console.log("All dashboard process done... Starting in web");
  app.listen(port, null, null, () => console.log(`Dashboard is up and running on port ${port}.`));
 };
