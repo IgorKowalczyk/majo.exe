7f69c286c89e3a6913f3638920851184cf8dd50f
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-19 11:59:38 +0200

    Fix file path

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ if (process.env.TOKEN) {
 }
 
 if (process.env.DASHBOARD = "true") {
- const Dashboard = require("../../dashboard/dashboard");
+ console.log("Getting dashboard config file...")
+ const Dashboard = require("./dashboard/dashboard");
+ console.log("Done!")
  Dashboard(client);
 } else {
   console.log(chalk.blue('Dashboard is now disabled. To enable it change the "DASHBOARD" value in .env file to "true" (Now is set to "') + chalk.blue.underline(`${config.dashboard}`) + chalk.blue('")') + chalk.blue.underline("\n" + client.user.username + " stats: " + `${client.guilds.cache.size}` + " servers, " + `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}` + " members"));
