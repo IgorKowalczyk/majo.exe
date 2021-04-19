7ff18d911cc134a1d43b85f2fa01dd9647be0e65
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-19 16:45:03 +0200

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -28,6 +28,7 @@ module.exports = async (client) => {
   clientID: config.id,
   clientSecret: config.clientSecret,
   callbackURL: `${config.domain}${config.port != 80 ? "" : `:${config.port}`}/callback`,
+  response_type: `token`,
   scope: ["identify", "guilds"]
  },
  (accessToken, refreshToken, profile, done) => { 
