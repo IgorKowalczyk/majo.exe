76cf02539151aa48487abca2d83ac8bba5533b82
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-19 13:04:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ if (process.env.DASHBOARD = "true") {
  Dashboard = require("./dashboard-run");
  console.log("Done!")
  Dashboard(client);
+ 
 } else {
   console.log(chalk.blue('Dashboard is now disabled. To enable it change the "DASHBOARD" value in .env file to "true" (Now is set to "') + chalk.blue.underline(`${config.dashboard}`) + chalk.blue('")') + chalk.blue.underline("\n" + client.user.username + " stats: " + `${client.guilds.cache.size}` + " servers, " + `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}` + " members"));
 }
