64c4e80b1f379458477311dc0ac1f2d3c566668e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-21 19:29:14 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -13,7 +13,7 @@ module.exports = {
    const embed = new Discord.MessageEmbed()
     .setTitle(`<a:sucess:759354039242063903> YAY!`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
     .setColor("RANDOM")
-    .setDescription(`:tada: [Click this link to invite me! \[Recomended!\]](https://discord.com/oauth2/authorize/?permissions=${config.premissions}&scope=${config.scopes}&client_id=${client.user.id}\nOr [click this link to invite me \[as root, not recomended\]](https://discord.com/oauth2/authorize/?permissions=8&scope=${config.scopes}&client_id=${client.user.id}`)
+    .setDescription(`:tada: [Click this link to invite me!](https://discord.com/oauth2/authorize/?permissions=${config.premissions}&scope=${config.scopes}&client_id=${client.user.id} [Recomended!]\nOr [click this link to invite me **as root**](https://discord.com/oauth2/authorize/?permissions=8&scope=${config.scopes}&client_id=${client.user.id}`)
     .setTimestamp()
     .setFooter(`~${client.user.username} created by ${config.author}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
    return message.channel.send(embed);
