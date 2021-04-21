dccdea54551b7005a1cb8ef5b1ada647dd04627c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-21 16:53:17 +0200

    Update wanted.js

--- commands/wanted.js
+++ commands/wanted.js
@@ -12,7 +12,7 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    const User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.member;
-   const wanted = await new canvacord.Canvas.wanted(User.user.displayAvatarURL({ dynamic: false, format: 'png', size: 2048 }));
+   const wanted = await canvacord.Canvas.wanted(User.user.displayAvatarURL({ dynamic: false, format: 'png', size: 2048 }));
    const attachment = new Discord.MessageAttachment(wanted, "wanted.png");
    return message.channel.send(attachment);
   } catch (err) {
@@ -27,5 +27,5 @@ module.exports = {
 
 ///////////////////////
 // Constructor
-// new canvacord.Canvas.function
+// canvacord.Canvas.function
 ///////////////////////
