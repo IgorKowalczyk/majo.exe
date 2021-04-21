b642240a831e33b22b7652c362e1f022b4d22361
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-21 14:14:31 +0200

    Update tigeered.js

--- commands/tigeered.js
+++ commands/tigeered.js
@@ -11,9 +11,9 @@ module.exports = {
  usage: "trigerred [user mention]",
  run: async (client, message, args) => {
   try {
-   let User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.member;
-   let triggered = await canvacord.trigger(User.displayAvatarURL({ format: "png", dynamic: false }));
-   let attachment = new MessageAttachment(triggered, "triggered.gif");
+   const User = await message.mentions.members.first() || message.member;
+   const triggered = await canvacord.trigger(User.displayAvatarURL({ dynamic: false, format: 'png', size: 2048 }));
+   const attachment = new MessageAttachment(triggered, "triggered.gif");
    return message.channel.send(attachment);
   } catch (err) {
    console.log(err);
