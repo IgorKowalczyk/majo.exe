c20a7d787f96cec0a4e8ac69c35dc7faefb9546b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-21 15:24:38 +0200

    Update commands

--- commands/wasted.js
+++ commands/wasted.js
@@ -12,8 +12,8 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    const User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.member;
-   const triggered = await canvacord.wasted(User.user.displayAvatarURL({ dynamic: false, format: 'png', size: 2048 }));
-   const attachment = new Discord.MessageAttachment(triggered, "wasted.png");
+   const wasted = await canvacord.wasted(User.user.displayAvatarURL({ dynamic: false, format: 'png', size: 2048 }));
+   const attachment = new Discord.MessageAttachment(wasted, "wasted.png");
    return message.channel.send(attachment);
   } catch (err) {
    console.log(err);
