7017eb455b8179e6f69e3dd6ebe00a8c04497147
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-21 19:10:05 +0200

    Update commands

--- commands/invite.js
+++ commands/invite.js
@@ -11,7 +11,7 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    const embed = new Discord.MessageEmbed()
-    .setTitle(`:white_check_mark: YAY!`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+    .setTitle(`<a:sucess:759354039242063903> YAY!`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
     .setColor("RANDOM")
     .setDescription(`:tada: [Click this link to invite me! \[Recomended!\]](https://discord.com/oauth2/authorize/?permissions=${config.premissions}&scope=${config.scopes}&client_id=${client.user.id}\nOr [click this link to invite me \[as root, not recomended\]](https://discord.com/oauth2/authorize/?permissions=8&scope=${config.scopes}&client_id=${client.user.id}`)
     .setTimestamp()
