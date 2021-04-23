8417dafbfa703b87785d562d355e113904812019
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-23 19:35:49 +0200

    add console error log

--- commands/chat.js
+++ commands/chat.js
@@ -38,6 +38,7 @@ module.exports = {
      message.channel.send(embed);
     }
    } catch(err) {
+    console.log(err);
     message.channel.send({embed: {
      color: 16734039,
      description: "Something went wrong... :cry:"
