4e3de176166e91d4cdbf9eee7687edcb9208b890
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-28 11:06:32 +0200

    Update

--- commands/play.js
+++ commands/play.js
@@ -145,20 +145,20 @@ module.exports = {
      thumbnail: songInfo.thumbnail,
      duration: songInfo.durationFormatted,
     };
-    if (!song) {
-     console.log("Song not found");
-    }
-    if(!song.title && !song.url && !song.thumbnail && !song.duration) {
-      console.log("Someting went wrong with url");
-    }
    } catch (error) {
     message.channel.send({embed: {
      color: 16734039,
      description: "Something went wrong... :cry:"
     }})
    }
-   
+
   }
+  if (!song) {
+    console.log("Song not found");
+   }
+   if(!song.title && !song.url && !song.thumbnail && !song.duration) {
+     console.log("Someting went wrong with url");
+   }
   let thumb = "https://images-ext-2.discordapp.net/external/55DLQjqMFAc-wAiT7O1NJS158tv6OUhsSHmBXmJ0Pkg/%3Fsqp%3D-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE%3D%26rs%3DAOn4CLBz4zlh_7gUzfPyMIc3OcanVj2xyw/https/i.ytimg.com/vi/jJLpszZ6o-c/hq720.jpg";
   if (song.thumbnail === undefined) thumb = "https://images-ext-2.discordapp.net/external/55DLQjqMFAc-wAiT7O1NJS158tv6OUhsSHmBXmJ0Pkg/%3Fsqp%3D-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE%3D%26rs%3DAOn4CLBz4zlh_7gUzfPyMIc3OcanVj2xyw/https/i.ytimg.com/vi/jJLpszZ6o-c/hq720.jpg";
   else thumb = song.thumbnail.url;
