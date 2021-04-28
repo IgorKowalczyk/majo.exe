af720e7bfffcf09b42c9417ce43a29c8090dfcfb
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-28 16:18:18 +0200

    Update

--- utilities/play.js
+++ utilities/play.js
@@ -4,6 +4,7 @@ const Discord = require("discord.js");
 const createBar = require("string-progressbar");
 const lyricsFinder = require("lyrics-finder");
 const config = require("../config");
+// const { delete } = require("snekfetch");
 
 module.exports = {
  async play(song, message, client, filters, silient) {
@@ -14,6 +15,10 @@ module.exports = {
    const endembed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setDescription(`💿 Music queue ended so I'm leaving the voice channel.`);
+   try {
+    playingMessage.delete({timeout: 1000});
+   } catch (err) {
+   }
    return queue.textChannel.send(endembed);
   }
   let stream = null;
