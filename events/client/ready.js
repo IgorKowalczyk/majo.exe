9331de2f06e68fe44e3ccaf1c719f2b2ead4b339
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-04-30 15:12:28 +0200

    Update...

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,39 +4,40 @@ const config = require("../../config");
 
 module.exports = (client) => {
  try {
-  setInterval(() => {
-   const emojis = ["😆", "😄", "😎", "😂", "🥳", "😘", "😜", "🤑", "😁", "😉", "🥰", "😍", "🤯", "🥶", "🤩", "😇", "😊", "☺️", "😌", "😋"];
-   const emoji = emojis[Math.floor(Math.random()*emojis.length)];
-   var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
-   const enddate = new Date().getFullYear() + "/06/13";
-   const enddateEEP = new Date().getFullYear() + "/04/18";
-   const statuslist = [];
-   if (date == enddate) {
-    statuslist.push(
-     `🎉 ${client.guilds.cache.size} servers 🎉`,
-     `🎉 ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members 🎉`,
-     `🎉 ${config.prefix} help 🎉`,	
-     `🎉 Happy Birthday Discord! 🎉`,
-    );	
-   } else if (date == enddateEEP) {	
-    statuslist.push(	
-     `🔥 ${client.guilds.cache.size} servers 🔥`,
-     `🔥 ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members 🔥`,
-     `🔥 ${config.prefix} help 🔥`,	
-     `🔥 EEP 4 LIFE (04/18)! 🔥`,
-    );	
-   } else {	
-    statuslist.push(	
-     `${emoji} | ${client.guilds.cache.size} servers`,
-     `${emoji} | ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members`,
-     `${emoji} | ${config.prefix} help`,
-     `${emoji} | Waiting for verification! (${client.guilds.cache.size} guilds 🥰)`,
-    );	
-   }	
-   const random = Math.floor(Math.random() * (statuslist.length - 1) + 1);
-   client.user.setActivity(statuslist[random], { type: 'WATCHING' });
-  }, 10000);
+  //setInterval(() => {
+   // const emojis = ["😆", "😄", "😎", "😂", "🥳", "😘", "😜", "🤑", "😁", "😉", "🥰", "😍", "🤯", "🥶", "🤩", "😇", "😊", "☺️", "😌", "😋"];
+   // const emoji = emojis[Math.floor(Math.random()*emojis.length)];
+   // var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+   // const enddate = new Date().getFullYear() + "/06/13";
+   // const enddateEEP = new Date().getFullYear() + "/04/18";
+   // const statuslist = [];
+   // if (date == enddate) {
+   // statuslist.push(
+   //  `🎉 ${client.guilds.cache.size} servers 🎉`,
+   //  `🎉 ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members 🎉`,
+   //  `🎉 ${config.prefix} help 🎉`,	
+   //  `🎉 Happy Birthday Discord! 🎉`,
+   // );	
+  // } else if (date == enddateEEP) {	
+  //  statuslist.push(	
+//`🔥 ${client.guilds.cache.size} servers 🔥`,
+ //    `🔥 ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members 🔥`,
+ //    `🔥 ${config.prefix} help 🔥`,	
+  //   `🔥 EEP 4 LIFE (04/18)! 🔥`,
+   // );	
+ //  } else {	
+  //  statuslist.push(	
+  //   `${emoji} | ${client.guilds.cache.size} servers`,
+  //   `${emoji} | ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members`,
+ //    `${emoji} | ${config.prefix} help`,
+  //   `${emoji} | Waiting for verification! (${client.guilds.cache.size} guilds 🥰)`,
+ //   );	
+ //  }	
+ //  const random = Math.floor(Math.random() * (statuslist.length - 1) + 1);
+ //  client.user.setActivity(statuslist[random], { type: 'WATCHING' });
+  //}, 10000);
   
+  client.user.setActivity("🕯️", { type: 'WATCHING' });
   client.user.setStatus("online");
   const datelog = new Date();
    currentDate = datelog.getDate();
