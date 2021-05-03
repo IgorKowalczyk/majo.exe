db3616334ee389552d14e0c5f525a756c59581a4
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-03 21:18:20 +0200

    Update ship command

--- commands/ship.js
+++ commands/ship.js
@@ -6,48 +6,22 @@ module.exports = {
  aliases: [],
  description: "Ship members",
  category: "Fun",
- usage: "ship <member> (member)",
+ usage: "ship <member> <member>",
  run: async (client, message, args) => {
   try {
-   let user1;
-   let user2
-   const users =  message.mentions.users.array();
-   if (users[0]) {
-    console.log("M1 " + users[0])
-    
-    user1 = users[0];
-   } else if(args[0]) {
-    console.log("A1" + args[0]);
-    user1 = args[0];
-   } else {
-    user1 = message.author;
+   const user1 = args[0];
+   if (!user1) {
+    return message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | Please mention a user to ship!"
+    }})     
    }
-   if (user1 == message.author) {
-    if(args[0]) {
-     console.log("A2 " + args[0]);
-     user2 = args[0];
-    } else if(message.mentions.member.first()) {
-     console.log("M2 " + message.mentions.member.first());
-     user2 = message.mentions.member.first();
-    } else {
-     return message.channel.send({embed: {
-      color: 16734039,
-      description: "❌ | Please mention a user to ship!"
-     }}) 
-    }
-   } else {
-    if(args[1]) {
-     console.log("A2 " + args[1]);
-     user2 = args[1];
-    } else if(users[1]) {
-     console.log("M2 " + users[1]);
-     user2 = users[1];
-    } else {
-     return message.channel.send({embed: {
-      color: 16734039,
-      description: "❌ | Please mention a user to ship!"
-     }}) 
-    }
+   const user2 = args[1]
+   if (!user2) {
+    return message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | Please mention a secound user to ship!"
+    }}) 
    }
    const ship = Math.floor(Math.random() * 100) + 1;
    const bar = progressbar(100, ship, 10, "<:bar:838757737327755335>", "<:bar2:838757737596190782>", "💔 ", " ❤️", false)
