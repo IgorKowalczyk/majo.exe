92d2799378e7c7b26437bca712460af6b14f4550
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-04 19:42:34 +0200

    Add new commands

---
+++ commands/furry.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const booru = require('booru');
+
+module.exports = {
+ name: "furry",
+ aliases: [],
+ description: "Search internet for your furry!",
+ category: "NSFW",
+ usage: "furry",
+ run: async (client, message, args) => {
+  try {
+   if (!message.channel.nsfw) {
+    return message.channel.send({embed: {
+     color: 16734039,
+     description: "💢 | You can use this command only in an NSFW Channel!"
+    }})
+   }
+   const query = message.content.split(/\s+/g).slice(1).join(" ");
+   if (!query) {
+    return message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | You must enter a text to search for furry!"
+    }})
+   }
+   booru.search('e6', [query], {nsfw: true, limit: 1, random: true })
+    .then(images => {
+     for (let image of images) {
+      const embed = new Discord.MessageEmbed()
+       .setTitle(":smirk: Furry")
+       .setImage(image.fileUrl)
+       .setColor('RANDOM')
+       .setURL(image.fileUrl);
+      return message.channel.send({ embed });
+     }
+    }).catch(err => {
+     if (err.name === 'booruError') {
+      return message.channel.send({embed: {
+       color: 16734039,
+       description: `❌ | No results found for: ${query}`
+      }})
+     } else {
+      return message.channel.send({embed: {
+       color: 16734039,
+       description: `❌ | No results found for: ${query}`
+      }})
+     }
+    })
+  } catch (err) {
+   console.log(err);
+   message.channel.send({embed: {
+    color: 16734039,
+    description: "Something went wrong... :cry:"
+   }})
+  }
+ }
+}
