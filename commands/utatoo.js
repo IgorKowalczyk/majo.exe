e134e1ea3229b3d6d6a47e37ef424f6ae311638f
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-04 16:07:58 +0200

    Add new command

---
+++ commands/utatoo.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const config = require("../config");
+const prefix = config.prefix;
+const AmeClient = require('amethyste-api');
+const AmeAPI = new AmeClient(process.env.AMEAPI);
+
+module.exports = {
+ name: "utatoo",
+ aliases: [],
+ description: "Hmmm",
+ category: "Image",
+ usage: "utatoo [user mention, user id, user name]",
+ run: async (client, message, args) => {
+  try {
+    const User = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase().includes() === args.join(' ').toLocaleLowerCase()) || message.member;
+   const wait = await message.channel.send({embed: {
+     color: 4779354,
+     description: "✨ | Please wait... I'm generating your image",
+    }})
+   const buffer = await AmeAPI.generate("utatoo", {
+    url: User.user.displayAvatarURL({
+     format: "png",
+     size: 2048
+    })
+   });
+   const attachment = new Discord.MessageAttachment(buffer, "utatoo.png");
+   message.channel.send(attachment);
+  } catch (err) {
+   console.log(err);
+   message.channel.send({embed: {
+    color: 16734039,
+    description: "Something went wrong... :cry:"
+   }})
+  }
+ }
+}
