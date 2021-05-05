63acbca69e3cc128c329c50397d2d502ea25a10b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-05 09:44:45 +0200

    Update commands

---
+++ commands/futa.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const config = require("../config");
+const prefix = config.prefix;
+
+module.exports = {
+ name: "futa",
+ aliases: ["futanari"],
+ description: "Display a random futa image/gif",
+ category: "NSFW",
+ usage: "futa",
+ run: async (client, message, args) => {
+  if (!message.channel.nsfw) {
+   return message.channel.send({embed: {
+    color: 16734039,
+    description: "💢 | You can use this command only in an NSFW Channel!"
+   }})
+  }
+  (async () => {
+   try {
+    const response = await fetch("https://nekos.life/api/v2/img/futanari")
+    const body = await response.json();
+    const embed = new Discord.MessageEmbed()
+     .setTitle(":smirk: Futa!", message.guild.iconURL({ dynamic: true, format: 'png'}))
+     .setImage(body.url)
+     .setColor("RANDOM")
+     .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+     .setTimestamp()
+     .setURL(body.url);
+    message.channel.send(embed);
+   } catch(err) {
+    message.channel.send({embed: {
+     color: 16734039,
+     description: "Something went wrong... :cry:"
+    }})
+   }
+  })();
+ }
+}
--- commands/suggest.js
+++ commands/suggest.js
@@ -19,19 +19,23 @@ module.exports = {
     return message.channel.send({embed: {
      color: 16734039,
      description: "❌ | Your suggestion can have a maximum of 1000 characters!",
-    }})  
+    }})
    }
    const channel = client.channels.cache.get(config.suggestionschannel)
    if (channel) {
     const embed = new Discord.MessageEmbed()
      .setAuthor("🤔" + message.author.username + " suggestion!", message.guild.iconURL())
      .setColor("RANDOM")
-     .setDescription(args.join(" "))
+     .setDescription(message.args)
      .addField("Reporter", `<@${message.author.id}> (ID: ${message.author.id})`)
      .addField("User guild", `${message.guild.name} (ID: ${message.guild.id})`)
-     .setFooter(message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+     .setFooter("Majo.exe", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
      .setThumbnail(message.guild.iconURL())
     channel.send(embed);
+    const success = new Discord.MessageEmbed()
+     .setColor("RANDOM")
+     .setDescription(`${message.author} your suggestion was send, you can view it in Majo.exe Developers server in <#${suggestionschannel} channel.`)
+     .setFooter(`[Majo.exe Developers](${config.server})`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
    } else {
     return message.channel.send({embed: {
      color: 16734039,
