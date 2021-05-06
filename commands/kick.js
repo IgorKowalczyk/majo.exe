74072c420e3196f21711becffc2e22589dfc97d8
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-06 13:44:21 +0200

    Add kick command

---
+++ commands/kick.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+
+module.exports = {
+ name: "kick",
+ aliases: [],
+ description: "Kicks a member from guild",
+ category: "Moderation",
+ usage: "kick <mention> <reason>",
+ run: async (client, message, args) => {
+  try {
+   if (!message.member.hasPermission("KICK_MEMBERS")) {
+    return await message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | You don't have premission to kick members"
+    }})
+   }
+   let mentioned = await message.mentions.members.first();
+   let reason = await args.slice(1).join(' ');
+   if (!mentioned) {
+    return await message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | Mention a valid member!"
+    }})
+   }
+   if (!mentioned.kickable) {
+    return await message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | You cannot kick this member!"
+    }})
+   }
+   if (message.author === mentioned) {
+    return await message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | You cant kick yourself!"
+    }})
+   }
+   if (!reason) {
+    reason = "No reason provided!";
+   }
+   mentioned.kick(reason);
+   await message.channel.send({embed: {
+    color: 16734039,
+    description: ":arrow_right: " + mentioned.displayName + " has been kicked. Reason: " + reason + "!"
+   }})
+  } catch (err) {
+    console.log(err);
+    message.channel.send({embed: {
+     color: 16734039,
+     description: "Something went wrong... :cry:"
+   }})
+  }
+ }
+}
+ 
