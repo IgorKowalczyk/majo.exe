bb4ddd869daa65e362d99200187719d2c2ef6155
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-06 20:51:01 +0200

    Update

--- commands/kiss.js
+++ commands/kiss.js
@@ -17,7 +17,7 @@ module.exports = {
     description: "❌ | You must mention user to kiss ;-;"
    }})
   }
-  if (message.author === user) {
+  if (message.author === user || message.member == member) {
    return await message.channel.send({embed: {
     color: 16734039,
     description: "❌ | You cant kiss yourself ;-; (Try kissing someone else, your love. Maybe you need some help?)"
