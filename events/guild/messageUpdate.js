f0bd935813235474c6f88580aa5bee0f5f8947be
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-09 13:49:27 +0200

    Add message edit logger

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -2,30 +2,30 @@ const Discord = require('discord.js');
 const config = require("../../config");
 
 module.exports = async (client, oldMessage, newMessage) => {
-/*
-try {
- if (oldMessage.author.bot) return;
- if (!oldMessage.guild.member(client.user).hasPermission("EMBED_LINKS")) return;
- if (!oldMessage.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return;
- var logChannel = oldMessage.guild.channels.cache.find(c => c.name === "log");
- if (!logChannel) return;
- if (oldMessage.content.startsWith("https://")) return;
- if (oldMessage.content.startsWith("http://")) return;
- let messageUpdate = new Discord.MessageEmbed()
-  .setTitle("**MESSAGE EDIT**")
-  .setThumbnail(oldMessage.author.avatarURL())
-  .setColor("RANDOM")
-  .setDescription(`**\n**:wrench: Successfully \`\`EDIT\`\` **MESSAGE** In ${oldMessage.channel}\n\n**Channel:** \`\`${oldMessage.channel.name}\`\` (ID: ${oldMessage.channel.id})\n**Message ID:** ${oldMessage.id}\n**Sent By:** <@${oldMessage.author.id}> (ID: ${oldMessage.author.id})\n\n**Old Message:**\`\`\`${oldMessage}\`\`\`\n**New Message:**\`\`\`${newMessage}\`\`\``)
-  .setTimestamp()
-  .setFooter(oldMessage.guild.name, oldMessage.guild.iconURL());
-  logChannel.send(messageUpdate);
-} catch (err) {
- let embed = new Discord.MessageEmbed()
-  .setColor("#FF0000")
-  .setTitle("Error!")
-  .setDescription("**Error Code:** *" + err + "*")
-  .setTimestamp();
-  return logChannel.send(embed);
-}
-*/
+ try {
+  if (oldMessage.author.bot) return;
+  if (!oldMessage.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
+  if (!log.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
+  var log = oldMessage.guild.channels.cache.find(c => c.name === "log");
+  if (!log) return;
+  if (oldMessage.content.startsWith("https://")) return;
+  if (oldMessage.content.startsWith("http://")) return;
+  const event = new Discord.MessageEmbed()
+   .setTitle(`Message Edited`)
+   .setColor('RANDOM')
+   .setThumbnail(oldMessage.author.avatarURL())
+   .addField("Channel", `<#${oldMessage.channel.id}> (ID: ${oldMessage.channel.id})`)
+   .addField("Message ID", `${oldMessage.id}`)
+   .addField("Created at", `${oldMessage.createdAt}`)
+   .addField("TTS", `${oldMessage.tts}`)
+   .addField("Pinned", `${oldMessage.pinned}`)
+   .addField("Send By", `<@${oldMessage.author.id}> (ID: ${oldMessage.author.id})`)
+   .addField("Old Message", "\`\`\`" + `${oldMessage}` + "\`\`\`")
+   .addField("New Message", "\`\`\`" + `${newMessage}` + "\`\`\`")
+   .setTimestamp()
+   .setFooter(message.guild.name, message.guild.iconURL())
+  log.send(event);
+ } catch (err) {
+  console.log(err);
+ }
 }
