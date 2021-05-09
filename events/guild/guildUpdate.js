b5628f1ea38a1770b313b71397e02cdf93548cbe
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-09 11:49:36 +0200

    Update

---
+++ events/guild/guildUpdate.js
@@ -0,0 +1,115 @@
+const Discord = require('discord.js');
+const config = require("../../config");
+
+module.exports = async (client, oldGuild, newGuild) => {
+ try {
+  if (!guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
+  const log = guild.channels.cache.find(log => log.name === "log")
+  if(!log) return;
+  if(log.type !== "text") return;
+  if (!log.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
+  if(log) {
+   if(oldGuild.name != newGuild.name) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild name changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old name", `${oldGuild.name}`)
+     .addField("New name", `${newGuild.name}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);
+   }
+   if(oldGuild.owner != newGuild.owner) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild owner changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old owner", `${oldGuild.owner}`)
+     .addField("New owner", `${newGuild.owner}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.preferredLocale != newGuild.preferredLocale) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild preferred locale changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old preferred locale", `${oldGuild.preferredLocale}`)
+     .addField("New preferred locale", `${newGuild.preferredLocale}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.publicUpdatesChannelID != newGuild.publicUpdatesChannelID) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild public updates channel changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old public updates channel", `<#${oldGuild.publicUpdatesChannelID}>`)
+     .addField("New public updates channel", `<#${newGuild.publicUpdatesChannelID}>`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.rulesChannelID != newGuild.rulesChannelID) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild rules channel changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old rules channel", `<#${oldGuild.rulesChannelID}>`)
+     .addField("New rules channel", `<#${newGuild.rulesChannelID}>`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.systemChannelID != newGuild.systemChannelID) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild system channel changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old system channel", `<#${oldGuild.systemChannelID}>`)
+     .addField("New system channel", `<#${newGuild.systemChannelID}>`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.verificationLevel != newGuild.verificationLevel) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild verification level changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old verification level", `${oldGuild.verificationLevel}`)
+     .addField("New verification level", `${newGuild.verificationLevel}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.widgetEnabled != newGuild.widgetEnabled) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild widget update")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old widget channel", `${oldGuild.widgetEnabled}`)
+     .addField("New widget channel", `${newGuild.widgetEnabled}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+   if(oldGuild.widgetChannelID != newGuild.widgetChannelID) {
+    const embed = new Discord.MessageEmbed()
+     .setTitle("Guild widget channel changed")
+     .setThumbnail(uavatar)
+     .setColor("RANDOM")
+     .addField("Old widget channel", `${oldGuild.widgetChannelID}`)
+     .addField("New widget channel", `${newGuild.widgetChannelID}`)
+     .setTimestamp()
+     .setFooter(guild.name, guild.iconURL())
+    log.send(embed);  
+   }
+  }
+ } catch (err) {
+  console.log(err);
+ }
+}
