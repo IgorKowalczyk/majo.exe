01cedee8446bb1ccec6e06068a207c980a99181e
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-10 15:18:19 +0200

    Fix

--- commands/Utility/greroll.js
+++ commands/Utility/greroll.js
@@ -8,19 +8,19 @@ module.exports = {
  usage: "greroll <giveaway-id>",
  run: async (client, message, args) => {
   try {
+   const messageID = args[0];
+   if(!messageID && !args[0]) {
+    return message.channel.send({embed: {
+     color: 16734039,
+     description: "❌ | Please enter a giveaway message ID"
+    }})
+   }
    let giveaway = client.giveawaysManager.giveaways.find((g) => g.guildID === message.guild.id && g.prize === args.join(' ')) ||
    client.giveawaysManager.giveaways.find((g) => g.guildID === message.guild.id && g.messageID === args[0]);
    if (!giveaway) {
     return message.channel.send({embed: {
      color: 16734039,
      description: '❌ | Unable to find a giveaway for `'+ args.join(' ') +'`.'
-    }})
-   }
-   const messageID = message.args[0];
-   if(!messageID && !args[0]) {
-    return message.channel.send({embed: {
-     color: 16734039,
-     description: "❌ | Please enter a giveaway message ID"
     }})
    }
    client.giveawaysManager.reroll(messageID).then(() => {
