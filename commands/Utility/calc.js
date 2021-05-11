3d3561981fa9cf20235ad696b8d1d9b37a04e9d1
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-11 10:43:18 +0200

    Update footer anch change category

--- commands/Utility/calc.js
+++ commands/Utility/calc.js
@@ -5,7 +5,7 @@ module.exports = {
  name: "calc",
  aliases: ["math"],
  description: "Discord calculator",
- category: "Fun",
+ category: "Utility",
  usage: "calc <task>",
  run: async (client, message, args) => {
   try {
@@ -13,21 +13,25 @@ module.exports = {
     return message.channel.send({embed: {
      color: 16734039,
      description: "❌ | You must provide a equation to be solved on the calculator! (eg. 9 + 10)"
-    }}) 
+    }})
    }
    const question = args.join(' ');
    try {
     let answer;
+    let footer;
     if(question.indexOf('9 + 10') > -1) {
      answer = '21 (XD!)';
+     footer = "You are a idiot | Requested by " + `${message.author.username}`;
     } else {
      answer = math.eval(question);
+     footer = "Requested by " + `${message.author.username}`;
     }
     const calc = new Discord.MessageEmbed()
      .setTitle("💡 Calculator")
      .setColor("RANDOM")
      .addField("Question: ", `${question}`)
      .addField("Answer: ", `${answer}`)
+     .setFooter(`${footer}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
     return message.channel.send(calc);
    } catch (err) {
     message.channel.send({embed: {
