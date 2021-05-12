48f27a2ced868030091da97fa84c2e62681d7f71
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-12 19:21:42 +0200

    Fix

--- commands/Economy/daily.js
+++ commands/Economy/daily.js
@@ -10,7 +10,7 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    let amount = Math.floor(Math.random() * 500) + 100;
-   let add = await eco.daily(message.author.id, message.guild.id, amount);
+   let add = await client.economy.daily(message.author.id, message.guild.id, amount);
    if (add.cooldown) return message.reply(`You already claimed your daily coins. Come back after ${add.time.days} days, ${add.time.hours} hours, ${add.time.minutes} minutes & ${add.time.seconds} seconds.`);
    return message.reply(`you claimed ${add.amount} as your daily coins and now you have total ${add.money} coins.`);
   } catch (err) {
