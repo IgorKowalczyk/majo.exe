fd2d7e169891a66000e2705ff23983b73c8bdf57
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-13 21:48:04 +0200

    Update

--- commands/Fun/poke.js
+++ commands/Fun/poke.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 
 module.exports = {
- name: "hug",
+ name: "poke",
  aliases: [],
- description: "Give a hug to mention user",
+ description: "Poke user",
  category: "Fun",
- usage: "cuddle <user>",
+ usage: "poke <user>",
  run: async (client, message, args) => {
   (async () => {
    try {
@@ -20,24 +20,24 @@ module.exports = {
     if(user == message.author) {
      return message.channel.send({embed: {
       color: 5294200,
-      description: "🤦 | You can't poke yourself!"
+      description: "🤦 | You can't poke yourself tfu!"
      }})
     }
     if(user == client.user) {
      return message.channel.send({embed: {
       color: 5294200,
-      description: "🤦 | Oh, you tried to poke me but u cant hehe"
+      description: "🤦 | Oh, you tried to poke me but u cant hehe (hopefully)"
      }})
     }
-    const response = await fetch("https://nekos.life/api/v2/img/cuddle")
+    const response = await fetch("https://nekos.life/api/v2/img/poke")
     const body = await response.json();
     const embed = new Discord.MessageEmbed()
-     .setTitle(user.username + " Just got a hug from " + message.author.username, message.guild.iconURL({ dynamic: true, format: 'png'}))
+     .setTitle(user.username + " just got poked by " + message.author.username, message.guild.iconURL({ dynamic: true, format: 'png'}))
      .setImage(body.url)
      .setURL(body.url)
      .setColor("RANDOM")
      .setDescription((user.toString() + " got a hug from " + message.author.toString()))
-     .setFooter("Requested by " + `${message.author.username}` + " • (this is so cute ＼( ^o^ )／)", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
+     .setFooter("Requested by " + `${message.author.username}` + " • (rip)", message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
      .setTimestamp()
     .setURL(body.url);
     message.channel.send(embed);
