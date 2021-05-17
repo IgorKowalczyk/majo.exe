5c43eda35d455daea82856bbc05f0a84a2828dcc
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-17 09:50:38 +0200

    Fix avatar bug

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -12,7 +12,7 @@ module.exports = {
     const uavatar = User.user.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 });
     const embed = new Discord.MessageEmbed()
      .setColor("RANDOM")
-     .setAuthor(User.username + "'s Avatar", uavatar)
+     .setAuthor(User.user.username + "'s Avatar", uavatar)
      .setImage(uavatar) 
      .setTimestamp()
      .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 2048 }))
