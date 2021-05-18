d9bf609643e1dd9f6e6b71b67873d4f82181c847
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-18 12:59:16 +0200

    Fix imports (logging)

--- events/guild/channelUpdate.js
+++ events/guild/channelUpdate.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require("../../config");
+const MySQL = require('mysql');
 
 module.exports = async (client, oldChannel, newChannel) => {
  try {
