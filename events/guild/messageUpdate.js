e1e9e397664547a42f9c6935f54f45f7036381f1
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-18 12:31:21 +0200

    Final update to mysql query

--- events/guild/messageUpdate.js
+++ events/guild/messageUpdate.js
@@ -14,43 +14,33 @@ module.exports = async (client, oldMessage, newMessage) => {
  try {
   const sqlquery = 'SELECT channelid AS res FROM logs WHERE guildid = ' + oldMessage.guild.id;
   sql.query(sqlquery, function (error, results, fields) {
-  if(error) {
-   console.log(error);
-  }
-  console.log("results " + results[0]);
-  console.log("Fields" + fields);
-  console.log("response" + results[0].res)
-  console.log("results lenght" + results.length);
-  if (!results || results.length == 0) {
+   if(error) console.log(error);
+   if (!results || results.length == 0) {
+    sql.end();
+    return;
+   }
+   const log = results[0].res
    sql.end();
-   console.log(results);
-   return;
-  }
-  console.log("results2 " + results) // For tests only
-  results.send("This happens if i'm thinking right");
-  sql.end();
+   if (oldMessage.author.bot) return;
+   if (!oldMessage.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
+   if (!log) return;
+   if (!newMessage.embeds) return console.log("yes");
+   const event = new Discord.MessageEmbed()
+    .setTitle(`Message Edited`)
+    .setColor('RANDOM')
+    .setThumbnail(oldMessage.author.avatarURL())
+    .addField("Channel", `<#${oldMessage.channel.id}> (ID: ${oldMessage.channel.id})`)
+    .addField("Message ID", `${oldMessage.id}`)
+    .addField("Created at", `${oldMessage.createdAt}`)
+    .addField("TTS", `${oldMessage.tts}`)
+    .addField("Pinned", `${oldMessage.pinned}`)
+    .addField("Send By", `<@${oldMessage.author.id}> (ID: ${oldMessage.author.id})`)
+    .addField("Old Message", "\`\`\`" + `${oldMessage}` + "\`\`\`")
+    .addField("New Message", "\`\`\`" + `${newMessage}` + "\`\`\`")
+    .setTimestamp()
+    .setFooter(oldMessage.guild.name, oldMessage.guild.iconURL())
+   log.send(event);
   });
-  if (oldMessage.author.bot) return;
-  if (!oldMessage.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
-  // if (!log.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
-  const log = oldMessage.guild.channels.cache.find(c => c.name === "log");
-  if (!log) return;
-  if (!newMessage.embeds) return console.log("yes");
-  const event = new Discord.MessageEmbed()
-   .setTitle(`Message Edited`)
-   .setColor('RANDOM')
-   .setThumbnail(oldMessage.author.avatarURL())
-   .addField("Channel", `<#${oldMessage.channel.id}> (ID: ${oldMessage.channel.id})`)
-   .addField("Message ID", `${oldMessage.id}`)
-   .addField("Created at", `${oldMessage.createdAt}`)
-   .addField("TTS", `${oldMessage.tts}`)
-   .addField("Pinned", `${oldMessage.pinned}`)
-   .addField("Send By", `<@${oldMessage.author.id}> (ID: ${oldMessage.author.id})`)
-   .addField("Old Message", "\`\`\`" + `${oldMessage}` + "\`\`\`")
-   .addField("New Message", "\`\`\`" + `${newMessage}` + "\`\`\`")
-   .setTimestamp()
-   .setFooter(oldMessage.guild.name, oldMessage.guild.iconURL())
-  log.send(event);
  } catch (err) {
   console.log(err);
  }
