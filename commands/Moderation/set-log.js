9c23a59bc1b0311f28c3c5f4594f65cfe60184d8
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-18 18:20:45 +0200

    Update logging

--- commands/Moderation/set-log.js
+++ commands/Moderation/set-log.js
@@ -36,9 +36,10 @@ module.exports = {
    sql.query(sqlquery, function (error, results, fields) {
     if(error) return console.log(error);
     if(results[0]) {
-     console.log("1");
-     const update = "UPDATE logs SET channelid = " + channel.id + "WHERE guildid = " + message.guild.id + "; SELECT channelid AS res FROM logs WHERE guildid = " + message.guild.id;
+     console.log("Updated");
+     const update = "UPDATE logs SET channelid = " + channel.id + "WHERE guildid = " + message.guild.id;
      sql.query(update, function (error, results, fields) {
+      if(error) console.log(error);
       message.lineReply({embed: {
        color: 4779354,
        description: `✨ | Success! Updated logs channel, new logs channel is ${channel} (ID: ${channel.id})`,
@@ -47,9 +48,10 @@ module.exports = {
       })
     } else {
      console.log("2");
-     const insert = "INSERT INTO `logs` (`guildid`, `channelid`) VALUES (" + message.guild.id + "," + channel.id + "); SELECT channelid AS res FROM logs WHERE guildid = " + message.guild.id;
+     const insert = "INSERT INTO `logs` (`guildid`, `channelid`) VALUES (" + message.guild.id + "," + channel.id + ");";
      sql.query(insert, function (error, results, fields) {
-      console.log("success, added");
+      if(error) console.log(error);
+      console.log("Added");
       message.lineReply({embed: {
        color: 4779354,
        description: `✨ | Success! New channel for logs is ${channel} (ID: ${channel.id})`,
