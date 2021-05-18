2c41a84bb7acc2e3db45d297a172e0e32c731d68
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-18 17:07:47 +0200

    Update

---
+++ commands/Moderation/set-log.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const MySQL = require('mysql');
+
+module.exports = {
+ name: "set-log",
+ aliases: ["log-set", "l-set", "set-log-channel"],
+ description: "Set log channel for the guild",
+ category: "Moderation",
+ usage: "set-log <channel>",
+ run: async (client, message, args) => {
+  try {
+   const sql = MySQL.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    charset: 'utf8mb4',
+    port: "3306"
+   });
+   const channel = message.mentions.channels.first();
+   const sqlquery = 'SELECT channelid AS res FROM logs WHERE guildid = ' + message.guild.id;
+   sql.query(sqlquery, function (error, results, fields) {
+    if(error) return console.log(error);
+    selectresponse = results[0].res;
+   })
+   console.log(selectresponse);
+  } catch (err) {
+   console.log(err);
+   message.lineReply({embed: {
+    color: 16734039,
+    description: "Something went wrong... :cry:"
+   }})
+  }
+ }
+}
+ 
