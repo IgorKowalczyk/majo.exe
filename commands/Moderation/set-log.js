ce6ce29662d2152d57d461fc0eb1b030c04de7ee
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-19 08:42:14 +0200

    Update

--- commands/Moderation/set-log.js
+++ commands/Moderation/set-log.js
@@ -16,21 +16,13 @@ module.exports = {
      description: "❌ | You must enter a channel!"
     }})
    }
-   const sql = MySQL.createConnection({
+   const sql = MySQL.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     charset: 'utf8mb4',
     port: "3306"
-   });
-   sql.connect((err) => {
-    if (err) {
-     console.error('Impossible to connect to MySQL server. Code: ' + err.code);
-     process.exit(99);
-    } else {
-     console.log('[SQL] Connected to the MySQL server! Connection ID: ' + sql.threadId);
-    }
    });
    const sqlquery = 'SELECT channelid AS res FROM logs WHERE guildid = ' + message.guild.id;
    sql.query(sqlquery, function (error, results, fields) {
--- events/guild/channelCreate.js
+++ events/guild/channelCreate.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 const config = require("../../config");
 const MySQL = require('mysql');
-const sql = MySQL.createPool({
- host: process.env.MYSQL_HOST,
- user: process.env.MYSQL_USER,
- password: process.env.MYSQL_PASSWORD,
- database: process.env.MYSQL_DATABASE,
- charset: 'utf8mb4',
- port: "3306"
-});
 
 module.exports = async (client, channel) => {
  try {
+  const sql = MySQL.createPool({
+   host: process.env.MYSQL_HOST,
+   user: process.env.MYSQL_USER,
+   password: process.env.MYSQL_PASSWORD,
+   database: process.env.MYSQL_DATABASE,
+   charset: 'utf8mb4',
+   port: "3306"
+  });
   const sqlquery = 'SELECT channelid AS res FROM logs WHERE guildid = ' + channel.guild.id;
   sql.query(sqlquery, function (error, results, fields) {
    if(error) console.log(error);
