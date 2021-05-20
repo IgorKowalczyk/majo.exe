66910e7dc23da890650224938cfef8fc81c97ff4
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-20 11:58:13 +0200

    Update (Code from older bot)

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -9,28 +9,44 @@ module.exports = {
  usage: "userinfo [user]",
  run: async (client, message, args) => { 
   try {
-   const member = message.mentions.members.first() || message.author;
-   const embed = new Discord.MessageEmbed()
-    .setTitle(`${member.displayName}'s Information`)
-    .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
-    .addField('User', member, true)
-    .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
-    .addField('ID', `\`${member.id}\``, true)
-    .addField('Status', member.presence.status || "None", true)
-    .addField('Bot', `\`${member.user.bot}\``, true)
-    .addField('Color Role', member.roles.color || '`None`', true)
-    .addField('Highest Role', member.roles.highest, true)
-    .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
-    .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
-    .setFooter(message.member.displayName, message.author.displayAvatarURL({dynamic: true}))
-    .setTimestamp()
-    .setColor(member.displayHexColor);
-   if (activities.length > 0) embed.setDescription(activities.join('\n'));
-   if (customStatus) embed.spliceFields(0, 0, {
-    name: 'Custom Status',
-    value: customStatus
-   });
-   if (member.flags.length > 0) embed.addField('Badges', member.flags.map(flag => flags[flag]).join('\n'));
+   const user = message.mentions.members.first() || message.author;
+   let stat = {
+    online: "https://emoji.gg/assets/emoji/9166_online.png",
+    idle: "https://emoji.gg/assets/emoji/3929_idle.png",
+    dnd: "https://emoji.gg/assets/emoji/2531_dnd.png",
+    offline: "https://emoji.gg/assets/emoji/7445_status_offline.png"
+   }
+   let badges = await user.user.flags
+   badges = await badges.toArray();
+   let newbadges = [];
+   badges.forEach(m => {
+    newbadges.push(m.replace("_", " "))
+   })
+   const embed = new MessageEmbed()
+   .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
+   let array = []
+   if (user.user.presence.activities.length) {
+    let data = user.user.presence.activities;
+    for (let i = 0; i < data.length; i++) {
+     let name = data[i].name || "None"
+     let xname = data[i].details || "None"
+     let zname = data[i].state || "None"
+     let type = data[i].type
+     array.push(`**${type}** : \`${name} : ${xname} : ${zname}\``)
+     if (data[i].name === "Spotify") {
+      embed.setThumbnail(`https://i.scdn.co/image/${data[i].assets.largeImage.replace("spotify:", "")}`)
+     }
+     embed.setDescription(array.join("\n"))
+    }
+   }
+   embed.setColor(user.displayHexColor === "#000000" ? "#ffffff" : user.displayHexColor)
+   embed.setAuthor(user.user.tag, user.user.displayAvatarURL({ dynamic: true }))
+   if (user.nickname !== null) embed.addField("Nickname", user.nickname)
+   embed.addField("Joined At", moment(user.user.joinedAt).format("LLLL"))
+    .addField("Account Created At", moment(user.user.createdAt).format("LLLL"))
+    .addField("Common Information", `ID: \`${user.user.id}\`\nDiscriminator: ${user.user.discriminator}\nBot: ${user.user.bot}\nDeleted User: ${user.deleted}`)
+    .addField("Badges", newbadges.join(", ").toLowerCase() || "None")
+    .setFooter(user.user.presence.status, stat[user.user.presence.status])
    message.lineReply(embed);
   } catch(err) {
    console.log(err);
