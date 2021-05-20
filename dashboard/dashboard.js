41f8b7cf73e544358ec1e9da11d3f088d52cf94f
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-20 21:10:33 +0200

    Update

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -28,7 +28,7 @@ module.exports = async (client) => {
   clientSecret: config.clientSecret,
   callbackURL: `${config.domain}${config.port != 80 ? "" : `:${config.port}`}/callback`,
   response_type: `token`,
-  scope: ["identify", "guilds"]
+  scope: ["identify", "guilds", "applications.commands"]
  },
  (accessToken, refreshToken, profile, done) => { 
   process.nextTick(() => done(null, profile));
