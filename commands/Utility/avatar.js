37d53218c2a41e409e42a41da1b3356d38d8646c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-29 18:25:08 +0200

    Fix

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -9,20 +9,7 @@ module.exports = {
  run: async (client, message, args) => {
   const Extra = require('discord-buttons')(client);
   try {
-   const User =
-    (await message.mentions.members.first()) ||
-    message.guild.members.cache.get(args[0]) ||
-    message.guild.members.cache.find(
-     (r) =>
-      r.user.username.toLowerCase().includes() ===
-      args.join(" ").toLocaleLowerCase()
-    ) ||
-    message.guild.members.cache.find(
-     (r) =>
-      r.displayName.toLowerCase().includes() ===
-      args.join(" ").toLocaleLowerCase()
-    ) ||
-    message.member;
+   const User = (await message.mentions.members.first()) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((r) => r.user.username.toLowerCase().includes() === args.join(" ").toLocaleLowerCase() ) || message.guild.members.cache.find((r) => r.displayName.toLowerCase().includes() === args.join(" ").toLocaleLowerCase() ) || message.member;
    const uavatar = User.user.displayAvatarURL({
     dynamic: true,
     format: "png",
