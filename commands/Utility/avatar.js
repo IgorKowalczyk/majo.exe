e903222bacafe9f30b9ebd08a089942db096b54c
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-29 18:44:46 +0200

    .

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -21,7 +21,7 @@ module.exports = {
     .setImage(uavatar)
     .setTimestamp()
     .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({dynamic: true, format: "png", size: 2048}) );
-   message.channel.send({ button: button, embed: embed});
+   message.lineReply({ button: button, embed: embed});
   } catch (err) {
    message.lineReply({
     embed: {
