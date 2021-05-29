117b2c85ca5bd7a37f9b0ee3b6ce674179bd0e3d
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-29 18:39:58 +0200

    fix

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const Extra = require('discord-buttons');
 
 module.exports = {
  name: "avatar",
@@ -7,7 +8,6 @@ module.exports = {
  category: "Utility",
  usage: "avatar [user mention, user id, user name]",
  run: async (client, message, args) => {
-  const Extra = require('discord-buttons')(client);
   try {
    const User = (await message.mentions.members.first()) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((r) => r.user.username.toLowerCase().includes() === args.join(" ").toLocaleLowerCase() ) || message.guild.members.cache.find((r) => r.displayName.toLowerCase().includes() === args.join(" ").toLocaleLowerCase() ) || message.member;
    const uavatar = User.user.displayAvatarURL({dynamic: true, format: "png", size: 2048});
