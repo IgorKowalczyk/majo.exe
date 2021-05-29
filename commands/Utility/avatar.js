e0d01f29d273285c7eb204f7985441447cda6502
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-29 15:14:22 +0200

    Fix

--- commands/Utility/avatar.js
+++ commands/Utility/avatar.js
@@ -41,7 +41,7 @@ module.exports = {
      "Requested by " + `${message.author.username}`,
      message.author.displayAvatarURL({dynamic: true, format: "png", size: 2048})
     );
-   message.channel.send(embed, button);
+   message.lineReply({ button: button, embed: embed});
   } catch (err) {
    message.lineReply({
     embed: {
