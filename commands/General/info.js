2665f3337e6aec71540e5899f2942164366ba4e6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-30 17:45:27 +0200

    Fix

--- commands/General/info.js
+++ commands/General/info.js
@@ -32,7 +32,7 @@ module.exports = {
     .addField("Platform", osutils.platform())
     .addField('Node', `${process.version}`, true)
     .addField("CPU Cores", osutils.cpuCount() + " Cores", true)
-    .addField("CPU Usage", `${(v * 100).toString().split(".")[0] + "." + (v * 100).toString().split(".")[1].split('')[0] + (v * 100).toString().split(".")[1].split('')[1]}%`, true)
+    .addField("CPU Usage", `${(osutils.cpuUsage * 100).toString().split(".")[0] + "." + (osutils.cpuUsage * 100).toString().split(".")[1].split('')[0] + (osutils.cpuUsage * 100).toString().split(".")[1].split('')[1]}%`, true)
     .addField("Total Memory", osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split('')[0] + osutils.totalmem().toString().split(".")[1].split('')[1] + "MB", true)
     .addField("RAM Usage Of VPS", `${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + ( osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split('')[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split('')[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split('')[0] + osutils.totalmem().toString().split(".")[1].split('')[1]}MB`, true)
     .addField("RAM Usage Of Bot", (process.memoryUsage().heapUsed / 1024 / 1024 ).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split('')[0] + osutils.totalmem().toString().split(".")[1].split('')[1] + "MB", true)
