484c45d3d83b8c04dad4cf6b9b5d4a7e829f2374
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-14 13:20:00 +0200

    Update chat.js

--- commands/Fun/chat.js
+++ commands/Fun/chat.js
@@ -41,7 +41,7 @@ module.exports = {
       },
      });
     } else {
-     const embed = new Discord.MessageEmbed()
+     const embed = new Discord.MessageEmbed() // Prettier
       .setDescription(body.cnt)
       .setColor("RANDOM")
       .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: "png", size: 2048 }))
