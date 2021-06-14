86d4cf2a67bf57aa91dfc960d48ddec851536dbf
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-14 12:59:31 +0200

    Update calculator.js

--- utilities/calculator.js
+++ utilities/calculator.js
@@ -90,7 +90,12 @@ module.exports = async (message) => {
 
  message.channel
   .send({
-   embed: new MessageEmbed().setTitle("Calculator").setDescription(stringify).setColor("RANDOM").setTimestamp(),.setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: "png", size: 2048 })),
+   embed: new MessageEmbed() // prettier
+    .setTitle("Calculator")
+    .setDescription(stringify)
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: "png", size: 2048 })),
    components: [
     {
      type: 1,
