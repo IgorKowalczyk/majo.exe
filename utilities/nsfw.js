e880585ddbf998fca1a8544e3a3dfa4246b71955
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-19 17:22:13 +0200

    Try to fix module

--- utilities/nsfw.js
+++ utilities/nsfw.js
@@ -7,5 +7,4 @@ module.exports = async (message) => {
   .setDescription("💢 | You can use this command only in an NSFW Channel!")
   .setFooter("Requested by " + message.author.username, message.author.displayAvatarURL())
   .setImage("https://media.discordapp.net/attachments/721019707607482409/855827123616481300/nsfw.gif")
- return nsfw;
 }
