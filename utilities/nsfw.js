1ace7ccdad3376741bb0d848a98bca9e71cc5fab
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-20 14:03:38 +0200

    Fix

--- utilities/nsfw.js
+++ utilities/nsfw.js
@@ -1,8 +1,11 @@
 const Discord = require("discord.js");
 
+module.exports = async (message) => {
 const nsfwembed = new Discord.MessageEmbed()
  .setColor("#FF5757")
  .setDescription("💢 | You can use this command only in an NSFW Channel!")
  .setFooter("Requested by " + message.author.username, message.author.displayAvatarURL())
  .setImage("https://media.discordapp.net/attachments/721019707607482409/855827123616481300/nsfw.gif")
+}
 
+module.exports.nsfwembed;
