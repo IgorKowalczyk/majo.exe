b8d502b4f326bcea92a27c0c68be7b4922d99197
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-23 21:03:11 +0200

    Update

--- commands/General/info.js
+++ commands/General/info.js
@@ -3,6 +3,7 @@ const config = require("../../config");
 const moment = require("moment");
 const osutils = require("os-utils");
 require("moment-duration-format");
+const { dependencies } = require("../../package.json");
 
 module.exports = {
  name: "info",
@@ -42,11 +43,12 @@ module.exports = {
     .addField("⏳ Uptime", `${duration}`, true)
     .addField("🏓 Ping", Math.round(client.ws.ping) + "ms", true)
     .addField("<:discduckmicroshit:856174395205615647>  Platform", osutils.platform(), true)
-    .addField("<:node:792080285205790760> Node", `${process.version}`, true)
-    .addField("<:cpu:856174395436171294> CPU Cores", osutils.cpuCount() + " Cores", true)
-    .addField("<:ram:856174395508391986> Total Memory", osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
-    .addField("<:ram:856174395508391986> RAM Usage (VPS)", `${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)`, true)
-    .addField("<:ram:856174395508391986> RAM Usage (BOT)", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB", true)
+    .addField("<:node:792080285205790760> Node", "```" + process.version + "```", true)
+    .addField("<:discordjs:756951296392560811> Discord.js", "```" + dependencies["discord.js"] + "```", true)
+    .addField("<:cpu:856174395436171294> CPU", osutils.cpus()[0].model.substring( 0, os.cpus()[0].model.indexOf( "CPU" ) ) + " (`" + osutils.cpuCount() + "` cores)", true)
+    .addField("<:ram:856174395508391986> Total Memory", "```" +  osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB```")
+    .addField("<:ram:856174395508391986> RAM Usage (VPS)", `\`\`\`${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)\`\`\``)
+    .addField("<:ram:856174395508391986> RAM Usage (BOT)", "```" + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB```")
     .addField("Useful Links", `[Support server](${config.server}) | ${webpanel} [Invite me](https://discord.com/oauth2/authorize/?permissions=${config.premissions}&scope=bot&client_id=${client.user.id})`)
     .setFooter(
      "Requested by " + `${message.author.username}`,
