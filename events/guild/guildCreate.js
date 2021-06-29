4a49e2052603095acd2769636173f639767c0a30
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-29 08:48:20 +0200

    Fix logging

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -4,7 +4,7 @@ const chalk = require("chalk");
 
 module.exports = async (client, guild) => {
  try {
-  console.log(chalk.bold(chalk.blue.bold("[MAJO]")) + chalk.cyan.bold(` New guild joined: `) + chalk.blue.bold.underline(guild.name) + chalk.cyan.bold(" (ID: ") + chalk.blue.bold.underline(guild.id) + chalk.cyan.bold(") This guild has ") + chalk.blue.bold.underline(guild.memberCount) + chalk.cyan.bold("members!"));
+  console.log(chalk.bold(chalk.blue.bold("[MAJO]")) + chalk.cyan.bold(` New guild joined: `) + chalk.blue.bold.underline(guild.name) + chalk.cyan.bold(" (ID: ") + chalk.blue.bold.underline(guild.id) + chalk.cyan.bold(") This guild has ") + chalk.blue.bold.underline(guild.memberCount) + chalk.cyan.bold(" members!"));
  } catch (err) {
   console.log(err);
  }
