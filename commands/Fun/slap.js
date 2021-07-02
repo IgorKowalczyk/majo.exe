59d1564d80dc34a43bfacc167374f84719b36ae6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-07-02 20:49:52 +0200

    Fix name

--- commands/Fun/slap.js
+++ commands/Fun/slap.js
@@ -31,7 +31,7 @@ module.exports = {
     const body = await response.json();
     const embed = await new Discord.MessageEmbed() // Prettier()
      .setColor("RANDOM")
-     .setTitle(user.username + " just got slapped by " + message.author.username)
+     .setTitle(member.username + " just got slapped by " + message.author.username)
      .setFooter(
       "That must hurt ._. | Requested by " + `${message.author.username}`,
       message.author.displayAvatarURL({
