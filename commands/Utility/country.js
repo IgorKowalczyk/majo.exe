d1a5f4aefbd2060da2defd463657dab9a7085559
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-07-03 18:35:59 +0200

    Update

--- commands/Utility/country.js
+++ commands/Utility/country.js
@@ -8,55 +8,65 @@ module.exports = {
  category: "Utility",
  usage: "country <country name>",
  run: async (client, message, args) => {
-  const country = args.slice().join(" ");
-  if (!country) {
-   return message.lineReply({
-    embed: {
-     color: 16734039,
-     description: "<:error:860884617770303519> | Please provide a valid country!",
-    },
-   });
-  }
-  const url = "https://restcountries.eu/rest/v2/name/" + country;
-  let response;
   try {
-   response = await fetch(url).then((res) => res.json());
-  } catch (e) {
-   return message.lineReply({
+   const country = args.slice().join(" ");
+   if (!country) {
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | Please provide a country name!",
+     },
+    });
+   }
+   const url = "https://restcountries.eu/rest/v2/name/" + country;
+   let response;
+   try {
+    response = await fetch(url).then((res) => res.json());
+   } catch (e) {
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | An error occured, please try again!",
+     },
+    });
+   }
+   try {
+    const data = response[0];
+    const embed = new Discord.MessageEmbed()
+     .setColor("RANDOM")
+     .setTitle(data.name)
+     .setThumbnail(`https://www.countryflags.io/${data.alpha2Code}/flat/64.png`)
+     .setTimestamp()
+     .addField("🖊️ Native Name", `\`\`\`${data.nativeName}\`\`\``)
+     .addField("🏛️ Capital", `\`\`\`${data.capital ? data.capital : "None"}\`\`\``)
+     .addFIeld("📍 Location", `\`\`\`${data.subregion ? data.subregion : data.region}\`\`\``)
+     .addField("💱 Currency", `\`\`\`${data.currencies[0].code} ${data.currencies[0].symbol}\`\`\``)
+     .addField("<:members:856161806606401556> Population", `\`\`\`${data.population.toLocaleString()}\`\`\``)
+     .addField("🌐 Area", `\`\`\`${data.area.toLocaleString()}km\`\`\``)
+     .addField("👅 Languages", `\`\`\`${data.languages.map((lang) => lang.name).join("/")}\`\`\``)
+     .setFooter(
+      "Requested by " + `${message.author.username}`,
+      message.author.displayAvatarURL({
+       dynamic: true,
+       format: "png",
+       size: 2048,
+      })
+     );
+    message.channel.send(embed);
+   } catch (err) {
+    console.log(err);
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | Please provide a valid country!",
+     },
+    });
+   }
+  } catch (err) {
+   message.lineReply({
     embed: {
      color: 16734039,
-     description: "<:error:860884617770303519> | An error occured, please try again!",
-    },
-   });
-  }
-  try {
-   const data = response[0];
-   const embed = new Discord.MessageEmbed()
-    .setColor("RANDOM")
-    .setTitle(data.name)
-    .setThumbnail(`https://www.countryflags.io/${data.alpha2Code}/flat/64.png`)
-    .setTimestamp()
-    .addField("🖊️ Native Name", `\`\`\`${data.nativeName}\`\`\``)
-    .addField("🏛️ Capital", `\`\`\`${data.capital ? data.capital : "None"}\`\`\``)
-    .addFIeld("📍 Location", `\`\`\`${data.subregion ? data.subregion : data.region}\`\`\``)
-    .addField("💱 Currency", `\`\`\`${data.currencies[0].code} ${data.currencies[0].symbol}\`\`\``)
-    .addField("<:members:856161806606401556> Population", `\`\`\`${data.population.toLocaleString()}\`\`\``)
-    .addField("🌐 Area", `\`\`\`${data.area.toLocaleString()}km\`\`\``)
-    .addField("👅 Languages", `\`\`\`${data.languages.map((lang) => lang.name).join("/")}\`\`\``)
-    .setFooter(
-     "Requested by " + `${message.author.username}`,
-     message.author.displayAvatarURL({
-      dynamic: true,
-      format: "png",
-      size: 2048,
-     })
-    );
-   message.channel.send(embed);
-  } catch {
-   return message.lineReply({
-    embed: {
-     color: 16734039,
-     description: "<:error:860884617770303519> | Please provide a valid country!",
+     description: "Something went wrong... :cry:",
     },
    });
   }
