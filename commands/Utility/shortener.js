ee4bafd1dfcb8035aee329cde324b869e6c40f46
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-07-14 09:06:41 +0200

    Add new command

---
+++ commands/Utility/shortener.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const shorten = require("isgd");
+const { MessageButton, default: discordButtons, MessageActionRow } = require("discord-buttons");
+
+module.exports = {
+ name: "shortener",
+ aliases: ["url-shortener", "link-shortener"],
+ description: "Shorter a url",
+ category: "Utility",
+ usage: "shortener <link>",
+ run: async (client, message, args) => {
+  try {
+   if (!args[0]) {
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | Please provide a link!",
+     },
+    });
+   }
+   shorten.shorten(args[0], function (res) {
+    const urldone = new Discord.MessageEmbed() // Prettier
+     .setColor("RANDOM")
+     .setTitle("Your shortened URL")
+     .setDescription(`Your url is: **${res}**\n`);
+    const url_button = new MessageButton() // Prettier
+     .setStyle("url")
+     .setLabel(`go to ${res}`)
+     .setURL(`${res}`)
+     .setEmoji("🤖");
+    message.lineReply(urldone, url_button);
+   });
+  } catch (err) {
+   console.log(err);
+   message.lineReply({
+    embed: {
+     color: 16734039,
+     description: "Something went wrong... :cry:",
+    },
+   });
+  }
+ },
+};
