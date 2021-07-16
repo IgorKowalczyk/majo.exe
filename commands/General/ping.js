2d0a250ffbc12b12fa003c4c3bd032ae6deb87c7
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-07-16 11:08:45 +0200

    Fix minus ping

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -10,13 +10,14 @@ module.exports = {
   try {
    const ping = new Discord.MessageEmbed() // Prettier()
     .setTitle(":ping_pong: Pong!")
-    .addField("My ping: ", `${Date.now() - message.createdTimestamp} ms`)
+    .addField("My ping: ", `${(Date.now() - message.createdTimestamp).toString().replace(/-/g, "")} ms`)
     .addField("API ping (Websocket): ", `${Math.round(client.ws.ping)} ms`)
     .setFooter("Requested by " + `${message.author.username}`, message.author.displayAvatarURL({ dynamic: true, format: "png", size: 2048 }))
     .setColor("RANDOM")
     .setTimestamp();
    message.lineReply(ping);
   } catch (err) {
+   console.log(err);
    message.lineReply({
     embed: {
      color: 16734039,
