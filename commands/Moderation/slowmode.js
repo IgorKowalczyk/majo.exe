5286388185b347754654d2544096d9947326832d
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-09 11:59:12 +0200

    Add command to set channel slowmode

---
+++ commands/Moderation/slowmode.js
@@ -0,0 +1,107 @@
+const Discord = require("discord.js");
+
+module.exports = {
+ name: "slowmode",
+ aliases: ["set-slowmode"],
+ description: "Set channel slowmode",
+ category: "Moderation",
+ usage: "slowmode <time>",
+ run: async (client, message, args) => {
+  try {
+   if (!message.guild.me.hasPermission("MANAGE_CHANNEL")) {
+    return await message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | I don't have premission to set slowmode!",
+     },
+    });
+   }
+   if (!message.member.hasPermission("MANAGE_CHANNEL")) {
+    return await message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | You don't have premission to set slowmode!",
+     },
+    });
+   }
+   const amount = parseInt(args[0]);
+   if (isNaN(amount) || !args[0]) {
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> | It doesn't seem to be valid number",
+     },
+    });
+   }
+   if (args[0] === amount + "s") {
+    message.channel.setRateLimitPerUser(amount);
+    if (amount > 1) {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " seconds",
+      },
+     });
+    } else {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " second",
+      },
+     });
+    }
+   }
+   if (args[0] === amount + "min") {
+    message.channel.setRateLimitPerUser(amount * 60);
+    if (amount > 1) {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " minutes",
+      },
+     });
+    } else {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " minute",
+      },
+     });
+    }
+   }
+   if (args[0] === amount + "h") {
+    message.channel.setRateLimitPerUser(amount * 60 * 60);
+    if (amount > 1) {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " hours",
+      },
+     });
+    } else {
+     return message.lineReply({
+      embed: {
+       color: 4779354,
+       description: "<:sucess:872104651179323432> slowmode is now set to " + amount + " hour",
+      },
+     });
+    }
+   } else {
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: "<:error:860884617770303519> |  You can only set seconds(s), minutes(min) and hours(h)",
+     },
+    });
+   }
+  } catch (err) {
+   console.log(err);
+   message.lineReply({
+    embed: {
+     color: 16734039,
+     description: "Something went wrong... :cry:",
+    },
+   });
+  }
+ },
+};
