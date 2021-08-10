c9183d1eaa22e54cb522b83fbd5927d20f05db85
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-10 14:27:14 +0200

    Update

--- commands/Utility/reputation.js
+++ commands/Utility/reputation.js
@@ -32,7 +32,7 @@ module.exports = {
     sql.query(sqlquery, function (error, results, fields) {
      if (error) return console.log(error);
      const embed = new Discord.MessageEmbed()
-      .setTitle("Reputation")
+      .setTitle(`${member} Reputation`)
       .setDescription(`✨ ${member} reputation: \`${results[0] ? (rep = results[0].toString()) : (rep = 0)}\`\n\n💡 You can add or remove reputation by using \`${prefix} reputation [+/-] <member>\``)
       .setTimestamp()
       .setThumbnail(member.user.displayAvatarURL())
