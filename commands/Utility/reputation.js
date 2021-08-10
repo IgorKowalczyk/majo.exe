33d374be34dac48e6c3fe7c913d7e727c6cdf1a9
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-10 14:11:29 +0200

    Update reputation [WIP!]

--- commands/Utility/reputation.js
+++ commands/Utility/reputation.js
@@ -8,10 +8,10 @@ module.exports = {
  aliases: ["rep"],
  description: "Adds, checks or removes reputation for a user",
  category: "Utility",
- usage: "reputation [+ | add / - | remove / * | check] <user>",
+ usage: "reputation [+ / add or - / remove] <user>",
  run: async (client, message, args) => {
   try {
-   const member =  (await message.mentions.members.first()) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((r) => r.user.username.toLowerCase().includes() === args.join(" ").toLocaleLowerCase()) || message.guild.members.cache.find((r) => r.displayName.toLowerCase().includes() === args.join(" ").toLocaleLowerCase());
+   const member = (await message.mentions.members.first()) || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find((r) => r.user.username.toLowerCase().includes() === args.join(" ").toLocaleLowerCase()) || message.guild.members.cache.find((r) => r.displayName.toLowerCase().includes() === args.join(" ").toLocaleLowerCase());
    if (!member) {
     return message.lineReply({
      embed: {
@@ -20,26 +20,124 @@ module.exports = {
      },
     });
    }
-   console.log(member.id);
-   const sqlquery = "SELECT rep AS res FROM `reputation` WHERE memberid = " + member.id;
-   sql.query(sqlquery, function (error, results, fields) {
-    if (error) return console.log(error);
-    if (results[0]) {
-     message.lineReply({
-      embed: {
-       color: 4779354,
-       description: `Your reputation: \`${results[0].res}\``,
-      },
-     });
-    } else {
-     message.lineReply({
-      embed: {
-       color: 16734039,
-       description: `Your reputation: \`0\``,
-      },
-     });
-    }
-   });
+   if (args[0] === "+") {
+    // Add reputation
+    const sqlquery = "SELECT rep AS res FROM `reputation` WHERE memberid = " + member.id;
+    sql.query(sqlquery, function (error, results, fields) {
+     if (error) return console.log(error);
+     if (results[0]) {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`${results[0].res}\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     } else {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`0\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     }
+    });
+   } else if (args[0] === "-") {
+    // Remove reputation
+    const sqlquery = "SELECT rep AS res FROM `reputation` WHERE memberid = " + member.id;
+    sql.query(sqlquery, function (error, results, fields) {
+     if (error) return console.log(error);
+     if (results[0]) {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`${results[0].res}\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     } else {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`0\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     }
+    });
+   } else {
+    // Check reputation
+    const sqlquery = "SELECT rep AS res FROM `reputation` WHERE memberid = " + member.id;
+    sql.query(sqlquery, function (error, results, fields) {
+     if (error) return console.log(error);
+     if (results[0]) {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`${results[0].res}\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     } else {
+      const embed = new Discord.MessageEmbed()
+       .setTitle("Reputation")
+       .setDescription(`${member} reputation: \`0\`\nYou can add or remove reputation by using ${prefix} reputation [+/-] <member>`)
+       .setTimestamp()
+       .setThumbnail(member.displayAvatarURL())
+       .setColor("RANDOM")
+       .setFooter(
+        "Requested by " + `${message.author.username}`,
+        message.author.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        })
+       );
+      message.lineReply(embed);
+     }
+    });
+   }
   } catch (err) {
    console.log(err);
    message.lineReply({
