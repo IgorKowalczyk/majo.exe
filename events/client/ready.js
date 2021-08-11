801da43eb2f03f196eacba295ecd94f8a947af7c
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-11 11:21:57 +0200

    Add new status emojis

--- events/client/ready.js
+++ events/client/ready.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
    return string.charAt(0).toUpperCase() + string.slice(1);
   }
   setInterval(() => {
-   const emojis = ["😆", "😄", "😎", "😂", "🥳", "😘", "😜", "😁", "😉", "🥰", "😍", "🤯", "🥶", "🤩", "😇", "😊", "☺️", "😌", "😋"];
+   const emojis = ["😆", "😄", "😎", "😂", "🥳", "😘", "😜", "😁", "😉", "🥰", "😍", "🤯" , "🤩", "😇", "😊", "☺️", "😌", "😋","😳", "😚", "😏", "😱", "🥵", "😶‍🌫️", "🤕", "😴"];
    const emoji = emojis[Math.floor(Math.random() * emojis.length)];
    var date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
    const discordbday = new Date().getFullYear() + "/05/13";
