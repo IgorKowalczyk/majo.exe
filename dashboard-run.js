e1947cefda60f044e42ebb3baa4c6adae484f13e
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-13 12:21:40 +0200

    Fix dashboard config

--- dashboard-run.js
+++ dashboard-run.js
@@ -3,12 +3,11 @@ const client = new Discord.Client({
  disableEveryone: true,
 });
 const chalk = require("chalk");
-const gradient = require("gradient-string");
 
 require("dotenv").config();
 
 client.on("ready", () => {
- if ((process.env.DASHBOARD = "true")) {
+ if (process.env.DASHBOARD = "true") {
   console.log(chalk.bold(chalk.blue.bold("[SQL]")) + chalk.cyan.bold(" Getting dashboard config file..."));
   const webrun = require("./dashboard/dashboard");
   webrun(client);
@@ -20,4 +19,6 @@ client.on("ready", () => {
 if (process.env.TOKEN) {
  client.login(process.env.TOKEN);
  console.log(chalk.bold(chalk.blue.bold("[SQL]")) + chalk.cyan.bold(" Web dashboard client logged"));
+} else {
+ throw new Error("You need to enter bot token to run dashboard!");
 }
