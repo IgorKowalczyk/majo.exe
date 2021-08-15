131ea6f58c4331b127fc0b5c962fee6e92ec5dd7
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-15 16:32:37 +0200

    Update info command

--- commands/General/info.js
+++ commands/General/info.js
@@ -8,7 +8,6 @@ const cpu = osu.cpu;
 const os = osu.os;
 const drive = osu.drive;
 const proc = osu.proc;
-const netstat = osu.netstat;
 const { dependencies } = require("../../package.json");
 
 module.exports = {
@@ -19,59 +18,50 @@ module.exports = {
  usage: "info",
  run: async (client, message, args) => {
   try {
-   if (process.env.DOMAIN) {
-    webpanel = `[Dashboard](${process.env.DOMAIN}) |`;
-   } else {
-    webpanel = " ";
-   }
+   const botuptime = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+   const osuptime = moment.duration(os.uptime()).format(" D [days], H [hrs], m [mins], s [secs]");
    function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
    }
-   const botuptime = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-   const osuptime = moment.duration(os.uptime()).format(" D [days], H [hrs], m [mins], s [secs]");
    cpu.usage().then((cpupercentage) => {
-    netstat.inOut().then((netusage) => {
-     console.log(netusage);
-     const embed = new Discord.MessageEmbed() // Prettier()
-      .setTitle(
-       `📄 Generic Information`,
-       message.guild.iconURL({
-        dynamic: true,
-        format: "png",
-       })
-      )
-      .setColor("RANDOM")
-      .setDescription(`Global prefix: \`${process.env.PREFIX}\`\n`)
-      .addField("<:owner:856161806199947285> Developer", `<@${config.owner_id}> \[[Website](${config.aurhor_website})\]`)
-      .setThumbnail(
-       client.user.displayAvatarURL({
-        dynamic: true,
-        format: "png",
-        size: 2048,
-       })
-      )
-      .addField("<:discordlogo:856166057639149568> Guild Count", `\`${client.guilds.cache.size} guilds\``, true)
-      .addField("<:members:856161806606401556> User Count", `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members\``, true)
-      .addField("<:channel:856161806586085376> Channel Count", `\`${client.channels.cache.size} channels\``, true)
-      .addField("🏓 Ping", "`" + Math.round(client.ws.ping) + "ms`", true)
-      .addField("<:discduckmicroshit:856174395205615647>  Operating System", "`" + capitalizeFirstLetter(osutils.platform()) + " (" + os.arch() + ")`", true)
-      .addField("<:node:792080285205790760> Node", "`" + process.version + "`", true)
-      .addField("<:discordjs:756951296392560811> Discord.js", "`" + dependencies["discord.js"].replace("^", "v") + "`", true)
-      .addField("⏳ Uptime", `Bot: \`${botuptime}\` | Server: \`${osuptime}\``)
-      .addField("<:cpu:856174395436171294> CPU", "```" + cpu.model() + " (" + cpu.count() + " cores)" + " [" + cpupercentage + "% used / " + proc + " proc. running]```")
-      .addField("<:ram:856174395508391986> RAM Usage (VPS)", `\`\`\`${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)\`\`\``)
-      .addField("<:ram:856174395508391986> RAM Usage (BOT)", "```" + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB " + `(${((100 * (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)) / osutils.totalmem().toString().split(".")[0]).toFixed(2)}%)` + "```")
-      .addField("🔗 Useful Links", `[Support server](${config.support_server}) | ${webpanel} [Invite me](https://discord.com/oauth2/authorize/?permissions=${config.permissions}&scope=bot&client_id=${client.user.id})`)
-      .setFooter(
-       "Requested by " + `${message.author.username}`,
-       message.author.displayAvatarURL({
-        dynamic: true,
-        format: "png",
-        size: 2048,
-       })
-      );
-     message.lineReply(embed);
-    });
+    const embed = new Discord.MessageEmbed() // Prettier()
+     .setTitle(
+      `📄 Generic Information`,
+      message.guild.iconURL({
+       dynamic: true,
+       format: "png",
+      })
+     )
+     .setColor("RANDOM")
+     .setDescription(`Global prefix: \`${process.env.PREFIX}\`\n`)
+     .addField("<:owner:856161806199947285> Developer", `<@${config.owner_id}> \[[Website](${config.aurhor_website})\]`)
+     .setThumbnail(
+      client.user.displayAvatarURL({
+       dynamic: true,
+       format: "png",
+       size: 2048,
+      })
+     )
+     .addField("<:discord_2:876473291777404959> Guild Count", `\`${client.guilds.cache.size} guilds\``, true)
+     .addField("<:members:856161806606401556> User Count", `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members\``, true)
+     .addField("<:channel:856161806586085376> Channel Count", `\`${client.channels.cache.size} channels\``, true)
+     .addField("💿 Operating System", "`" + capitalizeFirstLetter(osutils.platform()) + " (" + os.arch() + ")`", true)
+     .addField("📦 Tools", `\`\`\`Node.js: ${process.version} | Discord.js: ${dependencies["discord.js"].replace("^", "v")}`)
+     .addField("⏳ Uptime", `\`\`\`Bot: ${botuptime} | Server: ${osuptime}\`\`\``)
+     .addField("🏓 Ping", `\`\`\`Bot: ${Math.round(client.ws.ping)} | API: ${(Date.now() - message.createdTimestamp).toString().replace(/-/g, "")}`)
+     .addField("<:cpu:856174395436171294> CPU", "```" + cpu.model() + " (" + cpu.count() + " cores)" + " [" + cpupercentage + "% used / " + proc + " proc. running]```")
+     .addField("<:ram:856174395508391986> RAM Usage (VPS)", `\`\`\`${(osutils.totalmem() - osutils.freemem()).toString().split(".")[0] + "." + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[0] + (osutils.totalmem() - osutils.freemem()).toString().split(".")[1].split("")[1]}/${osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1]}MB (${(100 - osutils.freememPercentage() * 100).toString().split(".")[0] + "." + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[0] + (100 - osutils.freememPercentage() * 100).toString().split(".")[1].split("")[1]}%)\`\`\``)
+     .addField("<:ram:856174395508391986> RAM Usage (BOT)", "```" + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB/" + osutils.totalmem().toString().split(".")[0] + "." + osutils.totalmem().toString().split(".")[1].split("")[0] + osutils.totalmem().toString().split(".")[1].split("")[1] + "MB " + `(${((100 * (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)) / osutils.totalmem().toString().split(".")[0]).toFixed(2)}%)` + "```")
+     .addField("🔗 Useful Links", `[Support server](${config.support_server}) |${(process.env.DOMAIN) ? " [Dashboard](" + process.env.DOMAIN + "}) |" : ""} [Invite me](https://discord.com/oauth2/authorize/?permissions=${config.permissions}&scope=bot&client_id=${client.user.id})`)
+     .setFooter(
+      "Requested by " + `${message.author.username}`,
+      message.author.displayAvatarURL({
+       dynamic: true,
+       format: "png",
+       size: 2048,
+      })
+     );
+    message.lineReply(embed);
    });
   } catch (err) {
    console.log(err);
