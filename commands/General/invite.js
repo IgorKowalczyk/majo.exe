999ffc7d1f6be2b1eacec44b634809c08600d6b5
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-16 11:32:34 +0200

    Add title to invite command

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -11,6 +11,7 @@ module.exports = {
   try {
    const embed = new Discord.MessageEmbed() // Prettier
     .setColor("RANDOM")
+    .setTitle("<:success:860884617820110909> Yay!")
     .setDescription(`:tada: **[Click this link to invite me!](https://discord.com/oauth2/authorize/?permissions=${config.permissions}&scope=${config.scopes}&client_id=${client.user.id})** **__[Recomended!]__**\nOr [click this link to invite me *as root*](https://discord.com/oauth2/authorize/?permissions=8&scope=${config.scopes}&client_id=${client.user.id}) [Not recomended!]`)
     .setTimestamp()
     .setFooter(
