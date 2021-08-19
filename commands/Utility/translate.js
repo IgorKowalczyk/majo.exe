cb732887d53c986a030ab36ffc97edeff7c4c523
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-19 20:35:10 +0200

    Update translate [Out of wip]

--- commands/Utility/translate.js
+++ commands/Utility/translate.js
@@ -28,10 +28,9 @@ module.exports = {
     });
    }
    translate(text, { to: language.toLowerCase()}).then(res => {
-    console.log(res.from.language);
     const embed = new Discord.MessageEmbed()
     .setTitle(`${client.bot_emojis.success} Success!`)
-    .setDescription(`From: \`${res.from.language}\`\nTo: \`${language.toLowerCase()}\``)
+    .setDescription(`From: \`${JSON.parse(res.from.language).iso}\`\nTo: \`${language.toLowerCase()}\``)
     .addField(`${client.bot_emojis.input} Text to translate`, "```" + text + "```")
     .addField(`${client.bot_emojis.output} Tanslated text`, "```" + res.text + "```")
     .setColor("RANDOM")
