2578325dc9b744ac62db83ad1459fc85f07bbe98
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-22 21:05:34 +0200

    Update image

--- commands/General/servers.js
+++ commands/General/servers.js
@@ -18,7 +18,7 @@ module.exports = {
       size: 2048,
      })
     )
-    .setImage("https://media.discordapp.net/attachments/721019707607482409/879067533557256263/10-37-discorddiscord-discord-discorddiscord-messenger-oh-no-40275865.png?width=338&height=645")
+    .setImage("https://media.discordapp.net/attachments/721019707607482409/879068001264107520/hehehe-gangbang-hehhehe.png")
     .setColor("RANDOM")
     .setTimestamp();
    message.lineReply(embed);
