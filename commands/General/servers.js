c9b1bca912ff11795072e0ba1a50f1b14ee52000
IgorKowalczyk <xxmajonezxx@gmail.com>
2021-08-22 21:26:33 +0200

    Update servers

--- commands/General/servers.js
+++ commands/General/servers.js
@@ -9,7 +9,7 @@ module.exports = {
  run: async (client, message, args) => {
   try {
    const embed = new Discord.MessageEmbed() // Prettier
-    .setDescription(`${client.bot_emojis.rocket} I'm in \`${client.guilds.cache.size}\` servers!`)
+    .setTitle(`${client.bot_emojis.rocket} I'm in \`${client.guilds.cache.size}\` servers!`)
     .setFooter(
      `Requested by ${message.author.username}`,
      message.author.displayAvatarURL({
