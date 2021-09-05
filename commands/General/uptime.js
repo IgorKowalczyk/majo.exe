ad8fdbd35cd824ef51166a06ca9b4a6706041227
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-05 17:31:24 +0200

    Create uptime.js

---
+++ commands/General/uptime.js
@@ -0,0 +1,49 @@
+
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+require("moment-duration-format");
+
+module.exports = {
+ name: "uptime",
+ aliases: ["bot", "botuptime"],
+ description: "Display a bot uptime",
+ category: "General",
+ usage: "uptime",
+ run: async (client, message, args) => {
+  try {
+   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+   const date = new Date();
+   const timestamp = date.getTime() - Math.floor(client.uptime);
+   const embed = new MessageEmbed() // Prettier
+    .setTitle(
+     `${client.bot_emojis.uptime} Uptime`,
+     message.guild.iconURL({
+      dynamic: true,
+      format: "png",
+     })
+    )
+    .addField(`${client.bot_emojis.stopwatch} Uptime`, `\`\`\`${duration}\`\`\``)
+    .addField(`${client.bot_emojis.rocket} Date Launched`, `\`\`\`${moment(timestamp).format("LLLL")}\`\`\``)
+    .setTimestamp()
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    )
+    .setColor("RANDOM");
+   if (client.config.status) {
+    embed.addField(`${client.bot_emojis.status_online} Servers Status`, "```" + client.config.status + "```");
+   }
+   message.reply({embeds: [embed]});
+  } catch (err) {
+   console.log(err);
+   const error = new MessageEmbed() // Prettier
+    .setDescription(`Something went wrong... ${client.bot_emojis.sadness}`)
+    .setColor("RED");
+   message.reply({ embeds: [error] });
+  }
+ },
+};
