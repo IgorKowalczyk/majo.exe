36ae57fc86f2188e7cc3c7b24237bfd9e61407b6
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-08 16:32:06 +0200

    Update servers.js

--- commands/General/servers.js
+++ commands/General/servers.js
@@ -10,6 +10,8 @@ module.exports = {
   try {
    const embed = new MessageEmbed() // Prettier
     .setTitle(`${client.bot_emojis.rocket} I'm in \`${client.guilds.cache.size}\` servers!`)
+    .addField(`${client.bot_emojis.member} User Count`, `\`${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} members\``, true)
+    .addField(`${client.bot_emojis.channel} Channel Count`, `\`${client.channels.cache.size} channels\``, true)
     .setFooter(
      `Requested by ${message.author.username}`,
      message.author.displayAvatarURL({
@@ -21,14 +23,14 @@ module.exports = {
     .setImage("https://media.discordapp.net/attachments/710425657003212810/884064564034023454/Screenshot_2021-09-05-15-16-44-22_7c6675ada7b05a8d2d5c5ffa2a487337.jpg")
     .setColor("RANDOM")
     .setTimestamp();
-   const row = new MessageActionRow()// Prettier
-   .addComponents(
-    new MessageButton() // Prettier
-     .setURL(`https://discord.com/oauth2/authorize/?permissions=${client.config.permissions}&scope=${client.config.scopes}&client_id=${client.user.id}`)
-     .setEmoji(client.bot_emojis.giveaway)
-     .setLabel("Invite me!")
-     .setStyle("LINK")
-   );
+   const row = new MessageActionRow() // Prettier
+    .addComponents(
+     new MessageButton() // Prettier
+      .setURL(`https://discord.com/oauth2/authorize/?permissions=${client.config.permissions}&scope=${client.config.scopes}&client_id=${client.user.id}`)
+      .setEmoji(client.bot_emojis.channel)
+      .setLabel("Invite me!")
+      .setStyle("LINK")
+    );
    message.reply({ embeds: [embed], components: [row] });
   } catch (err) {
    console.log(err);
