a45d5926b7c9e1887d92fc4feb2a067a1781152b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-12 14:46:54 +0200

    Create bot-token.js

---
+++ commands/Fun/bot-token.js
@@ -0,0 +1,37 @@
+
+const { MessageEmbed } = require("discord.js");
+const axios = require("axios").default;
+
+module.exports = {
+ name: "bot-token",
+ aliases: ["discord-token"],
+ description: "Generate (fake) random Discord Bot token",
+ category: "Fun",
+ usage: "bot-token",
+ run: async (client, message, args) => {
+   try {
+   const options = {
+    method: "GET",
+    url: "https://some-random-api.ml/bottoken",
+   };
+   axios.request(options).then((response) => {
+    const embed = new MessageEmbed()
+     .setColor("RANDOM")
+     .setFooter(
+      `Requested by ${message.author.username}`,
+      message.author.displayAvatarURL({
+       dynamic: true,
+       format: "png",
+       size: 2048,
+      })
+     )
+     .setTitle(`${client.bot_emojis.discord_logo} Random Discord Token`)
+     .setDescription("```" + response.data.token + "```\n||Notice: This token is automatically generated, it is not a real token for discord bot! It is only supposed to look like this!||");
+    message.reply({embeds: [embed]});
+   });
+   } catch (err) {
+   console.log(err);
+   message.reply({ embeds: [client.command_error_embed] });
+  }
+ },
+};
