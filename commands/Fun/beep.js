0218d1c107fac7faf65ac93b6311af5895d606d9
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-12 13:00:02 +0200

    Create beep.js

---
+++ commands/Fun/beep.js
@@ -0,0 +1,30 @@
+
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "beep",
+ aliases: [],
+ description: "Beep-Boop!",
+ category: "Fun",
+ usage: "beep",
+ run: async (client, message, args) => {
+  try {
+   const embed = new MessageEmbed() // Prettier
+    .setColor("RANDOM")
+    .setTitle(`${client.bot_emojis.clock} Boop!`)
+    .setImage(`https://c.tenor.com/xEv4LjI27pkAAAAC/time-clock.gif`)
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   message.reply({ embeds: [embed]});
+  } catch (err) {
+   console.log(err);
+   message.reply({ embeds: [client.command_error_embed] });
+  }
+ },
+};
