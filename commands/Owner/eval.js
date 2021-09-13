1d107f734ae02d9d382f2777a906cf334aaea4de
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-13 13:13:20 +0200

    Create eval.js

---
+++ commands/Owner/eval.js
@@ -0,0 +1,59 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "eval",
+ aliases: [],
+ description: "Evaluates and runs JavaScript code",
+ category: "Owner",
+ usage: "eval <code>",
+ run: async (client, message, args) => {
+  try {
+   if (message.author.id !== client.config.owner_id) {
+    const embed = new MessageEmbed() // Prettier
+     .setColor("RED")
+     .setDescription(`${client.bot_emojis.error} | You do not have permission to run this command (Only owner of the bot can run this)!`);
+    return message.reply({ embeds: [embed] });
+   }
+   var result = args.join(" ");
+   if (!result) {
+    const embed = new MessageEmbed() // Prettier
+     .setColor("RED")
+     .setDescription(`${client.bot_emojis.error} | Please input code to evaluate!`);
+    return message.reply({ embeds: [embed] });
+   }
+   let evaluated = eval(result);
+   let type = typeof evaluated;
+   console.log("Code to eval: " + result);
+   const success = new MessageEmbed() // Prettier
+    .setColor("#2f3136")
+    //.setTitle("💡 Eval")
+    .addField(`${client.bot_emojis.screw_that} Type`, "```js\n" + `${type}` + "```")
+    .addField(`${client.bot_emojis.input} Input`, "```js\n" + `${args.join(" ")}` + "```")
+    .addField(`${client.bot_emojis.output} Output`, "```js\n" + evaluated + "```")
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   message.reply({ embeds: [success] });
+  } catch (err) {
+   const errormessage = new MessageEmbed() // Prettier
+    .setColor("#e31212")
+    .setTitle("An error has occured")
+    .addField(`Input:\n`, "```js\n" + `${result}` + "```", false)
+    .addField(`Output:\n`, "```js\n" + `${err.message}` + "```", true)
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   return message.reply({ embeds: [errormessage] });
+  }
+ },
+};
