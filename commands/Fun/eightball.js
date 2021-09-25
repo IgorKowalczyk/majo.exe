05264132650ed9b6815772f38985a54adba7520b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-09-25 12:22:58 +0200

    Add new commands

---
+++ commands/Fun/eightball.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "eightball",
+ aliases: ["8ball", "fortune"],
+ description: "Tells you a fortune",
+ category: "Fun",
+ usage: "eightball <question>",
+ run: async (client, message, args) => {
+  try {
+   if (!args.length)
+    return message.lineReply({
+     embed: {
+      color: 16734039,
+      description: `${client.bot_emojis.error} | You need to enter question :/\n\n**Usage:** \`${process.env.PREFIX} 8ball <question>\``,
+     },
+    });
+   const fortunes = ["Yes.", "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definelty.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now...", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good...", "Very doubtful."];
+   const embed = new MessageEmbed() // Prettier
+    .setDescription(`${client.bot_emojis.magic_ball} | ${fortunes[Math.floor(Math.random() * fortunes.length)]}`)
+    .setTimestamp()
+    .setColor("RANDOM")
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   await message.reply({embeds: [embed]});
+  } catch (err) {
+   console.log(err);
+   message.reply({ embeds: [client.command_error_embed] });
+  }
+ },
+};
