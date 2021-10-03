307c8acc41166eefd8e02389ab87535939c3b504
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-03 17:00:49 +0200

    Add new commands

---
+++ commands/Fun/sneeze.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "sneeze",
+ aliases: [],
+ description: "Achoo!",
+ category: "Fun",
+ usage: "sneeze",
+ run: async (client, message, args) => {
+  try {
+   const sneezes = ["**Achoo!**", "*chew!*", "Ah... Ah... **A_CHOO!_**", "_Ah..._***CHOOOOOOOOOOOOOOOOOOOO!***", "*Achoo!* Excuse me!"];
+   const embed = new MessageEmbed() // Prettier
+    .setColor("RANDOM")
+    .setTitle(`${client.bot_emojis.sneeze} ${sneezes[Math.floor(Math.random() * Math.floor(sneezes.length))]}`)
+    .setImage(`https://media.discordapp.net/attachments/709721624588320840/894226312019968070/sneeze.gif`)
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   message.reply({ embeds: [embed] });
+  } catch (err) {
+   console.log(err);
+   return client.createCommandError(message, err);
+  }
+ },
+};
