a8870c433c00842ed4aff68f22684e7fe66e6aba
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-04 16:39:50 +0200

    Add ban command

---
+++ commands/Moderation/ban.js
@@ -0,0 +1,50 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "ban",
+ aliases: [],
+ description: "Ban a member",
+ category: "Moderation",
+ usage: "ban <mention> [reason]",
+ run: async (client, message, args) => {
+  try {
+   if (!message.guild.me.permissions.has("BAN_MEMBERS")) {
+    return client.createError(message, `${client.bot_emojis.error} | I don't have premission to ban members!`);
+   }
+   if (!message.member.permissions.has("BAN_MEMBERS")) {
+    return client.createError(message, `${client.bot_emojis.error} | You don't have premission to ban members!`);
+   }
+   let mentioned = await message.mentions.members.first();
+   let message_args = await args.slice(1).join(" ");
+   let reason = `${message_args || "No reason provided!"} | Banned by: ${message.author.tag}`
+   if (!mentioned) {
+    return client.createError(message, `${client.bot_emojis.error} | Mention a valid member!\n\n**Usage:** \`${client.prefix} ban <mention> [reason]\``);
+   }
+   if (!mentioned.bannable) {
+    return client.createError(message, `${client.bot_emojis.error} | You cannot ban this member!`);
+   }
+   if (message.author === mentioned) {
+    return client.createError(message, `${client.bot_emojis.error} | You can't ban yourself!`);
+   }
+   message.guild.members.ban(mentioned, {
+    reason: reason,
+   });
+   const embed = new MessageEmbed()
+    .setDescription(`${client.bot_emojis.success} | ${mentioned.displayName} has been banned.\n\n>>> Reason: \`${reason}\``)
+    .setTimestamp()
+    .setColor("GREEN")
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+ await message.reply({embeds: [embed]})
+  } catch (err) {
+   console.log(err);
+   return client.createCommandError(message, err);
+  }
+ },
+};
