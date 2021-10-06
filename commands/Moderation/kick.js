9dd2b75d09c7167743b30fca6eb05cacd5a12b44
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-06 11:16:35 +0200

    Add new commands

---
+++ commands/Moderation/kick.js
@@ -0,0 +1,50 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "kick",
+ aliases: [],
+ description: "Kick a member",
+ category: "Moderation",
+ usage: "kick <mention> [reason]",
+ run: async (client, message, args) => {
+  try {
+   if (!message.guild.me.permissions.has("KICK_MEMBERS")) {
+    return client.createError(message, `${client.bot_emojis.error} | I don't have premission to kick members!`);
+   }
+   if (!message.member.permissions.has("KICK_MEMBERS")) {
+    return client.createError(message, `${client.bot_emojis.error} | You don't have premission to kick members!`);
+   }
+   let mentioned = await message.mentions.members.first();
+   let message_args = await args.slice(1).join(" ");
+   let reason = `${message_args || "No reason provided!"} | Kicked by: ${message.author.tag}`;
+   if (!mentioned) {
+    return client.createError(message, `${client.bot_emojis.error} | Mention a valid member!\n\n**Usage:** \`${client.prefix} kick <mention> [reason]\``);
+   }
+   if (!mentioned.kickable) {
+    return client.createError(message, `${client.bot_emojis.error} | You cannot kick this member!`);
+   }
+   if (message.author === mentioned) {
+    return client.createError(message, `${client.bot_emojis.error} | You can't kick yourself!`);
+   }
+   message.guild.members.kick(mentioned, {
+    reason: reason,
+   });
+   const embed = new MessageEmbed()
+    .setDescription(`${client.bot_emojis.success} | ${mentioned.displayName} has been kicked.\n\n>>> Reason: \`${reason}\``)
+    .setTimestamp()
+    .setColor("GREEN")
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    );
+   await message.reply({ embeds: [embed] });
+  } catch (err) {
+   console.log(err);
+   return client.createCommandError(message, err);
+  }
+ },
+};
