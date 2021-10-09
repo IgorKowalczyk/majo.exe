6ce56c0224356c0bbd1280ea6d45a1e12fa6cee1
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-09 21:05:26 +0200

    Update dashboard

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -52,17 +52,16 @@ module.exports = async (client) => {
  );
  app.use(express.static("dashboard/static"));
 
-
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.expectCt());
-app.use(helmet.frameguard());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.hsts());
-app.use(helmet.ieNoOpen());
-app.use(helmet.noSniff());
-app.use(helmet.permittedCrossDomainPolicies());
-app.use(helmet.referrerPolicy());
-app.use(helmet.xssFilter());
+ app.use(helmet.dnsPrefetchControl());
+ app.use(helmet.expectCt());
+ app.use(helmet.frameguard());
+ app.use(helmet.hidePoweredBy());
+ app.use(helmet.hsts());
+ app.use(helmet.ieNoOpen());
+ app.use(helmet.noSniff());
+ app.use(helmet.permittedCrossDomainPolicies());
+ app.use(helmet.referrerPolicy());
+ app.use(helmet.xssFilter());
  app.use((req, res, next) => {
   res.setHeader("Permissions-Policy", "	accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()");
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -161,9 +160,9 @@ app.use(helmet.xssFilter());
    }
   }
  );
-   
+
  // Features list redirect endpoint.
-  app.get("/profile", checkAuth, async (req, res) => {
+ app.get("/profile", checkAuth, async (req, res) => {
   renderTemplate(res, req, "profile.ejs", {
    perms: Discord.Permissions,
   });
