f8907af39f46b3b0c28f1ab0c99b355fe5e06ac7
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-10-11 13:13:48 +0200

    Create base64-decode.js

---
+++ commands/Utility/base64-decode.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+ name: "base64-decode",
+ aliases: ["b64-decode", "base-64-decode"],
+ description: "Decode Base64 text to normal",
+ category: "Utility",
+ usage: "base64-decode <text>",
+ run: async (client, message, args) => {
+  try {
+   if (!args[0]) {
+return client.createError(message, `${client.bot_emojis.error} | You must enter a Base64 code to decode!`);
+   }
+   if (!args[0].lenght > 50) {
+return client.createError(message, `${client.bot_emojis.error} | You must enter a Base64 code shorer than 50 characters!`);
+   }
+   const buffer = new Buffer.from(args.join(" "), "base64");
+   const base64 = buffer.toString("utf-8");
+   const embed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setFooter(
+     `Requested by ${message.author.username}`,
+     message.author.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     })
+    )
+    .setTitle(`${client.bot_emojis.sparkles} Base64 Decoder`)
+    .addField(`${client.bot_emojis.input} Text to decode`, `\`\`\`${args.join(" ")}\`\`\``)
+    .addField(`${client.bot_emojis.output} Decoded text`, `\`\`\`${base64 || "An unknown error ocurred while decoding!"}\`\`\``);
+   message.reply({embeds: [embed]});
+  } catch (err) {
+   console.log(err);
+   return client.createCommandError(message, err);
+  }
+ },
+};
