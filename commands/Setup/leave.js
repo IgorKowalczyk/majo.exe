cc5db876c146df4bdc8f603ed47bff54cfb935bf
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-10-18 12:13:09 +0200

    Fix leave command

--- commands/Setup/leave.js
+++ commands/Setup/leave.js
@@ -15,7 +15,7 @@ module.exports = {
    if (args[0]) {
     const channel = message.mentions.channels.first();
     if (!channel) {
-     return client.createError(message, `${client.bot_emojis.error} | You must mention channel to set-up\n\n**Usage:** \`${client.prefix} welcome [channel]\``);
+     return client.createError(message, `${client.bot_emojis.error} | You must mention channel to set-up\n\n**Usage:** \`${client.prefix} leave [channel]\``);
     }
     const user_perms = channel.permissionsFor(message.author);
     const bot_perms = channel.permissionsFor(message.guild.me);
@@ -55,12 +55,12 @@ module.exports = {
      if (results[0]) {
       const embed = new MessageEmbed() // Prettier
        .setColor("GREEN")
-       .setDescription(`${client.bot_emojis.success} | Your current leave channel is: <#${results[0].res}>. You can channge channel by using \`${client.prefix} welcome [channel]\`!`);
+       .setDescription(`${client.bot_emojis.success} | Your current leave channel is: <#${results[0].res}>. You can channge channel by using \`${client.prefix} leave [channel]\`!`);
       return message.reply({ embeds: [embed] });
      } else {
       const embed = new MessageEmbed() // Prettier
        .setColor("GREEN")
-       .setDescription(`${client.bot_emojis.error} | You haven't configured leave channel on this server yet, run \`${prefix} leave [channel]\` to configure welcome messages!`);
+       .setDescription(`${client.bot_emojis.error} | You haven't configured leave channel on this server yet, run \`${prefix} leave [channel]\` to configure leave messages!`);
       return message.reply({ embeds: [embed] });
      }
     });
