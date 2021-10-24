85ed53f456623ef6ba0201acdc22e88a971d82c5
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-24 18:21:42 +0200

    Format code

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -246,13 +246,13 @@ module.exports = async (client) => {
  app.get("/api", function (req, res) {
   res.header("Content-Type", "application/json");
   const json = {
-    guilds: client.guilds.cache.size,
-    id: client.id,
-    members: client.guilds.cache.reduce((a, g) => a + g.memberCount, 0),
-    prefix: process.env.PREFIX,
-    channels: client.channels.cache.size,
-    uptime: client.uptime
-  }
+   guilds: client.guilds.cache.size,
+   id: client.id,
+   members: client.guilds.cache.reduce((a, g) => a + g.memberCount, 0),
+   prefix: process.env.PREFIX,
+   channels: client.channels.cache.size,
+   uptime: client.uptime,
+  };
   res.send(json);
  });
 
