ea5564748c38968b0be4e73ef78a43e7513dbeb2
igorkowalczyk <xxmajonezxx@gmail.com>
2021-10-25 19:28:03 +0200

    Update serverinfo

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -18,16 +18,16 @@ module.exports = {
     .setAuthor(message.guild.name, message.guild.iconURL())
     .setColor("#4f545c")
     .setThumbnail(message.guild.iconURL())
-    .addField(`${client.bot_emojis.owner_crown} Owner`, `<@${message.guild.ownerId}> (ID: \`${message.guild.ownerId}\`)`, true)
-    .addField(`${client.bot_emojis.channel} Server ID`, `\`${message.guild.id}\``, true)
-    .addField(`${client.bot_emojis.discord_logo} Description`, `${message.guild.description || "No server description!"}`)
+    .addField(`${client.bot_emojis.owner_crown} Owner`, `> <@${message.guild.ownerId}> (ID: \`${message.guild.ownerId}\`)`, true)
+    .addField(`${client.bot_emojis.channel} Server ID`, `> \`${message.guild.id}\``, true)
+    .addField(`${client.bot_emojis.discord_logo} Description`, `> ${message.guild.description || "No server description!"}`)
     .addField(`${client.bot_emojis.member} Members`, `\`${message.guild.memberCount}/${message.guild.maximumMembers}\` members (\`${message.guild.members.cache.filter((member) => member.user.bot).size}\` bots)`)
     //.addField(`${client.bot_emojis.channel} Channels`, message.guild.channels.cache.size, true)
     //.addField(`${client.bot_emojis.role} Roles`, message.guild.roles.cache.size, true)
-    .addField(`${client.bot_emojis.discord_badges} Emojis`, `Total emojis: \`${message.guild.emojis.cache.size}\``, true)
-    .addField(`${client.bot_emojis.boosts_animated} Boosts`, `\`${message.guild.premiumSubscriptionCount}\` (${capitalize(message.guild.premiumTier.toLowerCase().replace("_", " "))})`, true)
-    .addField(`${client.bot_emojis.lock} Verification`, `\`${capitalize(message.guild.verificationLevel.toLowerCase().replace("_", " "))}\``, true)
-    .addField(`${client.bot_emojis.stopwatch} Creation Date`, `<t:${moment(message.channel.guild.createdTimestamp).unix()}> <t:${moment(message.channel.guild.createdTimestamp).unix()}:R>`, true)
+    .addField(`${client.bot_emojis.discord_badges} Emojis`, `> Total emojis: \`${message.guild.emojis.cache.size}\``, true)
+    .addField(`${client.bot_emojis.boosts_animated} Boosts`, `> \`${message.guild.premiumSubscriptionCount}\` (${capitalize(message.guild.premiumTier.toLowerCase().replace("_", " "))})`, true)
+    .addField(`${client.bot_emojis.lock} Verification`, `> \`${capitalize(message.guild.verificationLevel.toLowerCase().replace("_", " "))}\``, true)
+    .addField(`${client.bot_emojis.stopwatch} Creation Date`, `> <t:${moment(message.channel.guild.createdTimestamp).unix()}> (<t:${moment(message.channel.guild.createdTimestamp).unix()}:R>)`, true)
     .setFooter(
      `Requested by ${message.author.username}`,
      message.author.displayAvatarURL({
