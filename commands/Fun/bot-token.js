fa975690068dd86d7e3c3b72bedbb4973448d8e1
igorkowalczyk <xxmajonezxx@gmail.com>
2021-11-06 13:00:36 +0100

    Update bot-token command

--- commands/Fun/bot-token.js
+++ commands/Fun/bot-token.js
@@ -25,7 +25,7 @@ module.exports = {
       })
      )
      .setTitle(`${client.bot_emojis.discord_logo} Discord Token`)
-     .setDescription("```" + response.data.token + "```\n>>> ||Notice: This token is automatically generated, it is not a real token for discord bot! It is only supposed to look like this!||");
+     .setDescription("> ```" + response.data.token + "```\n>>> ||Notice: This token is automatically generated, it is not a real token for discord bot! It is only supposed to look like this!||");
     message.reply({ embeds: [embed] });
    });
   } catch (err) {
