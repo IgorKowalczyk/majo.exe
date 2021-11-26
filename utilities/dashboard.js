2260fc76d3f249ed0d2a0a232d5c982822f17df8
igorkowalczyk <xxmajonezxx@gmail.com>
2021-11-26 16:05:30 +0100

    Fix webhooks

--- utilities/dashboard.js
+++ utilities/dashboard.js
@@ -3,5 +3,5 @@ if (!process.env.SECRET) throw new Error("[HOST] You need to provide Secret in .
 if (!process.env.PORT) throw new Error("[HOST] You need to provide Port in .env - PORT=YOUR_WEBSITE_PORT");
 if (!process.env.ID) throw new Error("[HOST] You need to provide Discord Bot ID in .env - ID=YOUR_DISCORD_BOT_ID");
 if (!process.env.DOMAIN) throw new Error("[HOST] You need to provide Webiste domain in .env - DOMAIN=YOUR_WEBISTE_DOMAIN Note: Only website domain eg. https://example.com without slash at end!");
-if (!process.env.CONTACT_WEBHOOK) throw new Error("[HOST] You need to provide Discord Contact Webhook ID in .env - CONTACT_WEBHOOK_ID=YOUR_WEBHOOK_ID");
+if (!process.env.CONTACT_WEBHOOK) throw new Error("[HOST] You need to provide Discord Contact Webhook in .env - CONTACT_WEBHOOK=YOUR_WEBHOOK_URL");
 if (!process.env.RECAPTCHA_KEY) throw new Error("[HOST] You need to provide Google Recaptcha v2 Token in .env - RECAPTCHA_KEY=YOUR_RECAPTCHA_TOKEN");
