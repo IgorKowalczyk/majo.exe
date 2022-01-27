f0f9eaf47331198cfcae45622de7c6aa9f46a8db
igorkowalczyk <xxmajonezxx@gmail.com>
2022-01-27 11:58:53 +0100

    Format code

--- bot/events/guild/messageCreate.js
+++ bot/events/guild/messageCreate.js
@@ -89,7 +89,9 @@ module.exports = async (client, message) => {
         .setStyle("SECONDARY")
       );
       const slash_embed = new MessageEmbed() // Prettier
-      .setTitle(`${client.bot_emojis.slash_commands} Try Majo.exe slash commands!`).setDescription(`>>> Majo.exe will soon switch to slash commands!\nWe are still working on this feature so please be patient :tada:! \n\n*❕Majo.exe will stop responding to normal prefixes <t:1651269660:R>*`).setColor("#4f545c");
+       .setTitle(`${client.bot_emojis.slash_commands} Try Majo.exe slash commands!`)
+       .setDescription(`>>> Majo.exe will soon switch to slash commands!\nWe are still working on this feature so please be patient :tada:! \n\n*❕Majo.exe will stop responding to normal prefixes <t:1651269660:R>*`)
+       .setColor("#4f545c");
       message.reply({ embeds: [slash_embed], components: [slash_row] }).then((m) =>
        setTimeout(() => {
         if (m.deletable) m.delete();
--- bot/slash_commands/Fun/ship.js
+++ bot/slash_commands/Fun/ship.js
@@ -30,49 +30,47 @@ module.exports = {
   const ship = Math.floor(Math.random() * 100) + 1;
   const bar = progressbar(100, ship, 10, `${client.bot_emojis.emoji_bar_1}`, `${client.bot_emojis.emoji_bar_2}`, `${client.bot_emojis.broken_heart} `, ` ${client.bot_emojis.heart}`, false);
   const mehh = new MessageEmbed() // Prettier
-  .setTitle(
-   `${client.bot_emojis.reverse_nr_2_motherfucker} This isn't a match`,
-   interaction.guild.iconURL({
-    dynamic: true,
-    format: "png",
+   .setTitle(
+    `${client.bot_emojis.reverse_nr_2_motherfucker} This isn't a match`,
+    interaction.guild.iconURL({
+     dynamic: true,
+     format: "png",
+    })
+   )
+   .setThumbnail("https://cdn.discordapp.com/emojis/853644938867769454.gif?v=1")
+   .setDescription(`I shipped **${user1}** with **${user2}** and it is **${ship}%**\n${bar}`)
+   .setFooter({
+    text: `Requested by ${interaction.user.username}`,
+    iconURL: interaction.user.displayAvatarURL({
+     dynamic: true,
+     format: "png",
+     size: 2048,
+    }),
    })
-  )
-  .setThumbnail("https://cdn.discordapp.com/emojis/853644938867769454.gif?v=1")
-  .setDescription(`I shipped **${user1}** with **${user2}** and it is **${ship}%**\n${bar}`)
-  .setFooter({
-   text: `Requested by ${interaction.user.username}`,
-   iconURL: interaction.user.displayAvatarURL({
-    dynamic: true,
-    format: "png",
-    size: 2048,
-   }),
-  })
-  .setColor("RED");
- const love = new MessageEmbed() // Prettier
-  .setTitle(
-   `${client.bot_emojis.reverse_nr_2_motherfucker} They are born for each others!`,
-   interaction.guild.iconURL({
-    dynamic: true,
-    format: "png",
+   .setColor("RED");
+  const love = new MessageEmbed() // Prettier
+   .setTitle(
+    `${client.bot_emojis.reverse_nr_2_motherfucker} They are born for each others!`,
+    interaction.guild.iconURL({
+     dynamic: true,
+     format: "png",
+    })
+   )
+   .setThumbnail("https://cdn.discordapp.com/emojis/797365365595439104.gif?v=1")
+   .setDescription(`I shipped **${user1}** with **${user2}** and it is **${ship}%**\n${bar}`)
+   .setFooter({
+    text: `Requested by ${interaction.user.username}`,
+    iconURL: interaction.user.displayAvatarURL({
+     dynamic: true,
+     format: "png",
+     size: 2048,
+    }),
    })
-  )
-  .setThumbnail("https://cdn.discordapp.com/emojis/797365365595439104.gif?v=1")
-  .setDescription(`I shipped **${user1}** with **${user2}** and it is **${ship}%**\n${bar}`)
-  .setFooter({
-   text: `Requested by ${interaction.user.username}`,
-   iconURL: interaction.user.displayAvatarURL({
-    dynamic: true,
-    format: "png",
-    size: 2048,
-   }),
-  })
-  .setColor("GREEN");
- if (ship > 50) {
-  interaction.followUp({ embeds: [love] });
- } else {
-  interaction.followUp({ embeds: [mehh] });
- }
- 
- 
- }
-}
+   .setColor("GREEN");
+  if (ship > 50) {
+   interaction.followUp({ embeds: [love] });
+  } else {
+   interaction.followUp({ embeds: [mehh] });
+  }
+ },
+};
