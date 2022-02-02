4e0e05d922d4045e579e7341cbc8c23b4c5a9b4a
igorkowalczyk <xxmajonezxx@gmail.com>
2022-02-02 14:17:18 +0100

    Fix api user endpoint

--- api/endpoints/v1/user.js
+++ api/endpoints/v1/user.js
@@ -2,7 +2,13 @@ const fetch = require("node-fetch");
 module.exports = (app, client) => {
  app.get("/api/v1/user/:userID", (req, res, next) => {
   if (!req.params.userID) {
-   res.json({
+   return res.json({
+    error: true,
+    message: "Invaild user ID!",
+   });
+  }
+  if (isNaN(req.params.userID)) {
+   return res.json({
     error: true,
     message: "Invaild user ID!",
    });
@@ -14,8 +20,14 @@ module.exports = (app, client) => {
      authorization: `Bot ${client.token}`,
     },
    });
+   if (!request.status === 200) {
+    return res.json({
+     error: true,
+     message: "Invaild user ID!",
+    });
+   }
    const json = await request.json();
-   res.json(json);
+   return res.json(json);
   })();
  });
 };
