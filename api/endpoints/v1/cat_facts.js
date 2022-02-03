c7fbc238a4d39da7eeb11484eba3f353201682a0
igorkowalczyk <xxmajonezxx@gmail.com>
2022-02-03 10:13:57 +0100

    Fix api endpoint link

--- api/endpoints/v1/cat_facts.js
+++ api/endpoints/v1/cat_facts.js
@@ -1,7 +1,7 @@
 const cat_facts = require("../../../src/json/catfacts.json");
 
 module.exports = (app, client) => {
- app.get("/api/v1/fun/cat_facts", (req, res, next) => {
+ app.get("/api/v1/fun/cat_fact", (req, res, next) => {
   const random = cat_facts.facts[Math.floor(Math.random() * cat_facts.facts.length)];
   res.json({
    cat_fact: random,
