0f8bcbb242445efb3de23853b9b6babd419bdd91
igorkowalczyk <xxmajonezxx@gmail.com>
2022-03-11 18:12:30 +0100

    Add web config [wip]

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const { glob } = require("glob");
 const { promisify } = require("util");
 const Discord = require("discord.js");
 const globPromise = promisify(glob);
+const api_config = require("../config/web_config").api;
 const all_endpoints = new Discord.Collection();
 
 module.exports = async (app, client, port, config, secure_connection) => {
@@ -30,7 +31,7 @@ module.exports = async (app, client, port, config, secure_connection) => {
  const endpoints = await globPromise(`${process.cwd()}/api/endpoints/*/*/*.js`);
  endpoints.map(async (value) => {
   const file = require(value.replace(".js", ""));
-  console.log(chalk.bold(chalk.bold.red("> ") + chalk.blue.bold("[API]")) + chalk.cyan.bold(` Loaded endpoint ${process.env.DOMAIN}${port == 8080 ? "" : `:${port}`}${file.name}`));
+  if(api_config.show_endpoints_list) console.log(chalk.bold(chalk.bold.red("> ") + chalk.blue.bold("[API]")) + chalk.cyan.bold(` Loaded endpoint ${process.env.DOMAIN}${port == 8080 ? "" : `:${port}`}${file.name}`));
   all_endpoints.set(file.name, file);
  });
 };
---
+++ config/web_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+ api: {
+  show_endpoints_list: false,
+ },
+ dashboard: {
+  // Soon!
+ }
+}
