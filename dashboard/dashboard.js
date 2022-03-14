e82794bdba09921782d733bcaec214096cf4cb64
Igor Kowalczyk <49127376+IgorKowalczyk@users.noreply.github.com>
2022-03-14 17:22:05 +0000

    Remove testing endpoints and add animation

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -297,11 +297,6 @@ module.exports = (client) => {
     renderTemplate(res, req, "index.ejs");
    });
 
-   // Testing endpoint.
-   app.get("/brand-new", (req, res) => {
-    renderTemplate(res, req, "brand_new.ejs");
-   });
-
    // Arc.io file
    app.get("/arc-sw.js", (req, res) => {
     res.sendFile(path.join(__dirname + "/static/js/arc.js"));
