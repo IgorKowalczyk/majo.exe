d9b5b3ca03ff8b586f89774dfe174e13d50a52c4
igorkowalczyk <xxmajonezxx@gmail.com>
2022-04-14 16:10:08 +0200

    Format code

--- bot/slash_commands/General/ping.js
+++ bot/slash_commands/General/ping.js
@@ -8,47 +8,47 @@ module.exports = {
  category: "General",
  run: async (client, interaction, args) => {
   try {
-  const date_ping = await moment(Date.now()).unix();
-  const websocket_ping = Math.round(client.ws.ping);
-  const wait = new MessageEmbed() // Prettier
-  .setColor("#5865f2")
-  .setDescription(`${client.bot_emojis.loading} | Pong!...`);
-    interaction.followUp({ embeds: [wait] }).then(async(msg) => {
-      const api_ping = Date.now() - msg.createdTimestamp;
-    await client.database.getConnection(async(err, conn) => {
-      if(err) return client.createSlashCommandError(interaction, err);
-      await conn.ping(function (err) {
-        let ping = moment(Date.now()).unix() - date_ping;
-         const ping_message = new MessageEmbed()
-          .addField(`${client.bot_emojis.stopwatch} API Request Latency`, `\`\`\`${(api_ping).toString().replace(/-/g, "")}ms\`\`\``)
-          .addField(`${client.bot_emojis.stopwatch} API Websocket Latency`, `\`\`\`${websocket_ping}ms\`\`\``)
-          .addField(`${client.bot_emojis.stopwatch} Database Latency`, `\`\`\`${ping == 0 ? 1 : ping}ms\`\`\``)
-          .setFooter({
-           text: `Requested by ${interaction.member.user.username}`,
-           iconURL: interaction.member.user.displayAvatarURL({
-            dynamic: true,
-            format: "png",
-            size: 2048,
-           }),
-          })
-          .setColor("#5865F2")
-          .setTimestamp()
-          .setTitle(`${client.bot_emojis.ping} Pong!`);
-         if (client.config.status) {
-          const row = new MessageActionRow().addComponents(
-           new MessageButton() // Prettier
-            .setURL(`${client.config.status}`)
-            //.setEmoji(client.bot_emojis.status_online)
-            .setLabel("Status page")
-            .setStyle("LINK")
-          );
-          msg.edit({ ephemeral: false, embeds: [ping_message], components: [row] });
-         } else {
-          msg.edit({ ephemeral: false, embeds: [ping_message] });
-         }
-        });
-       });
-    })
+   const date_ping = await moment(Date.now()).unix();
+   const websocket_ping = Math.round(client.ws.ping);
+   const wait = new MessageEmbed() // Prettier
+    .setColor("#5865f2")
+    .setDescription(`${client.bot_emojis.loading} | Pong!...`);
+   interaction.followUp({ embeds: [wait] }).then(async (msg) => {
+    const api_ping = Date.now() - msg.createdTimestamp;
+    await client.database.getConnection(async (err, conn) => {
+     if (err) return client.createSlashCommandError(interaction, err);
+     await conn.ping(function (err) {
+      let ping = moment(Date.now()).unix() - date_ping;
+      const ping_message = new MessageEmbed()
+       .addField(`${client.bot_emojis.stopwatch} API Request Latency`, `\`\`\`${api_ping.toString().replace(/-/g, "")}ms\`\`\``)
+       .addField(`${client.bot_emojis.stopwatch} API Websocket Latency`, `\`\`\`${websocket_ping}ms\`\`\``)
+       .addField(`${client.bot_emojis.stopwatch} Database Latency`, `\`\`\`${ping == 0 ? 1 : ping}ms\`\`\``)
+       .setFooter({
+        text: `Requested by ${interaction.member.user.username}`,
+        iconURL: interaction.member.user.displayAvatarURL({
+         dynamic: true,
+         format: "png",
+         size: 2048,
+        }),
+       })
+       .setColor("#5865F2")
+       .setTimestamp()
+       .setTitle(`${client.bot_emojis.ping} Pong!`);
+      if (client.config.status) {
+       const row = new MessageActionRow().addComponents(
+        new MessageButton() // Prettier
+         .setURL(`${client.config.status}`)
+         //.setEmoji(client.bot_emojis.status_online)
+         .setLabel("Status page")
+         .setStyle("LINK")
+       );
+       msg.edit({ ephemeral: false, embeds: [ping_message], components: [row] });
+      } else {
+       msg.edit({ ephemeral: false, embeds: [ping_message] });
+      }
+     });
+    });
+   });
   } catch (err) {
    console.log(err);
    return client.createSlashCommandError(interaction, err);
--- utilities/mysql/database.js
+++ utilities/mysql/database.js
@@ -31,35 +31,35 @@ class Database {
  setup() {
   if (this.connection.state !== "disconnected") {
    this.connection.query("CREATE TABLE IF NOT EXISTS `guild_settings` ( `guild_id` BIGINT(64) NOT NULL , `anti_selfbots` INT(1), `prefix` VARCHAR(10) DEFAULT '', PRIMARY KEY (`guild_id`)); ", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"guild_settings"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `guild_stats` ( `guild_id` BIGINT(64) NOT NULL , `joins` JSON NULL, `leaves` JSON NULL, `last_updated` DATE NOT NULL, PRIMARY KEY (`guild_id`));", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"guild_stats"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `users` ( `user_id` varchar(32) NOT NULL , `reputation` bigint(20), `ban` INT(1), PRIMARY KEY(`user_id`))", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"users"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `giveaways` (`id` INT(1) NOT NULL AUTO_INCREMENT, `message_id` VARCHAR(64) NOT NULL, `data` JSON NOT NULL, PRIMARY KEY (`id`));", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"giveaways"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `welcome` (`guildid` VARCHAR(32) NOT NULL, `channelid` VARCHAR(32) NOT NULL, UNIQUE(`guildid`));", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"welcome"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `leave` (`guildid` VARCHAR(32) NOT NULL, `channelid` VARCHAR(32) NOT NULL, UNIQUE(`guildid`));", (error) => {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"leave"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
    this.connection.query("CREATE TABLE IF NOT EXISTS `logs` (`guildid` VARCHAR(32) NOT NULL, `channelid` VARCHAR(32) NOT NULL, UNIQUE(`guildid`));", function (error, results, fields) {
-    if (error & config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
+    if (error & config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Database error: ${error.code}`));
     if (config.show_tables) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.cyan.bold(" Fetched table ") + chalk.blue.underline('"logs"') + chalk.cyan.bold(" | Status: ") + chalk.blue.bold("200"));
    });
   } else {
-   if (config.show_errors)console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Can't setupd database. Database state is ${this.connection.state}`));
+   if (config.show_errors) console.log(chalk.white.bold("> ") + chalk.blue.bold("[MYSQL]") + chalk.red.bold(`Can't setupd database. Database state is ${this.connection.state}`));
   }
  }
 }
