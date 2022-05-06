1f6c8ecc33c34b37f3b1e66bd1a07bc4e20439c8
igorkowalczyk <xxmajonezxx@gmail.com>
2022-05-06 11:56:31 +0200

    Fix arc.io

--- dashboard/index.js
+++ dashboard/index.js
@@ -289,6 +289,7 @@ module.exports = (app, client, port, config, secure_connection, domain, express)
 
  // Arc.io file
  app.get("/arc-sw.js", (req, res) => {
+  res.type("text/javascript");
   res.sendFile(path.join(__dirname + "/static/js/arc.js"));
  });
 
