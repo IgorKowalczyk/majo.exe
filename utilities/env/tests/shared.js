0d189a3576f81cffdd800886aff6235a6e9f479c
igorkowalczyk <xxmajonezxx@gmail.com>
2022-05-15 09:55:41 +0200

    Remove PREFIX from code

--- utilities/env/tests/shared.js
+++ utilities/env/tests/shared.js
@@ -8,7 +8,6 @@ module.exports = async () => {
  if (!process.env.MYSQL_USER && (process.argv.includes("--bot") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.MYSQL_USER!`));
  if (!process.env.MYSQL_PASSWORD && (process.argv.includes("--bot") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.MYSQL_PASSWORD!`));
  if (!process.env.AMEAPI && (process.argv.includes("--bot") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.AMEAPI!`));
- if (!process.env.PREFIX && (process.argv.includes("--bot") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.PREFIX!`));
  if (!process.env.DOMAIN && (process.argv.includes("--api") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.DOMAIN!`));
  if (!process.env.PORT && (process.argv.includes("--api") || process.argv.includes("--dashboard"))) console.log(chalk.blue.bold("[DEBUG]") + chalk.red.bold(` Missing process.env.PORT!`));
 };
