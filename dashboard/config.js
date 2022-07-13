27b174cb7def2476e1cb013433773ab2f2f23683
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-07-13 22:00:33 +0200

    Push

--- dashboard/config.js
+++ dashboard/config.js
@@ -2,8 +2,8 @@ export const meta = {
  title: "Majo.exe",
  author: "Igor Kowalczyk",
  description: "Majo.exe - Discord bot for Fun, Memes, Images, Giveaway, Economy, Anime and NSFW! Majo.exe serve over 117 commands!",
- //url: "https://beta.majoexe.xyz",
- url: "http://localhost:3000",
+ url: "https://beta.majoexe.xyz",
+ //url: "http://localhost:3000",
  theme_color: "#5865F2",
  type: "website",
 };
