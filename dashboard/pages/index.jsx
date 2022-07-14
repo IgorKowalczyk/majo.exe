106d0989c7e9e3a6751612bda451b9f506e28aad
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-07-14 10:21:22 +0200

    Remove

--- dashboard/pages/index.jsx
+++ dashboard/pages/index.jsx
@@ -1,7 +1,6 @@
 import { Container } from "@components/elements/blocks/Container";
 import { Nav } from "@components/elements/nav/Nav";
 import Link from "next/link";
-import { parseUser } from "@/utils/parse-user";
 
 export default function Main(props) {
  return (
@@ -14,29 +13,8 @@ export default function Main(props) {
      <Link href="/api/oauth">
       <a className="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">Login</a>
      </Link>
-     <div className="flex gap-2">
-      Logged as {props.user.username}#{props.user.discriminator} 
-     <Link href="/api/logout">
-      <a className=" text-blue-500 font-bold hover:text-blue-700">Logout</a>
-     </Link>
-     </div>
     </div>
    </Container>
   </>
  );
 }
-
-export async function getServerSideProps(ctx) {
- const user = await parseUser(ctx);
- const isDashboardPage = ["/guilds", "/dashboard"].includes(ctx.resolvedUrl);
- if (!user && isDashboardPage) {
-   return {
-     redirect: {
-       destination: "/api/oauth",
-       permanent: false,
-     },
-   };
- }
-
- return { props: { user } };
-};
