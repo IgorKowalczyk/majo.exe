2650c54a5f6688285bb8601334c9814adb32641b
IgorKowalczyk <xxmajonezxx@gmail.com>
2022-07-25 15:26:34 +0200

    Update

--- dashboard/pages/index.jsx
+++ dashboard/pages/index.jsx
@@ -1,5 +1,7 @@
 import { Container } from "@components/blocks/Container";
 import Login from "@components/buttons/Login";
+import { ads } from "@/config";
+import AdSense from 'react-adsense';
 
 export default function Main(props) {
  return (
@@ -10,6 +12,12 @@ export default function Main(props) {
     </h1>
     <h2 className="text-center font-poppins text-xl opacity-50">Don't log into the panel unless you know what you're doing</h2>
     <Login />
+    <ins className="adsbygoogle"
+     style={{ display: "block" }}
+     data-ad-client="ca-pub-6635777632235269"
+     data-ad-slot="6215869385"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
    </div>
   </Container>
  );
