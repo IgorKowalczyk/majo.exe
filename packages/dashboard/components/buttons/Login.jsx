ac5fa5f59038dc61958da06b54d03a29bbdd423c
Igor Kowalczyk <majonezexe@protonmail.com>
2022-11-26 14:56:11 +0100

    Fix status session

--- packages/dashboard/components/buttons/Login.jsx
+++ packages/dashboard/components/buttons/Login.jsx
@@ -11,7 +11,7 @@ export function Login() {
     <ArrowUturnRightIcon className="mr-2 h-5 w-5" aria-hidden="true" role="img" /> Go to dashboard
    </Link>
   );
- } else if(status === "unauthenticated") {
+ } else {
   return (
    <Link href="/auth/login" onClick={() => signIn("discord")} className="flex cursor-pointer items-center rounded bg-button-primary px-4 py-2 font-inter leading-6 text-white duration-200 hover:bg-button-primary-hover motion-reduce:transition-none">
     <>
