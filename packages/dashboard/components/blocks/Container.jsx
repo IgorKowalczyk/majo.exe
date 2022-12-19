903181bebd4294922f27ded3ed9e7baf9241616d
Igor Kowalczyk <majonezexe@protonmail.com>
2022-12-19 18:43:19 +0100

    Add hotjar analytics

--- packages/dashboard/components/blocks/Container.jsx
+++ packages/dashboard/components/blocks/Container.jsx
@@ -33,6 +33,22 @@ export function Container(props) {
     <meta name="msapplication-TileColor" content={meta.theme_color} />
     {meta.twitter && <meta property="article:published_time" content={meta.twitter} />}
     {meta.date && <meta property="article:published_time" content={meta.date} />}
+    {process.env.HOTJAR_ID && (
+     <script
+      dangerouslySetInnerHTML={{
+       __html: `
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+`,
+      }}
+     />
+    )}
    </Head>
    <Nav />
    <Twemoji options={{ className: "twemoji" }}>
