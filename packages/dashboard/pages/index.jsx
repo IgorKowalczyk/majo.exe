6a3f97f1454a3436cd076b697a4f60cc97e05ba6
Igor Kowalczyk <majonezexe@protonmail.com>
2022-12-27 19:44:48 +0100

    Add display queries

--- packages/dashboard/pages/index.jsx
+++ packages/dashboard/pages/index.jsx
@@ -11,7 +11,7 @@ export default function Main() {
      <p className="bg-gradient-to-r from-[#007CF0] to-[#00DFD8] box-decoration-clone bg-clip-text text-fill-transparent">you will ever need.</p>
     </h1>
     <h1></h1>
-    <h2 className="max-w-[600px] text-center font-inter text-xl text-[#939DB8]">Majo.exe will not only keep your server entertained but also assist you with moderation and many other things!</h2>
+    <h2 className="max-w-[600px] text-center font-inter text-xl text-[#939DB8] px-4">Majo.exe will not only keep your server entertained but also assist you with moderation and many other things!</h2>
     <div className="mt-2 flex gap-4">
      <div className="shadow-xl shadow-button-primary/10 duration-300 hover:shadow-none">
       <Login />
