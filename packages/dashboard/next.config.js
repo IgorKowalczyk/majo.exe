fa778bf84a0a03291d5b27589e37a4e0d6097cd1
Igor Kowalczyk <majonezexe@protonmail.com>
2023-02-07 15:46:01 +0100

    Remove preact update browserslist

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -66,12 +66,6 @@ const nextConfig = {
  },
  webpack: (config, { isServer, dev }) => {
   if (!dev && !isServer) {
-   /*Object.assign(config.resolve.alias, {
-    "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-    react: "preact/compat",
-    "react-dom/test-utils": "preact/test-utils",
-    "react-dom": "preact/compat",
-   });*/
    config.plugins.push(
     new CompressionPlugin(),
     new LodashModuleReplacementPlugin(),
