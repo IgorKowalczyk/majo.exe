4b9f8def209c9cc77958461d9117cf56515a9bee
Igor Kowalczyk <majonezexe@protonmail.com>
2023-04-23 00:29:34 +0200

    Use clsx

--- apps/dashboard/components/nav/client/MenuDropdown.jsx
+++ apps/dashboard/components/nav/client/MenuDropdown.jsx
@@ -2,6 +2,7 @@
 
 import { Menu, Transition } from "@headlessui/react";
 import { RectangleStackIcon, Cog8ToothIcon, UserIcon, ArrowRightOnRectangleIcon, QuestionMarkCircleIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
@@ -14,10 +15,25 @@ export function MenuDropdown({ user }) {
     <div>
      <Menu.Button className="ml-4 flex cursor-pointer items-center gap-3 duration-200 motion-reduce:transition-none">
       {({ open }) => (
-       <div className={`${open ? "opacity-80" : "hover:opacity-80"} flex h-10 select-none items-center rounded bg-button-secondary px-4 py-2 duration-200 hover:bg-button-secondary-hover motion-reduce:transition-none`}>
+       <div
+        className={clsx(
+         {
+          "opacity-80 bg-button-secondary-hover": open,
+          "hover:opacity-80 hover:bg-button-secondary-hover": !open,
+         },
+         "flex h-10 select-none items-center rounded bg-button-secondary px-4 py-2 duration-200  motion-reduce:transition-none"
+        )}
+       >
         <Image width="32" height="32" quality={100} className="!h-8 !w-8 rounded-full" src={user.image} loading="lazy" alt={`${user.name} Avatar`} />
         <span className="!ml-2 ">{user.name}</span>
-        <ChevronDownIcon className={`${open ? "rotate-180" : ""} ml-2 h-4 w-4 duration-200 motion-reduce:transition-none`} />
+        <ChevronDownIcon
+         className={clsx(
+          {
+           "rotate-180": open,
+          },
+          "ml-2 h-4 w-4 duration-200 motion-reduce:transition-none"
+         )}
+        />
        </div>
       )}
      </Menu.Button>
@@ -27,21 +43,48 @@ export function MenuDropdown({ user }) {
       <div className="px-1 py-1 ">
        <Menu.Item>
         {({ active }) => (
-         <Link href="/dashboard" className={`${active ? "bg-button-primary text-white" : "text-gray-400"} group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none`}>
+         <Link
+          href="/dashboard"
+          className={clsx(
+           {
+            "bg-button-primary text-white": active,
+            "text-gray-400": !active,
+           },
+           "group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none"
+          )}
+         >
           <RectangleStackIcon className="mr-2 h-5 w-5 " aria-hidden="true" role="img" /> Dashboard
          </Link>
         )}
        </Menu.Item>
        <Menu.Item>
         {({ active }) => (
-         <Link href="/user/profile" className={`${active ? "bg-button-primary text-white" : "text-gray-400"} group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none`}>
+         <Link
+          href="/user/profile"
+          className={clsx(
+           {
+            "bg-button-primary text-white": active,
+            "text-gray-400": !active,
+           },
+           "group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none"
+          )}
+         >
           <UserIcon className="mr-2 h-5 w-5 " aria-hidden="true" role="img" /> Profile
          </Link>
         )}
        </Menu.Item>
        <Menu.Item>
         {({ active }) => (
-         <Link href="/user/settings" className={`${active ? "bg-button-primary text-white" : "text-gray-400"} group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none`}>
+         <Link
+          href="/user/settings"
+          className={clsx(
+           {
+            "bg-button-primary text-white": active,
+            "text-gray-400": !active,
+           },
+           "group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none"
+          )}
+         >
           <Cog8ToothIcon className="mr-2 h-5 w-5 " aria-hidden="true" role="img" /> Settings
          </Link>
         )}
@@ -50,14 +93,32 @@ export function MenuDropdown({ user }) {
       <div className="px-1 py-1">
        <Menu.Item>
         {({ active }) => (
-         <Link href="/discord" className={`${active ? "bg-button-primary text-white" : "text-gray-400"} group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none`}>
+         <Link
+          href="/discord"
+          className={clsx(
+           {
+            "bg-button-primary text-white": active,
+            "text-gray-400": !active,
+           },
+           "group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none"
+          )}
+         >
           <QuestionMarkCircleIcon className="mr-2 h-5 w-5 " aria-hidden="true" role="img" /> Support
          </Link>
         )}
        </Menu.Item>
        <Menu.Item>
         {({ active }) => (
-         <button onClick={() => signOut()} className={`${active ? "bg-button-action-primary text-white" : "text-gray-400"} group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none`}>
+         <button
+          onClick={() => signOut()}
+          className={clsx(
+           {
+            "bg-button-action-primary text-white": active,
+            "text-gray-400": !active,
+           },
+           "group my-1 flex w-full items-center rounded-md px-2 py-2 text-sm duration-200 motion-reduce:transition-none"
+          )}
+         >
           <ArrowRightOnRectangleIcon className="mr-2 h-5 w-5" aria-hidden="true" role="img" /> Logout
          </button>
         )}
