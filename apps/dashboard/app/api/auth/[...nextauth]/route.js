13bdb7061d7dec2cd151c27376c2e1860a1afeea
Igor Kowalczyk <majonezexe@protonmail.com>
2023-05-18 20:31:25 +0200

    Change route handler

---
+++ apps/dashboard/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,5 @@
+import authOptions from "lib/authOptions";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- apps/dashboard/pages/api/auth/[...nextauth].js
+++
@@ -1,4 +0,0 @@
-import authOptions from "lib/authOptions";
-import NextAuth from "next-auth";
-
-export default NextAuth(authOptions);
