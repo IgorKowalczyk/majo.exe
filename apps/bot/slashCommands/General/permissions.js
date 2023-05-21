b436fcbf2d8403000553fd6714f9028889cc2280
Igor Kowalczyk <majonezexe@protonmail.com>
2023-05-21 15:54:33 +0200

    Add permissions command

---
+++ apps/bot/slashCommands/General/permissions.js
@@ -0,0 +1,41 @@
+import { ApplicationCommandType, EmbedBuilder, codeBlock} from "discord.js";
+
+function convertCamelCaseToWords(text) {
+ return text.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+  return str.toUpperCase().trim();
+ });
+}
+
+export default {
+ name: "permissions",
+ description: "🎛️ Check Majo.exe's permissions in your server",
+ type: ApplicationCommandType.ChatInput,
+ cooldown: 5000,
+ dmPermission: false,
+ usage: "/permissions",
+ run: async (client, interaction) => {
+  try {
+   const clientMember = interaction.guild?.members.cache.get(client.user?.id);
+   const permissions = clientMember?.permissions.toArray();
+   const words =  permissions.map(permission => convertCamelCaseToWords(permission));
+
+   const embed = new EmbedBuilder()
+    .setColor("#5865F2")
+    .setTimestamp()
+    .setTitle(`🎛️ Permissions in ${interaction.guild?.name}`)
+    .setDescription(`> **${client.user?.username}** has the following permissions in this server:\n${codeBlock(words?.join("\n"))}`)
+    .setTimestamp()
+    .setFooter({
+     text: `Requested by ${interaction.member?.user?.username}`,
+     iconURL: interaction.member?.user?.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     }),
+    });
+   return interaction.reply({ ephemeral: false, embeds: [embed] });
+  } catch (err) {
+   client.errorMessages.generateErrorMessage(interaction, err);
+  }
+ },
+};
