b511b8d71395ec800cedd3e6e6fba54f35efba10
Igor Kowalczyk <majonezexe@protonmail.com>
2023-06-07 08:42:20 +0200

    Add experimental element

--- apps/dashboard/app/page.jsx
+++ apps/dashboard/app/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default async function Main() {
  return (
   <div className="h-screen w-full absolute before:grayscale after:absolute after:inset-0 after:h-full after:w-full after:-z-10 after:top-20 after:opacity-30 custom-bg z-20 before:md:bg-[url('/assets/svg/grid.svg')] before:opacity-50 before:w-full before:h-full before:absolute before:z-10">
-   <div className="z-20 relative flex h-screen flex-col items-center justify-center gap-4 bg-[radial-gradient(circle,rgba(2,0,36,0)0,rgb(16,17,16,100%))]">
+   <div className="z-20 py-32 relative flex h-screen flex-col items-center justify-center gap-4 bg-[radial-gradient(circle,rgba(2,0,36,0)0,rgb(16,17,16,100%))]">
     <Link href={"/invite"} className="text-center -mt-4 font-medium bg-background-menu-button/50 backdrop-blur flex flex-row items-center border group transition-all duration-200 hover:bg-background-menu-button/40 cursor-copy border-neutral-700 rounded-full px-6 py-1">
      Introducing Majo.exe v6
      <ArrowRightIcon className="inline-block w-4 h-4 ml-2 group-hover:translate-x-1 transition-all duration-200" />
@@ -21,6 +21,9 @@ export default async function Main() {
      <Login />
      <Invite />
     </div>
+    {/*
+    <div className="w-1/2 mt-24 bg-background-menu-button/80 h-screen border border-neutral-800 rounded-3xl"></div>
+ */}
    </div>
   </div>
  );
