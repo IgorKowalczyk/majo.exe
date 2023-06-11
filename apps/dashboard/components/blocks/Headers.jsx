9eebae41c9e78e8d7485a01977c4bb9a2bd56283
Igor Kowalczyk <majonezexe@protonmail.com>
2023-06-11 12:06:43 +0200

    Fix header

--- apps/dashboard/components/blocks/Headers.jsx
+++ apps/dashboard/components/blocks/Headers.jsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 
 export function HeaderBig({ children, className, ...props }) {
  return (
-  <h1 className={clsx("text-center text-6xl pt-4 font-extrabold text-white", className)} {...props}>
+  <h1 className={clsx("text-center text-4xl md:text-6xl pt-4 font-extrabold text-white", className)} {...props}>
    {children}
   </h1>
  );
