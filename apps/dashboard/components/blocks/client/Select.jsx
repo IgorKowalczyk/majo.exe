c05028a0e21f698a06e87af8b03478c3ad2ff9c5
Igor Kowalczyk <majonezexe@protonmail.com>
2023-06-20 10:12:35 +0200

    Update select menu

--- apps/dashboard/components/blocks/client/Select.jsx
+++ apps/dashboard/components/blocks/client/Select.jsx
@@ -8,7 +8,7 @@ export function Select({ options, selected, setSelected }) {
   <Listbox value={selected} onChange={setSelected}>
    <div className="relative mt-1">
     <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-     <span className="block truncate">{selected.name}</span>
+     <span className="block truncate">{selected}</span>
      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
       <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
      </span>
