16172996bdbcaae1bd15ab40171630d3c826b698
Igor Kowalczyk <majonezexe@protonmail.com>
2023-07-04 17:08:55 +0200

    Fix build error

--- apps/dashboard/app/api/invite/[invite]/route.js
+++ apps/dashboard/app/api/invite/[invite]/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { config } from "@majoexe/config";
+
 export const runtime = "edge";
 
 export function GET(request, { params }) {
  const invite = params.invite;
  if (!invite) return NextResponse.redirect("/dashboard");
- return NextResponse.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=${config.global.permissions}&scope=${config.global.scopes}%20identifys&guild_id=${invite}&redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL + "/dashboard")}&response_type=code`);
+ return NextResponse.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=1916267615&scope=guilds%20identify%20bot%20applications.commands&guild_id=${invite}&redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL + "/dashboard")}&response_type=code`);
 }
