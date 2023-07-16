c657ebadfc06a39c204459fc1dd469e1b0b56251
Igor Kowalczyk <majonezexe@protonmail.com>
2023-07-16 20:16:34 +0200

    Add guildMember Add/Remove events

---
+++ apps/bot/events/guild/guildMemberAdd.js
@@ -0,0 +1,34 @@
+import prismaClient from "@majoexe/database";
+
+export async function guildMemberAdd(client, member) {
+ if (!member || !member.user || !member.guild) return;
+ const joins = await prismaClient.guildJoin.findFirst({
+  where: {
+   guildId: member.guild.id,
+   date: {
+    gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+   },
+  },
+ });
+
+ if (!joins) {
+  await prismaClient.guildJoin.create({
+   data: {
+    guildId: member.guild.id,
+    date: new Date(),
+    joins: 1,
+   },
+  });
+ } else {
+  await prismaClient.guildJoin.update({
+   where: {
+    id: joins.id,
+   },
+   data: {
+    joins: {
+     increment: 1,
+    }
+   },
+  });
+ }
+}
---
+++ apps/bot/events/guild/guildMemberRemove.js
@@ -0,0 +1,34 @@
+import prismaClient from "@majoexe/database";
+
+export async function guildMemberRemove(client, member) {
+ if (!member || !member.user || !member.guild) return;
+ const leaves = await prismaClient.GuildLeave.findFirst({
+  where: {
+   guildId: member.guild.id,
+   date: {
+    gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+   },
+  },
+ });
+
+ if (!leaves) {
+  await prismaClient.GuildLeave.create({
+   data: {
+    guildId: member.guild.id,
+    date: new Date(),
+    leaves: 1,
+   },
+  });
+ } else {
+  await prismaClient.GuildLeave.update({
+   where: {
+    id: leaves.id,
+   },
+   data: {
+    leaves: {
+     increment: 1,
+    }
+   },
+  });
+ }
+}
