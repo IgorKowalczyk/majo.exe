ec538e5de7598b4005f3db6c3045da4796e67784
Igor Kowalczyk <majonezexe@protonmail.com>
2023-07-27 13:26:54 +0200

    Update charts

--- apps/dashboard/app/dashboard/[server]/statistics/page.jsx
+++ apps/dashboard/app/dashboard/[server]/statistics/page.jsx
@@ -43,48 +43,59 @@ export default async function Statistics({ params }) {
    },
   },
  });
+
  const guildJoinMap = new Map();
  const guildLeaveMap = new Map();
 
- for (const guildJoinData of guild.GuildJoin) {
-  const date = new Date(guildJoinData.date);
-  const dateFormatted = date.toISOString().split("T")[0];
-  guildJoinMap.set(dateFormatted, guildJoinData.joins);
+ for (const guildJoinData of guild.GuildJoin || []) {
+  const date = guildJoinData.date;
+  guildJoinMap.set(date, guildJoinData.joins);
  }
 
- for (const guildLeaveData of guild.GuildLeave) {
-  const date = new Date(guildLeaveData.date);
-  const dateFormatted = date.toISOString().split("T")[0];
-  guildLeaveMap.set(dateFormatted, guildLeaveData.leaves);
+ for (const guildLeaveData of guild.GuildLeave || []) {
+  const date = guildLeaveData.date;
+  guildLeaveMap.set(date, guildLeaveData.leaves);
  }
 
  const currentDate = new Date();
  const pastDays = 30;
 
- const guildJoin = [];
- const guildLeave = [];
+ let guildJoin = [];
+ let guildLeave = [];
 
  for (let i = 0; i < pastDays; i++) {
-  const date = new Date(currentDate);
-  date.setDate(currentDate.getDate() - i);
-  const dateFormatted = date.toISOString().split("T")[0];
-
-  const joins = guildJoinMap.get(dateFormatted) || 0;
-  const leaves = guildLeaveMap.get(dateFormatted) || 0;
+  const date = currentDate.getDate() - i;
+  const joins = guildJoinMap.get(date) || 0;
+  const leaves = guildLeaveMap.get(date) || 0;
 
   guildJoin.push({
-   date: dateFormatted,
+   date,
    Joins: joins,
   });
 
   guildLeave.push({
-   date: dateFormatted,
+   date,
    Leaves: leaves,
   });
  }
 
- guildJoin.reverse();
- guildLeave.reverse();
+ if (guildJoinMap.size === 0) {
+  guildJoin = [
+   {
+    date: currentDate.getDate(),
+    Joins: 0,
+   },
+  ];
+ }
+
+ if (guildLeaveMap.size === 0) {
+  guildLeave = [
+   {
+    date: currentDate.getDate(),
+    Leaves: 0,
+   },
+  ];
+ }
 
  return (
   <>
