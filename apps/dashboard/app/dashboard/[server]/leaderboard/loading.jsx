5a105d018a45ee5caba7402fe5a397dd6ae14981
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-06 13:42:54 +0200

    Add new loading states

---
+++ apps/dashboard/app/dashboard/[server]/leaderboard/loading.jsx
@@ -0,0 +1,21 @@
+import { Block } from "@/components/blocks/Block";
+import { Header1 } from "@/components/blocks/Headers";
+import { EmbedSkeleton } from "@/components/blocks/Skeletons";
+
+export default function Loading() {
+ return (
+  <>
+   <Header1>
+    <svg className="h-12 w-12 fill-white" viewBox="-2 -4 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin">
+     <path d="M2.776 5.106L3.648 11h12.736l.867-5.98-3.493 3.02-3.755-4.827-3.909 4.811-3.318-2.918zm10.038-1.537l-.078.067.141.014 1.167 1.499 1.437-1.242.14.014-.062-.082 2.413-2.086a1 1 0 0 1 1.643.9L18.115 13H1.922L.399 2.7a1 1 0 0 1 1.65-.898L4.35 3.827l-.05.06.109-.008 1.444 1.27 1.212-1.493.109-.009-.06-.052L9.245.976a1 1 0 0 1 1.565.017l2.005 2.576zM2 14h16v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z" />
+    </svg>
+    Leaderboard
+   </Header1>
+   <div className="mx-auto flex w-full max-w-2xl items-center justify-center overflow-auto">
+    <Block className="mt-4 w-full">
+     <EmbedSkeleton />
+    </Block>
+   </div>
+  </>
+ );
+}
---
+++ apps/dashboard/app/dashboard/[server]/loading.jsx
@@ -0,0 +1,59 @@
+import { Block } from "@/components/blocks/Block";
+import { Header1, Header4, Header5 } from "@/components/blocks/Headers";
+import { EmbedSkeleton, TextSkeleton } from "@/components/blocks/Skeletons";
+
+export default function Loading() {
+ return (
+  <>
+   <Header1 className={"mb-4 !justify-normal"}>
+    <div className="h-16 w-16 rounded-full bg-button-secondary" />
+    <div className="ml-4 flex flex-col !justify-start text-left">
+     <TextSkeleton className="w-32" />
+     <Header5 className="mt-2 opacity-60">
+      <TextSkeleton className="w-32" />
+     </Header5>
+    </div>
+   </Header1>
+
+   <Block className="!mt-4 flex w-full flex-row">
+    <div className="flex flex-row items-center gap-4">
+     <div className="flex items-center">
+      <div className="mr-2 h-3 w-3 rounded-full bg-[#81848f]" />0 members
+     </div>
+     <div className="flex items-center">
+      <div className="mr-2 h-3 w-3 rounded-full bg-[#22a55b]" />0 online
+     </div>
+    </div>
+    <span className="ml-auto">Powered by Majo.exe</span>
+   </Block>
+
+   <div className="mt-6 block gap-6 lg:flex lg:items-start">
+    <Block className="flex flex-col justify-start [flex:3_1_0] ">
+     <Header4 className="mb-4 !items-start !justify-normal opacity-80">Leaderboard</Header4>
+     <EmbedSkeleton className="h-64 w-full" />
+    </Block>
+    <div className="mt-6 flex flex-col justify-start gap-6 [flex:2_1_0%] lg:mt-0">
+     <Block>
+      <Header4 className="!items-start !justify-normal opacity-80">
+       Language
+       <span className="ml-auto font-medium">Loading...</span>
+      </Header4>
+     </Block>
+     <Block>
+      <Header4 className="mb-4 !items-start !justify-normal opacity-80">
+       Emojis
+       <span className="ml-auto font-medium opacity-60">Loading...</span>
+      </Header4>
+     </Block>
+
+     <Block>
+      <Header4 className="mb-4 !items-start !justify-normal opacity-80">
+       Stickers
+       <span className="ml-auto font-medium opacity-60">Loading...</span>
+      </Header4>
+     </Block>
+    </div>
+   </div>
+  </>
+ );
+}
