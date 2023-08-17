9cb7c967b7052be87bf44edbe67614ba2a3625ad
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-17 14:34:54 +0200

    Format code

--- apps/bot/events/guild/messageCreate.js
+++ apps/bot/events/guild/messageCreate.js
@@ -1,86 +1,8 @@
-import { createXPCard } from "../../util/images/xpCard.js";
 import prismaClient from "@majoexe/database";
-//import { fetchProfanity } from "@majoexe/util/database";
-//import Filter from "bad-words";
 import { EmbedBuilder, AttachmentBuilder } from "discord.js";
-// import { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { createXPCard } from "../../util/images/xpCard.js";
 
 const XPTimeout = new Map();
-
-/*
-const filter = new Filter();
-
-const takeAction = async (client, message) => {
- await message.delete();
-
- const query = await prismaClient.guildLogs.create({
-  data: {
-   guild: {
-    connectOrCreate: {
-     where: {
-      guildId: message.guild.id,
-     },
-     create: {
-      guildId: message.guild.id,
-     },
-    },
-   },
-   user: {
-    connectOrCreate: {
-     where: {
-      discordId: message.author.id,
-     },
-     create: {
-      discordId: message.author.id,
-      name: message.author.username,
-      global_name: message.author.global_name,
-      avatar: message.author.avatar || null,
-      discriminator: message.author.discriminator,
-     },
-    },
-   },
-   content: "Detected and deleted a message because it contained a bad word.",
-   type: "profanity",
-  },
- });
-
- const embed = new EmbedBuilder()
-  .setTitle("‼️ Bad word detected")
-  .setDescription(
-   `
-  ${message.author} message has been deleted for using a bad word.
-
-  **Message ID**: \`${message.id}\`
-  **User ID**: \`${message.author.id}\`
-  **Infraction ID**: \`${query.id}\`
-  `
-  )
-  .setColor("#EF4444")
-  .setTimestamp()
-  .setThumbnail(
-   message.author.displayAvatarURL({
-    dynamic: true,
-    format: "png",
-    size: 2048,
-   })
-  )
-  .setFooter({
-   iconURL: client.user.displayAvatarURL({
-    dynamic: true,
-    format: "png",
-    size: 2048,
-   }),
-   text: "The filter was applied because message scanning was enabled on the server!",
-  });
-
- if (client.config.dashboard.enabled && client.config.dashboard.link) {
-  const row = new ActionRowBuilder().addComponents(new ButtonBuilder().setLabel("View server logs").setStyle(ButtonStyle.Link).setURL(`${client.config.dashboard.link}/dashboard/${message.guild.id}/logs`));
-  return await message.channel.send({ embeds: [embed], components: [row] });
- }
-
- await message.channel.send({ embeds: [embed] });
-};
-*/
 
 export async function messageCreate(client, message) {
  if (message.author.bot) return;
@@ -176,12 +98,12 @@ export async function messageCreate(client, message) {
    name: "rank.png",
   });
 
-  const embed = new EmbedBuilder()
+  const embed = new EmbedBuilder() // prettier
    .setTitle("🎉 Level up!")
    .setDescription(`Congratulations ${message.author}! You have leveled up to level **${nextLevel}**!`)
    .setColor("#10B981")
    .setTimestamp()
-   .setImage("attachment://rank.png")
+   .setImage("attachment://rank.png");
 
   await message.channel.send({ embeds: [embed], files: [attachment] });
  }
