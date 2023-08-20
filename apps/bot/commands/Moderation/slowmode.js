e75bcf99876b9554f8e9835cbd1f871d33e6e4b5
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-20 13:57:21 +0200

    Add new command

---
+++ apps/bot/commands/Moderation/slowmode.js
@@ -0,0 +1,63 @@
+import { ApplicationCommandType, ApplicationCommandOptionType, PermissionsBitField, EmbedBuilder } from "discord.js";
+import ms from "ms";
+
+export default {
+ name: "slowmode",
+ description: "🕐 Set the slowmode for a channel",
+ type: ApplicationCommandType.ChatInput,
+ cooldown: 5000,
+ dm_permission: false,
+ usage: "/slowmode <time>",
+ options: [
+  {
+   name: "time",
+   description: "The time to set the slowmode to",
+   type: ApplicationCommandOptionType.String,
+   required: true,
+  },
+ ],
+ run: async (client, interaction, guildSettings) => {
+  try {
+   const time = interaction.options.getString("time");
+   const channel = interaction.channel;
+   const timeInMs = ms(time);
+
+   if (!timeInMs) {
+    return client.errorMessages.createSlashError(interaction, "❌ Invalid time provided");
+   }
+
+   if (!channel.permissionsFor(interaction.guild.members.me).has(PermissionsBitField.ManageChannels)) {
+    return client.errorMessages.createSlashError(interaction, "❌ I don't have permission to manage channels");
+   }
+
+   if (!channel.permissionsFor(interaction.member).has(PermissionsBitField.ManageChannels)) {
+    return client.errorMessages.createSlashError(interaction, "❌ You don't have permission to manage channels");
+   }
+
+   if (channel.rateLimitPerUser === timeInMs / 1000) {
+    return client.errorMessages.createSlashError(interaction, "❌ Slowmode is already set to that time");
+   }
+
+   const before = channel.rateLimitPerUser;
+   await channel.setRateLimitPerUser(timeInMs / 1000);
+
+   const embed = new EmbedBuilder()
+    .setColor(guildSettings?.embedColor || client.config.defaultColor)
+    .setTimestamp()
+    .setTitle("🕐 Slowmode")
+    .setDescription(`Slowmode has been changed from \`${ms(before * 1000)}\` to \`${time}\``)
+    .setFooter({
+     text: `Requested by ${interaction.member.user.username}`,
+     iconURL: interaction.member.user.displayAvatarURL({
+      dynamic: true,
+      format: "png",
+      size: 2048,
+     }),
+    });
+
+   return interaction.followUp({ ephemeral: false, embeds: [embed] });
+  } catch (err) {
+   client.errorMessages.internalError(interaction, err);
+  }
+ },
+};
