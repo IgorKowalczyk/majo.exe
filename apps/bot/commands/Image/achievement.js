c253916034e83efd7af22274a4c2761dbd464f46
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-22 16:09:28 +0200

    Add achievement command

---
+++ apps/bot/commands/Image/achievement.js
@@ -0,0 +1,56 @@
+import { loadImage, createCanvas } from "@napi-rs/canvas";
+import { ApplicationCommandType, ApplicationCommandOptionType, AttachmentBuilder, EmbedBuilder } from "discord.js";
+
+export default {
+ name: "achievement",
+ description: "🏆 Generate a achievement image",
+ type: ApplicationCommandType.ChatInput,
+ cooldown: 5000,
+ dm_permission: false,
+ usage: "/achievement <text>",
+ options: [
+  {
+   name: "text",
+   description: "Text to put in achievement",
+   type: ApplicationCommandOptionType.String,
+   required: false,
+   max_length: 50,
+  },
+ ],
+ run: async (client, interaction, guildSettings) => {
+  try {
+   const text = interaction.options.getString("text") || "How Did We Get Here?";
+   const background = await loadImage("./util/images/files/achievement.png");
+
+   const canvas = createCanvas(background.width, background.height);
+   const context = canvas.getContext("2d");
+   context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+   context.translate(120, 60);
+   context.font = "24px Arial";
+   context.fillStyle = "#c2c2c2";
+   context.fillText(text, 10, 22, 330);
+
+   const file = new AttachmentBuilder(canvas.toBuffer("image/png"), {
+    name: "achievement.png",
+   });
+
+   const embed = new EmbedBuilder()
+    .setTitle("🏆 Achievement")
+    .setImage("attachment://achievement.png")
+    .setColor(guildSettings?.embedColor || client.config.defaultColor)
+    .setTimestamp()
+    .setFooter({
+     text: `Requested by ${interaction.member.user.username}`,
+     iconURL: interaction.member.user.displayAvatarURL({
+      size: 256,
+     }),
+    });
+
+   return interaction.followUp({ embeds: [embed], files: [file] });
+  } catch (err) {
+   console.log(err);
+   client.errorMessages.internalError(interaction, err);
+  }
+ },
+};
