982f01852b548ad2a446dcea5f9ff48c06162e1d
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-28 23:29:46 +0200

    Format code

--- packages/database/src/redis.js
+++ packages/database/src/redis.js
@@ -5,10 +5,10 @@ import { Logger } from "./logger.js";
 let cache;
 
 if (process.env.REDIS_URL) {
-  Logger("info", "Redis URL found, setting up Global Redis cache...");
+ Logger("info", "Redis URL found, setting up Global Redis cache...");
  cache = new Redis(process.env.REDIS_URL);
 } else {
-  Logger("warn", "No Redis URL found, setting up Memory cache...");
+ Logger("warn", "No Redis URL found, setting up Memory cache...");
  cache = new Map();
 }
 
