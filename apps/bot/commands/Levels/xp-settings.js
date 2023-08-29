219132934e3f5f57a473aa916d842060dffe3e7e
Igor Kowalczyk <majonezexe@protonmail.com>
2023-08-29 20:36:57 +0200

    Add missing permissions check

--- apps/bot/commands/Levels/xp-settings.js
+++ apps/bot/commands/Levels/xp-settings.js
@@ -61,103 +61,113 @@ export default {
   },
  ],
  run: async (client, interaction, guildSettings) => {
-  const subcommand = interaction.options.getSubcommand();
-
-  if (subcommand === "change") {
-   const action = interaction.options.getString("action");
-   const type = interaction.options.getString("type");
-
-   if (type === "leveling") {
-    if (action === "enable") {
-     const settings = await fetchXPSettings(interaction.guild.id);
-
-     if (!settings || !settings.enableXP) {
-      return client.errorMessages.createSlashError(interaction, "❌ XP is disabled in this server, you can't enable leveling messages if XP is disabled");
-     }
-
-     await setXPLevelUpMessageSettings(interaction.guild.id, true);
-
-     const embed = new EmbedBuilder()
-      .setColor(guildSettings.embedColor || client.config.defaultColor)
-      .setTimestamp()
-      .setTitle("✅ Success")
-      .setDescription("> Successfully **`enabled`** leveling messages, now bot **will send** a message when a user levels up")
-      .setFooter({
-       text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
-       iconURL: interaction.member.user.displayAvatarURL({
-        size: 256,
-       }),
-      });
-     return interaction.followUp({ ephemeral: true, embeds: [embed] });
-    } else if (action === "disable") {
-     const settings = await fetchXPSettings(interaction.guild.id);
-
-     if (!settings || !settings.enableXP) {
-      return client.errorMessages.createSlashError(interaction, "❌ XP is disabled in this server, you can't disable leveling messages if XP is disabled");
-     }
-     await setXPLevelUpMessageSettings(interaction.guild.id, false);
-
-     const embed = new EmbedBuilder()
-      .setColor(guildSettings.embedColor || client.config.defaultColor)
-      .setTimestamp()
-      .setTitle("✅ Success")
-      .setDescription("> Successfully **`disabled`** leveling messages, now bot **will not send** a message when a user levels up")
-      .setFooter({
-       text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
-       iconURL: interaction.member.user.displayAvatarURL({
-        size: 256,
-       }),
-      });
-     return interaction.followUp({ ephemeral: true, embeds: [embed] });
-    }
-   } else if (type === "gaining") {
-    if (action === "enable") {
-     await setXPSettings(interaction.guild.id, true);
-
-     const embed = new EmbedBuilder()
-      .setColor(guildSettings.embedColor || client.config.defaultColor)
-      .setTimestamp()
-      .setTitle("✅ Success")
-      .setDescription("> Successfully **`enabled`** gaining XP, now users **will gain XP** when they send a message")
-      .setFooter({
-       text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
-       iconURL: interaction.member.user.displayAvatarURL({
-        size: 256,
-       }),
-      });
-     return interaction.followUp({ ephemeral: true, embeds: [embed] });
-    } else if (action === "disable") {
-     await setXPSettings(interaction.guild.id, false);
-
-     const embed = new EmbedBuilder()
-      .setColor(guildSettings.embedColor || client.config.defaultColor)
-      .setTimestamp()
-      .setTitle("✅ Success")
-      .setDescription("> Successfully **`disabled`** gaining XP, now users **will not gain XP** when they send a message")
-      .setFooter({
-       text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
-       iconURL: interaction.member.user.displayAvatarURL({
-        size: 256,
-       }),
-      });
-     return interaction.followUp({ ephemeral: true, embeds: [embed] });
-    }
+  try {
+   if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+    return client.errorMessages.createSlashError(interaction, "❌ You don't have permission to enable leveling messages, you need `Administrator` permission");
    }
-  } else if (subcommand === "view") {
-   const settings = await fetchXPSettings(interaction.guild.id);
 
-   const embed = new EmbedBuilder()
-    .setColor(guildSettings.embedColor || client.config.defaultColor)
-    .setTimestamp()
-    .setTitle("📈 XP Settings")
-    .setDescription(`> ${settings.enableXPLevelUpMessage ? client.config.emojis.success : client.config.emojis.error} **Leveling messages:** \`${settings.enableXPLevelUpMessage ? "Enabled" : "Disabled"}\` (Last updated: ${settings.enableXPLevelUpMessageLastChanged ? time(settings.enableXPLevelUpMessageLastChanged, "R") : "Never"})\n> ${settings.enableXP ? client.config.emojis.success : client.config.emojis.error} **Gaining XP:** \`${settings.enableXP ? "Enabled" : "Disabled"}\` (Last updated: ${settings.enableXPLastChanged ? time(settings.enableXPLastChanged, "R") : "Never"})`)
-    .setFooter({
-     text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
-     iconURL: interaction.member.user.displayAvatarURL({
-      size: 256,
-     }),
-    });
-   return interaction.followUp({ ephemeral: true, embeds: [embed] });
+   const subcommand = interaction.options.getSubcommand();
+
+   if (subcommand === "change") {
+    const action = interaction.options.getString("action");
+    const type = interaction.options.getString("type");
+
+    if (type === "leveling") {
+     if (action === "enable") {
+      const settings = await fetchXPSettings(interaction.guild.id);
+
+      if (!settings || !settings.enableXP) {
+       return client.errorMessages.createSlashError(interaction, "❌ XP is disabled in this server, you can't enable leveling messages if XP is disabled");
+      }
+
+      await setXPLevelUpMessageSettings(interaction.guild.id, true);
+
+      const embed = new EmbedBuilder()
+       .setColor(guildSettings.embedColor || client.config.defaultColor)
+       .setTimestamp()
+       .setTitle("✅ Success")
+       .setDescription("> Successfully **`enabled`** leveling messages, now bot **will send** a message when a user levels up")
+       .setFooter({
+        text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
+        iconURL: interaction.member.user.displayAvatarURL({
+         size: 256,
+        }),
+       });
+      return interaction.followUp({ ephemeral: true, embeds: [embed] });
+     } else if (action === "disable") {
+      const settings = await fetchXPSettings(interaction.guild.id);
+
+      if (!settings || !settings.enableXP) {
+       return client.errorMessages.createSlashError(interaction, "❌ XP is disabled in this server, you can't disable leveling messages if XP is disabled");
+      }
+      await setXPLevelUpMessageSettings(interaction.guild.id, false);
+
+      const embed = new EmbedBuilder()
+       .setColor(guildSettings.embedColor || client.config.defaultColor)
+       .setTimestamp()
+       .setTitle("✅ Success")
+       .setDescription("> Successfully **`disabled`** leveling messages, now bot **will not send** a message when a user levels up")
+       .setFooter({
+        text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
+        iconURL: interaction.member.user.displayAvatarURL({
+         size: 256,
+        }),
+       });
+      return interaction.followUp({ ephemeral: true, embeds: [embed] });
+     }
+    } else if (type === "gaining") {
+     if (action === "enable") {
+      await setXPSettings(interaction.guild.id, true);
+
+      const embed = new EmbedBuilder()
+       .setColor(guildSettings.embedColor || client.config.defaultColor)
+       .setTimestamp()
+       .setTitle("✅ Success")
+       .setDescription("> Successfully **`enabled`** gaining XP, now users **will gain XP** when they send a message")
+       .setFooter({
+        text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
+        iconURL: interaction.member.user.displayAvatarURL({
+         size: 256,
+        }),
+       });
+      return interaction.followUp({ ephemeral: true, embeds: [embed] });
+     } else if (action === "disable") {
+      await setXPSettings(interaction.guild.id, false);
+
+      const embed = new EmbedBuilder()
+       .setColor(guildSettings.embedColor || client.config.defaultColor)
+       .setTimestamp()
+       .setTitle("✅ Success")
+       .setDescription("> Successfully **`disabled`** gaining XP, now users **will not gain XP** when they send a message")
+       .setFooter({
+        text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
+        iconURL: interaction.member.user.displayAvatarURL({
+         size: 256,
+        }),
+       });
+      return interaction.followUp({ ephemeral: true, embeds: [embed] });
+     }
+    }
+   } else if (subcommand === "view") {
+    const settings = await fetchXPSettings(interaction.guild.id);
+
+    const embed = new EmbedBuilder()
+     .setColor(guildSettings.embedColor || client.config.defaultColor)
+     .setTimestamp()
+     .setTitle("📈 XP Settings")
+     .setDescription(`> ${settings.enableXPLevelUpMessage ? client.config.emojis.success : client.config.emojis.error} **Leveling messages:** \`${settings.enableXPLevelUpMessage ? "Enabled" : "Disabled"}\` (Last updated: ${settings.enableXPLevelUpMessageLastChanged ? time(settings.enableXPLevelUpMessageLastChanged, "R") : "Never"})\n> ${settings.enableXP ? client.config.emojis.success : client.config.emojis.error} **Gaining XP:** \`${settings.enableXP ? "Enabled" : "Disabled"}\` (Last updated: ${settings.enableXPLastChanged ? time(settings.enableXPLastChanged, "R") : "Never"})`)
+     .setFooter({
+      text: `Requested by ${interaction.member.user.globalName || interaction.member.user.username}`,
+      iconURL: interaction.member.user.displayAvatarURL({
+       size: 256,
+      }),
+     });
+    return interaction.followUp({ ephemeral: true, embeds: [embed] });
+   } else {
+    return client.errorMessages.createSlashError(interaction, "❌ Invalid subcommand, please use `/xp-settings <subcommand>`");
+   }
+  } catch (err) {
+   client.errorMessages.internalError(interaction, err);
   }
  },
 };
