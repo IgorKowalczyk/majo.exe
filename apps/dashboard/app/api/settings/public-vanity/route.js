9c0b45483cffc01ebbef695872235aa7972d96b1
Igor Kowalczyk <majonezexe@protonmail.com>
2023-09-02 23:42:36 +0200

    Add update vanity route

---
+++ apps/dashboard/app/api/settings/public-vanity/route.js
@@ -0,0 +1,228 @@
+import prismaClient from "@majoexe/database";
+import { getServer, getGuildMember } from "@majoexe/util/functions";
+import { getSession } from "lib/session";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+ try {
+  const session = await getSession();
+  const start = Date.now();
+
+  if (!session || !session.access_token) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Unauthorized",
+    }),
+    {
+     status: 401,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  const cloned = await request.clone();
+  const body = await cloned.json();
+  const { id, vanity } = body;
+
+  if (!id || typeof id !== "string" || typeof vanity !== "string") {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Bad Request. Missing id and vanity or invalid types",
+     code: 400,
+    }),
+    {
+     status: 400,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  if (!vanity.match(/^[a-zA-Z0-9]+$/)) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Bad Request. Vanity must be alphanumeric",
+     code: 400,
+    }),
+    {
+     status: 400,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  if (vanity.length > 20) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Bad Request. Vanity must be less than 20 characters",
+     code: 400,
+    }),
+    {
+     status: 400,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  const server = await getServer(id);
+
+  if (!server || server.error) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Server not found",
+     code: 404,
+    }),
+    {
+     status: 404,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  if (!server.bot) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Bot is not in server",
+     code: 404,
+    }),
+    {
+     status: 404,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  const serverMember = await getGuildMember(server.id, session.access_token);
+
+  if (!serverMember || !serverMember.permissions_names || !serverMember.permissions_names.includes("ManageGuild") || !serverMember.permissions_names.includes("Administrator")) {
+   return new NextResponse(
+    JSON.stringify({
+     error: "Unauthorized",
+     code: 401,
+    }),
+    {
+     status: 401,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  const current = await prismaClient.guild.findUnique({
+   where: {
+    guildId: id,
+   },
+  });
+
+  if (!current) {
+   await prismaClient.guild.create({
+    data: {
+     guildId: id,
+     vanity: vanity,
+    },
+   });
+
+   await prismaClient.guildLogs.create({
+    data: {
+     guild: {
+      connectOrCreate: {
+       where: {
+        guildId: id,
+       },
+       create: {
+        guildId: id,
+       },
+      },
+     },
+     user: {
+      connect: {
+       id: session.sub,
+      },
+     },
+     content: `Set vanity to ${vanity}`,
+     type: "vanity",
+    },
+   });
+
+   return new NextResponse(
+    JSON.stringify({
+     message: `Set vanity to ${vanity}`,
+     code: 200,
+    }),
+    {
+     status: 200,
+     headers: {
+      "server-timing": `response;dur=${Date.now() - start}`,
+     },
+    }
+   );
+  }
+
+  await prismaClient.guild.update({
+   where: {
+    guildId: id,
+   },
+   data: {
+    vanity: vanity,
+   },
+  });
+
+  await prismaClient.guildLogs.create({
+   data: {
+    guild: {
+     connectOrCreate: {
+      where: {
+       guildId: id,
+      },
+      create: {
+       guildId: id,
+      },
+     },
+    },
+    user: {
+     connect: {
+      id: session.sub,
+     },
+    },
+    content: `Set vanity to ${vanity}`,
+    type: "vanity",
+   },
+  });
+
+  return new NextResponse(
+   JSON.stringify({
+    message: `Set vanity to ${vanity}`,
+    code: 200,
+   }),
+   {
+    status: 200,
+    headers: {
+     "server-timing": `response;dur=${Date.now() - start}`,
+    },
+   }
+  );
+ } catch (err) {
+  console.log(err);
+  return new NextResponse(
+   JSON.stringify({
+    error: "Internal Server Error",
+    code: 500,
+   }),
+   {
+    status: 500,
+   }
+  );
+ }
+}
--- apps/dashboard/components/blocks/client/EnablePublicDashboard.jsx
+++ apps/dashboard/components/blocks/client/EnablePublicDashboard.jsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, CheckIcon, ExclamationCircleIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { useEffect, useState } from "react";
 import Switch from "./Switch";
 import { Input } from "@/components/buttons/server/Input";
+import { PrimaryButton } from "@/components/buttons/server/Primary";
 
 export function EnablePublicDashboard({ enabled, serverId, vanityURL }) {
  const [isEnabled, setIsEnabled] = useState(enabled);
  const [disabled, setDisabled] = useState(false);
  const [vanity, setVanity] = useState(vanityURL);
  const [vanityError, setVanityError] = useState(false);
+ const [buttonText, setButtonText] = useState("Update vanity");
 
  useEffect(() => {
   setIsEnabled(enabled);
@@ -66,6 +68,39 @@ export function EnablePublicDashboard({ enabled, serverId, vanityURL }) {
   }
  };
 
+ const updateVanity = async (e) => {
+  e.preventDefault();
+  setButtonText("Updating...");
+  setVanityError("");
+  const res = await fetch("/api/settings/public-vanity", {
+   method: "POST",
+   headers: {
+    "Content-Type": "application/json",
+   },
+   body: JSON.stringify({
+    id: serverId,
+    vanity: vanity,
+   }),
+  });
+
+  if (!res.ok) {
+   setButtonText("Update vanity");
+   setVanityError("Something went wrong.");
+  }
+
+  const json = await res.json();
+
+  if (json.code === 200) {
+   setButtonText("Updated!");
+   setTimeout(() => {
+    setButtonText("Update vanity");
+   }, 5000);
+  } else {
+   setButtonText("Update vanity");
+   setVanityError("Something went wrong.");
+  }
+ };
+
  return (
   <div className="flex flex-col items-start justify-start gap-4">
    <div className="mx-auto flex flex-col items-center justify-start gap-2 font-bold md:mx-0 md:flex-row">
@@ -74,33 +109,38 @@ export function EnablePublicDashboard({ enabled, serverId, vanityURL }) {
    </div>
    <div className="mx-auto flex flex-col items-center justify-start gap-2 font-bold md:mx-0 md:flex-row">
     Set server vanity URL:
-    <div className="group flex flex-row-reverse items-center justify-start gap-0">
-     <Input
-      type="text"
-      value={vanity}
-      onChange={(e) => changeVanityText(e)}
-      disabled={disabled}
-      placeholder={"Vanity URL"}
-      className={clsx(
-       {
-        "!border-red-400 focus:!border-red-400": vanityError,
-       },
+    <div className="flex flex-col items-center justify-start gap-2 md:flex-row">
+     <div className="group flex flex-row-reverse items-center justify-start gap-0">
+      <Input
+       type="text"
+       value={vanity}
+       onChange={(e) => changeVanityText(e)}
+       disabled={disabled}
+       placeholder={"Vanity URL"}
+       className={clsx(
+        {
+         "!border-red-400 focus:!border-red-400": vanityError,
+        },
 
-       "peer !w-fit rounded-l-none border-l-0 !pl-1 font-normal focus:!border-l-0"
-      )}
-      name="vanity"
-     />
-     <div
-      className={clsx(
-       {
-        "!border-red-400 focus:!border-red-400": vanityError,
-        "border-neutral-800 peer-focus:!border-button-primary": !vanityError,
-       },
-       "select-none rounded-md rounded-r-none border border-r-0 border-r-transparent bg-transparent py-2 pl-3 font-normal text-white/60 shadow-sm outline-none !ring-0 duration-200 "
-      )}
-     >
-      {process.env.NEXT_PUBLIC_URL}/server/
+        "peer !w-fit rounded-l-none border-l-0 !pl-1 font-normal focus:!border-l-0"
+       )}
+       name="vanity"
+      />
+      <div
+       className={clsx(
+        {
+         "!border-red-400 focus:!border-red-400": vanityError,
+         "border-neutral-800 peer-focus:!border-button-primary": !vanityError,
+        },
+        "select-none rounded-md rounded-r-none border border-r-0 border-r-transparent bg-transparent py-2 pl-3 font-normal text-white/60 shadow-sm outline-none !ring-0 duration-200 "
+       )}
+      >
+       {process.env.NEXT_PUBLIC_URL}/server/
+      </div>
      </div>
+     <PrimaryButton onClick={(e) => updateVanity(e)} disabled={disabled || vanityError} className="mx-auto font-normal md:mx-0">
+      {buttonText === "Updating..." ? <ArrowPathIcon className="mr-2 h-5 w-5 animate-spin" /> : <CheckIcon className="mr-2 h-5 w-5" />} {buttonText}
+     </PrimaryButton>
     </div>
    </div>
    {vanityError && (
