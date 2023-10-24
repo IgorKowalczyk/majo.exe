54fab0f696468e36b639703cc633a8df0801ece7
Igor Kowalczyk <majonezexe@protonmail.com>
2023-10-24 13:07:35 +0200

    Update @igorkowalczyk/eslint-config to v2

--- apps/dashboard/.eslintrc.js
+++ apps/dashboard/.eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
  root: true,
- extends: ["@majoexe/eslint-config", "next"],
+ extends: ["@igorkowalczyk/eslint-config/legacy", "next"],
  rules: {
   "react/no-unescaped-entities": "off",
   "@next/next/no-html-link-for-pages": "off",
+ },
+ env: {
+  node: true,
+  es2021: true,
+  browser: true,
+  es6: true,
+ },
+ parserOptions: {
+  sourceType: "module",
+  ecmaVersion: "latest",
  },
 };
--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,17 @@
-import eslintConfig from "@majoexe/eslint-config";
+import eslintConfig from "@igorkowalczyk/eslint-config/flat";
+import turboPlugin from "eslint-plugin-turbo";
 
 export default [
   {
     ...eslintConfig,
+    plugins: {
+      ...eslintConfig.plugins,
+      "turbo": turboPlugin,
+    },
+    rules: {
+      ...eslintConfig.rules,
+      "turbo/no-undeclared-env-vars": "warn",
+    }
   }
 ]
 
