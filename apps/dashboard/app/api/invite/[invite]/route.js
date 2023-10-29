cdf4878d3b4ffa7b90b4480b5fd3ec72a8a3a1fd
Igor Kowalczyk <majonezexe@protonmail.com>
2023-10-29 11:44:05 +0100

    Fix redirect

--- apps/dashboard/app/api/invite/[invite]/route.js
+++ apps/dashboard/app/api/invite/[invite]/route.js
@@ -4,6 +4,6 @@ export const runtime = "edge";
 
 export function GET(request, { params }) {
  const invite = params.invite;
- if (!invite) return NextResponse.redirect("/dashboard");
+ if (!invite) return NextResponse.redirect(new URL("/dashboard", request.url));
  return NextResponse.redirect(`https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=1916267615&scope=guilds%20identify%20bot%20applications.commands&guild_id=${invite}&redirect_uri=${encodeURIComponent(process.env.NEXTAUTH_URL + "/dashboard")}&response_type=code`);
 }
