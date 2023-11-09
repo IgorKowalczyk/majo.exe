7b0eff366a98aafd6445a2e75b67168788c4cfb9
Igor Kowalczyk <majonezexe@protonmail.com>
2023-11-09 12:41:19 +0100

    Update layout.jsx

--- apps/dashboard/app/layout.jsx
+++ apps/dashboard/app/layout.jsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import { dashboardConfig } from "@majoexe/config";
 import { Analytics } from "@vercel/analytics/react";
-import { GeistSans } from "geist/font";
+import { GeistSans } from "geist/font/sans";
 import Link from "next/link";
 import { Toaster } from "sonner";
 import { twMerge } from "tailwind-merge";
