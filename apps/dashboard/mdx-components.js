41a56fcbf0eb5e829225e1aa67afaffa9bfe27ce
Igor Kowalczyk <majonezexe@protonmail.com>
2023-11-30 23:30:00 +0100

    Refactor normalize function in mdx-components.js

--- apps/dashboard/mdx-components.js
+++ apps/dashboard/mdx-components.js
@@ -1,13 +1,17 @@
 function normalize(text) {
- if (typeof text !== "string") return "";
- return text
-  .trim()
-  .toLowerCase()
-  .replace(/[^a-z0-9]+/g, "-") // Replace all non-word chars with -
-  .replace(/\s+/g, "-") // Replace spaces with -
-  .replace(/&/g, "-and-") // Replace & with 'and'
-  .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
-  .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+ return (
+  text
+   .toString()
+   .trim()
+   .toLowerCase()
+   .replace(/[^a-z0-9]+/g, "-") // Replace all non-word chars with -
+   .replace(/\s+/g, "-") // Replace spaces with -
+   .replace(/&/g, "-and-") // Replace & with 'and'
+   /* eslint-disable no-useless-escape */
+   .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+   .replace(/\-\-+/g, "-") // Replace multiple - with single -
+  /* eslint-enable no-useless-escape */
+ );
 }
 
 export function useMDXComponents(components) {
