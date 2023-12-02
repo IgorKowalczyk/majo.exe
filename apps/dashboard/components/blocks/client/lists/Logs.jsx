b1d9a9c9e07a9a562232cc98cd27282a98dac118
Igor Kowalczyk <majonezexe@protonmail.com>
2023-12-02 17:38:42 +0100

    Refactor Disclosure component

--- apps/dashboard/components/blocks/client/lists/Logs.jsx
+++ apps/dashboard/components/blocks/client/lists/Logs.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Disclosure, Transition } from "@headlessui/react";
-import { NoSymbolIcon, ChevronDownIcon, PaintBrushIcon, MagnifyingGlassIcon, LinkIcon, UsersIcon, BarsArrowDownIcon, BarsArrowUpIcon, CubeTransparentIcon, CubeIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon, BarsArrowDownIcon, BarsArrowUpIcon } from "@heroicons/react/24/outline";
+import { CubeTransparentIcon, CubeIcon, NoSymbolIcon, PaintBrushIcon, LinkIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { formatDate, formatDuration } from "@majoexe/util/functions";
-import clsx from "clsx";
+import Link from "next/link";
 import React, { useState, useRef, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import { GraphSkeleton } from "../../Skeletons";
+import { Disclosure } from "../shared/Disclosure";
 import { Tooltip } from "../shared/Tooltip";
 import Image from "@/components/blocks/client/shared/Image";
 import { InputWithIcon } from "@/components/buttons/server/Input";
@@ -66,65 +67,55 @@ export default function Logs({ initialItems, id }) {
      </span>
     </Tooltip>
    </div>
-   <InfiniteScroll hasMore={hasMore} pageStart={0} loadMore={loadMore} loader={<GraphSkeleton className={"my-4 !h-20"} />}>
+   <InfiniteScroll hasMore={hasMore} pageStart={0} loadMore={loadMore} loader={<GraphSkeleton className={"mb-4 !h-20"} />}>
     {sortedFilteredItems.map((item, index) => (
-     <Disclosure key={index}>
-      {({ open }) => (
+     <Disclosure
+      key={index}
+      button={
        <>
-        <Disclosure.Button
-         className={clsx("bg-background-navbar my-4 flex w-full flex-row items-center justify-start gap-4 rounded-md border border-neutral-800 px-6 py-4 duration-200", {
-          "mb-0 rounded-b-none": open,
-         })}
-        >
-         <div className="relative">
-          {item.user?.avatar && <Image src={`https://cdn.discordapp.com/avatars/${item.user?.discordId}/${item.user?.avatar}.${item.user?.avatar.startsWith("a_") ? "gif" : "png"}`} alt={`${item.user?.name} avatar`} quality={95} width={32} height={32} className="min-h-12 min-w-12 h-12 w-12 rounded-full" />}
-          {item.type === "profanity" && <NoSymbolIcon className="bg-button-secondary/80 min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 text-red-400 opacity-80" />}
-          {item.type === "embed_color" && <PaintBrushIcon className="bg-button-secondary/80 text-button-primary min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 opacity-80" />}
-          {item.type === "command_change" && <CubeTransparentIcon className="bg-button-secondary/80 text-button-primary min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 opacity-80" />}
-          {item.type === "category_change" && <CubeIcon className="bg-button-secondary/80 text-button-primary min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 opacity-80" />}
-          {item.type === "public_dashboard" && <UsersIcon className="bg-button-secondary/80 min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 text-green-400 opacity-80" />}
-          {item.type === "vanity" && <LinkIcon className="bg-button-secondary/80 text-button-primary min-h-5 min-w-5 absolute bottom-0 right-0 h-5 w-5 rounded-full border border-white/10 p-1 opacity-80" />}
-         </div>
-         <div className="flex flex-col">
-          <p className="text-left font-bold">
-           {item.user?.global_name || item.user?.username}
-           {item.user?.discriminator !== "0" && <span className="opacity-70">#{item.user?.discriminator || "0000"}</span>}: {item.content}
-          </p>
-          <span className="text-left opacity-70">
-           {formatDate(item.createdAt)} ({formatDuration(new Date().getTime() - new Date(item.createdAt).getTime())} ago)
-          </span>
-         </div>
-         <ChevronDownIcon
-          className={clsx(
-           {
-            "rotate-180": open,
-           },
-           "min-h-4 min-w-4 ml-auto h-4 w-4 duration-200 motion-reduce:transition-none"
-          )}
-         />
-        </Disclosure.Button>
-
-        <Transition enter="transition duration-100 ease-out" enterFrom="transform scale-95 opacity-0" enterTo="transform scale-100 opacity-100" leave="transition duration-75 ease-out" leaveFrom="transform scale-100 opacity-100" leaveTo="transform scale-95 opacity-0">
-         <Disclosure.Panel className="bg-background-navbar w-full rounded-md rounded-t-none border border-t-0 border-neutral-800 px-6 py-4">
-          {item.actionTaken && (
-           <p>
-            <span className="font-bold">Action taken:</span> None
-           </p>
-          )}
-          <p>
-           <span className="font-bold">Type:</span> {item.type}
-          </p>
-          <p>
-           <span className="font-bold">Date:</span> {item.createdAt}
-          </p>
-          <p>
-           <span className="font-bold">User:</span> {item.user?.global_name || item.user?.username}
-           {item.user?.discriminator !== "0" && <span className="opacity-70">#{item.user?.discriminator || "0000"}</span>} (ID: {item.user?.discordId || item.user?.id})
-          </p>
-         </Disclosure.Panel>
-        </Transition>
+        <div className="flex flex-row items-center gap-4">
+         {item.type && (
+          <>
+           {item.type === "profanity" && <NoSymbolIcon className="min-h-6 min-w-6 h-6 w-6 text-red-400" />}
+           {item.type === "embed_color" && <PaintBrushIcon className="text-white/60 ui-open:text-accent-primary duration-200 min-h-6 min-w-6 h-6 w-6" />}
+           {item.type === "command_change" && <CubeTransparentIcon className="text-white/60 ui-open:text-accent-primary duration-200 min-h-6 min-w-6 h-6 w-6" />}
+           {item.type === "category_change" && <CubeIcon className="text-white/60 ui-open:text-accent-primary duration-200 min-h-6 min-w-6 h-6 w-6" />}
+           {item.type === "public_dashboard" && <UsersIcon className="text-white/60 ui-open:text-accent-primary duration-200 min-h-6 min-w-6 h-6 w-6" />}
+           {item.type === "vanity" && <LinkIcon className="text-white/60 ui-open:text-accent-primary duration-200 min-h-6 min-w-6 h-6 w-6" />}
+          </>
+         )}
+         {item.user?.avatar && <Image src={`https://cdn.discordapp.com/avatars/${item.user?.discordId}/${item.user?.avatar}.${item.user?.avatar.startsWith("a_") ? "gif" : "png"}`} alt={`${item.user?.name} avatar`} quality={95} width={32} height={32} className="min-h-12 min-w-12 h-12 w-12 rounded-full" />}
+        </div>
+        <div className="flex flex-col">
+         <p className="text-left font-bold">
+          {item.user?.global_name || item.user?.username}
+          {item.user?.discriminator !== "0" && <span className="opacity-70">#{item.user?.discriminator || "0000"}</span>}: {item.content}
+         </p>
+         <span className="text-left opacity-70">
+          {formatDate(item.createdAt)} ({formatDuration(new Date().getTime() - new Date(item.createdAt).getTime())} ago)
+         </span>
+        </div>
        </>
+      }
+     >
+      {item.actionTaken && (
+       <p>
+        <span className="font-bold">Action taken:</span> <span className="opacity-70">{item.actionTaken}</span>
+       </p>
       )}
+      <p>
+       <span className="font-bold">Type:</span> <span className="opacity-70">{item.type}</span>
+      </p>
+      <p>
+       <span className="font-bold">Date:</span> <span className="opacity-70">{item.createdAt}</span>
+      </p>
+      <p>
+       <span className="font-bold">User:</span>{" "}
+       <Link href={`/dashboard/${id}/user/${item.user?.discordId}`} className={"hover:text-button-primary opacity-70 hover:opacity-100 duration-200"}>
+        {item.user?.global_name || item.user?.username}
+        {item.user?.discriminator !== "0" && <span className="opacity-70">#{item.user?.discriminator || "0000"}</span>} (ID: {item.user?.discordId || item.user?.id})
+       </Link>
+      </p>
      </Disclosure>
     ))}
    </InfiniteScroll>
--- apps/dashboard/components/blocks/client/shared/Disclosure.jsx
+++ apps/dashboard/components/blocks/client/shared/Disclosure.jsx
@@ -1,48 +1,21 @@
 "use client";
 
-import { Disclosure, Transition } from "@headlessui/react";
+import { Disclosure as HeadlessDisclosure, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import clsx from "clsx";
 
-export function ClientDisclosure({ buttonElements, buttonIcon, children, ...props }) {
+export function Disclosure({ button, children, ...props }) {
  return (
-  <Disclosure {...props} className="w-full">
-   {({ open }) => (
-    <>
-     <Disclosure.Button
-      className={clsx("bg-background-navbar my-4 flex w-full flex-row items-center justify-start gap-4 rounded-md border border-neutral-800 px-6 py-4 duration-200", {
-       "mb-0 rounded-b-none": open,
-      })}
-     >
-      {buttonIcon && (
-       <div
-        className={clsx(
-         {
-          "[&>*]:fill-accent-primary": open,
-         },
-         "[&>*]:duration-200 [&>*]:motion-reduce:transition-none"
-        )}
-       >
-        {buttonIcon}
-       </div>
-      )}
-      {buttonElements}
+  <HeadlessDisclosure {...props} className="w-full">
+   <>
+    <HeadlessDisclosure.Button className="bg-background-navbar ui-open:mb-0 ui-open:rounded-b-none my-4 flex w-full flex-row items-center justify-start gap-4 rounded-md border border-neutral-800 px-6 py-4 duration-200">
+     {button}
+     <ChevronDownIcon className="min-h-4 min-w-4 ml-auto ui-open:stroke-accent-primary ui-open:rotate-180 h-4 w-4 duration-200 motion-reduce:transition-none" />
+    </HeadlessDisclosure.Button>
 
-      <ChevronDownIcon
-       className={clsx(
-        {
-         "stroke-accent-primary rotate-180": open,
-        },
-        "min-h-4 min-w-4 ml-auto h-4 w-4 duration-200 motion-reduce:transition-none"
-       )}
-      />
-     </Disclosure.Button>
-
-     <Transition enter="transition duration-100 ease-out" enterFrom="transform scale-95 opacity-0" enterTo="w-full transform scale-100 opacity-100" leave="w-full transition duration-75 ease-out" leaveFrom="transform scale-100 opacity-100" leaveTo="w-full transform scale-95 opacity-0">
-      <Disclosure.Panel className="bg-background-navbar w-full rounded-md rounded-t-none border border-t-0 border-neutral-800 px-6 py-4">{children}</Disclosure.Panel>
-     </Transition>
-    </>
-   )}
-  </Disclosure>
+    <Transition enter="transition duration-100 ease-out" enterFrom="transform scale-95 opacity-0" enterTo="w-full transform scale-100 opacity-100" leave="w-full transition duration-75 ease-out" leaveFrom="transform scale-100 opacity-100" leaveTo="w-full transform scale-95 opacity-0">
+     <HeadlessDisclosure.Panel className="bg-background-navbar w-full rounded-md rounded-t-none border border-t-0 border-neutral-800 px-6 py-4">{children}</HeadlessDisclosure.Panel>
+    </Transition>
+   </>
+  </HeadlessDisclosure>
  );
 }
