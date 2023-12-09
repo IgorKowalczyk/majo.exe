add564083cb05689738cff14f3af0258f6939df4
Igor Kowalczyk <majonezexe@protonmail.com>
2023-12-10 00:32:59 +0100

    Update loading animations

--- apps/dashboard/app/dashboard/[server]/logs/loading.jsx
+++ apps/dashboard/app/dashboard/[server]/logs/loading.jsx
@@ -1,5 +1,6 @@
-import { ListBulletIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, ListBulletIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Header1 } from "@/components/Headers";
+import { InputWithIcon } from "@/components/Input";
 import { GraphSkeleton } from "@/components/Skeletons";
 
 export default function Loading() {
@@ -10,8 +11,14 @@ export default function Loading() {
     Activity Logs
    </Header1>
    <div className="gap-4 overflow-auto">
+    <div className="mb-4 flex items-center justify-center gap-2">
+     <InputWithIcon placeholder="Search..." icon={<MagnifyingGlassIcon className="min-h-5 min-w-5 h-5 w-5 text-white/50" aria-hidden="true" role="img" />} disabled={true} />
+     <span className="hover:border-button-primary flex h-[41.6px] cursor-pointer items-center justify-center rounded-md border border-neutral-800 px-3 py-2 text-white duration-200">
+      <ArrowPathIcon className="min-h-5 min-w-5 h-5 w-5 animate-spin" />
+     </span>
+    </div>
     {[...Array(5)].map((_, i) => (
-     <GraphSkeleton className={"my-2 !h-20"} key={i} />
+     <GraphSkeleton className={"my-4 !h-[81.6px]"} key={i} />
     ))}
    </div>
   </>
--- apps/dashboard/components/Skeletons.jsx
+++ apps/dashboard/components/Skeletons.jsx
@@ -1,11 +1,7 @@
 import { twMerge } from "tailwind-merge";
 
 export function GraphSkeleton({ className }) {
- return (
-  <div className="flex flex-col items-center justify-center">
-   <div className={twMerge(className, "bg-background-menu-button relative isolate h-80 w-full overflow-hidden rounded-md border border-neutral-800 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent")} />
-  </div>
- );
+ return <div className={twMerge(className, "bg-background-menu-button relative isolate h-80 w-full overflow-hidden rounded-md border border-neutral-800 before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_1s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/10 before:to-transparent")} />;
 }
 
 export function TextSkeleton({ className, ...otherProps }) {
