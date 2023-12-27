623e9d987d5b4864cb4261167f407bed08dd273d
Igor Kowalczyk <majonezexe@protonmail.com>
2023-12-27 23:16:37 +0100

    Add loading state to giveaways

---
+++ apps/dashboard/app/dashboard/[server]/giveaways/loading.jsx
@@ -0,0 +1,18 @@
+import { GiftIcon } from "@heroicons/react/24/outline";
+import { Block } from "@/components/Block";
+import { Header1 } from "@/components/Headers";
+import { EmbedSkeleton } from "@/components/Skeletons";
+
+export default async function Loading() {
+ return (
+  <>
+   <Header1>
+    <GiftIcon className="h-9 min-h-9 w-9 min-w-9" />
+    Giveaways
+   </Header1>
+   <Block className="mt-4 flex w-full overflow-auto">
+    <EmbedSkeleton className="h-64 w-full" />
+   </Block>
+  </>
+ );
+}
