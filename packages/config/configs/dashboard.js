6e47f1b826cff54447ae47adfc39eae42bce7391
Igor Kowalczyk <majonezexe@protonmail.com>
2024-03-09 21:42:02 +0100

    Update bot logo URL in dashboard config

--- packages/config/configs/dashboard.js
+++ packages/config/configs/dashboard.js
@@ -2,7 +2,7 @@
 export const dashboardConfig = {
  title: "Majo.exe", // string. Dashboard title, will be shown in browser tab and in search results
  url: "https://majoexe.xyz", // string. Dashboard url, to Disable dashboard, set this to null (Note: when you change it to null the dashboard will not work and commands related to dashboard will not work)
- logo: "https://media.discordapp.net/attachments/905722570286960650/997068981187919962/logo-modified.png", // string. Logo of your bot
+ logo: "https://majoexe.xyz/assets/avatar.png", // string. Logo of your bot
  description: "Majo.exe - Discord bot for Fun, Memes, Images, Giveaway, Economy and Anime! Majo.exe serve over 117 commands!", // string. Dashboard description, will be shown in search results
  image: "/opengraph-image", // string. Dashboard open graph image (Note: this is not a url, this is a path to the image/generator, for example: /opengraph-image)
 };
