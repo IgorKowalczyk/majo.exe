addf5c2376e552558bdc5e0d6f299267b36d7034
Igor Kowalczyk <majonezexe@protonmail.com>
2024-09-29 21:59:26 +0200

    Refactor error component and update styles

--- apps/dashboard/app/error.jsx
+++ apps/dashboard/app/error.jsx
@@ -1,19 +1,21 @@
+"use client";
+
 import { ButtonPrimary } from "@/components/Buttons";
 import { ButtonSecondary } from "@/components/Buttons";
 import { Header2, Header3 } from "@/components/Headers";
 import { Icons, iconVariants } from "@/components/Icons";
 import "styles/glitch.css";
 
-export default function NotFound() {
+export default function Error({ refresh }) {
  return (
   <div className="relative z-20 flex min-h-screen w-full flex-col items-center justify-center gap-4">
    <div className="z-30">
-    <Header2 className="justify-center mb-2 !text-3xl">404, page not found!</Header2>
-    <Header3 className="text-center max-w-md font-normal opacity-50">We're sorry we can't find the page you're looking for. Please try again later or contact support.</Header3>
+    <Header2 className="justify-center mb-2 !text-3xl">500, server error!</Header2>
+    <Header3 className="text-center max-w-lg font-normal opacity-50">Sorry, we're having trouble loading this page. Please try again later or contact support.</Header3>
     <div className="mt-6 flex flex-wrap justify-center gap-4">
      <ButtonPrimary href="/">
-      <Icons.arrowLeft className={iconVariants({ variant: "button" })} />
-      Go back home
+      <Icons.refresh className={iconVariants({ variant: "button" })} onClick={refresh} />
+      Try again
      </ButtonPrimary>
      <ButtonSecondary href="/discord">
       <Icons.help className={iconVariants({ variant: "button" })} /> Contact support
