9cc32a47913c51bfda010dddb9fa8e4777ff38fc
Igor Kowalczyk <majonezexe@protonmail.com>
2024-11-21 23:01:43 +0100

    Fix Tooltip component to ensure content is not empty

--- apps/dashboard/components/client/shared/Tooltip.tsx
+++ apps/dashboard/components/client/shared/Tooltip.tsx
@@ -8,7 +8,7 @@ import "tippy.js/dist/tippy.css";
 import React from "react";
 
 export const Tooltip = React.forwardRef<HTMLDivElement, { children: React.ReactNode; content: string | number } & React.ComponentProps<typeof Tippy>>(({ children, content, ...props }, ref) => (
- <Tippy ref={ref} content={content || ""} animation="shift-away" plugins={[animateFill]} hideOnClick={false} duration={400} animateFill={true} className="tippy-box font-normal" theme="translucent" interactive={false} placement="top" {...props}>
+ <Tippy ref={ref} content={content} animation="shift-away" plugins={[animateFill]} hideOnClick={false} duration={400} animateFill={true} className="tippy-box font-normal" theme="translucent" interactive={false} placement="top" {...props}>
   {children}
  </Tippy>
 ));
