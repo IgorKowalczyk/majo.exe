6c90f357567cef54ce9def8ace6d0d75a3d4a376
Igor Kowalczyk <majonezexe@protonmail.com>
2024-11-24 20:51:19 +0100

    Refactor Tooltip component: change Trigger to use 'asChild' prop for improved flexibility

--- apps/dashboard/components/client/shared/Tooltip.tsx
+++ apps/dashboard/components/client/shared/Tooltip.tsx
@@ -12,7 +12,7 @@ export interface TooltipProps extends React.ComponentPropsWithoutRef<typeof Cont
 export const Tooltip = React.forwardRef<React.ElementRef<typeof Content>, TooltipProps>(({ className, content, children, sideOffset = 4, ...props }, ref) => (
  <Provider>
   <Root delayDuration={100}>
-   <Trigger>{children}</Trigger>
+   <Trigger asChild>{children}</Trigger>
    <Content ref={ref} sideOffset={sideOffset} className={cn("z-50 overflow-hidden rounded-md border border-neutral-700 bg-background-menu-button px-3 py-1.5 text-sm shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2", className)} {...props}>
     {content}
    </Content>
