b91afbb18458cddf66eb124704b2f517f40fecbe
Igor Kowalczyk <majonezexe@protonmail.com>
2024-12-21 15:44:02 +0100

    feat(logging): add logging for guild ban additions and removals

---
+++ apps/bot/events/guild/guildBanAdd.ts
@@ -0,0 +1,40 @@
+import { GuildLogType } from "@majoexe/database";
+import { getGuildLogSettings } from "@majoexe/util/database";
+import { GuildBan, Guild, EmbedBuilder } from "discord.js";
+import type { Majobot } from "@/index";
+
+export async function guildBanAdd(client: Majobot, ban: GuildBan) {
+ try {
+  if (!ban.guild) return;
+  const settings = await getGuildLogSettings(ban.guild.id, GuildLogType.GuildBanAdd);
+  if (!settings?.enabled || !settings.channelId) return;
+  const discordGuild = ban.guild as Guild;
+  const channel = await discordGuild.channels.fetch(settings.channelId);
+  if (!channel || !channel.isTextBased()) return;
+
+  const fields = [
+   {
+    name: "User",
+    value: `${ban.user.globalName || ban.user.username || "Unknown"} (${ban.user.id})`,
+   },
+   {
+    name: "Reason",
+    value: ban.reason || "No reason provided",
+   },
+  ];
+
+  const embed = new EmbedBuilder()
+   .setTitle("🔨 User Banned")
+   .setFields(fields)
+   .setColor("#EF4444")
+   .setTimestamp()
+   .setFooter({
+    text: `Banned by ${ban.user.globalName || ban.user.username || "Unknown"}`,
+    iconURL: ban.user.displayAvatarURL({ size: 256 }) || undefined,
+   });
+
+  channel.send({ embeds: [embed] });
+ } catch (err: unknown) {
+  client.debugger("error", err);
+ }
+}
---
+++ apps/bot/events/guild/guildBanRemove.ts
@@ -0,0 +1,36 @@
+import { GuildLogType } from "@majoexe/database";
+import { getGuildLogSettings } from "@majoexe/util/database";
+import { GuildBan, Guild, EmbedBuilder } from "discord.js";
+import type { Majobot } from "@/index";
+
+export async function guildBanRemove(client: Majobot, ban: GuildBan) {
+ try {
+  if (!ban.guild) return;
+  const settings = await getGuildLogSettings(ban.guild.id, GuildLogType.GuildBanRemove);
+  if (!settings?.enabled || !settings.channelId) return;
+  const discordGuild = ban.guild as Guild;
+  const channel = await discordGuild.channels.fetch(settings.channelId);
+  if (!channel || !channel.isTextBased()) return;
+
+  const fields = [
+   {
+    name: "User",
+    value: `${ban.user.globalName || ban.user.username || "Unknown"} (${ban.user.id})`,
+   },
+  ];
+
+  const embed = new EmbedBuilder()
+   .setTitle("🔓 User Unbanned")
+   .setFields(fields)
+   .setColor("#10B981")
+   .setTimestamp()
+   .setFooter({
+    text: `Unbanned by ${ban.user.globalName || ban.user.username || "Unknown"}`,
+    iconURL: ban.user.displayAvatarURL({ size: 256 }) || undefined,
+   });
+
+  channel.send({ embeds: [embed] });
+ } catch (err: unknown) {
+  client.debugger("error", err);
+ }
+}
