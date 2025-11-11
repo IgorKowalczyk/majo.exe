7324f6c4858961f8953a3a6e73f5d36979c32623
Igor Kowalczyk <majonezexe@protonmail.com>
2025-11-11 13:42:00 +0100

    Add prisma adapters

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "prisma/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaPg } from "@prisma/adapter-pg";
 import "@dotenvx/dotenvx/config";
 
 export default defineConfig({
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DATABASE_DIRECT_URL,
+  experimental: {
+    studio: true,
+    adapter: true,
   },
-  schema: "./prisma/schema.prisma",
+  engine: "js",
+  /* @ts-expect-error Adapter type */
+  async adapter() {
+    if (process.env.DATABASE_URL?.includes("neon.tech")) {
+      return new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+    } else {
+      return new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    }
+  },
   migrations: {
     path: "./prisma/migrations",
     seed: "tsx ./src/seed.ts",
